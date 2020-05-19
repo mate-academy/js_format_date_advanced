cc8b87cee0d7027360bbee79c8354d8038b6e81f
Kryvytskyy Volodymyr <vovakrivickiy34@gmail.com>
2020-05-19 16:58:40 +0300

    Update src/formatDate.js
    Co-authored-by: Vladyslav Polets <46418908+vpolets@users.noreply.github.com>

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,7 +1,13 @@
 'use strict';
 
 /**
- *
+ * Time flies, standards change. Let's get rid of the routine of changing the date format, and create a function for formatting dates.
+* Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable, and the new `toFormat` array variable. Function returns given date in `toFormat` format.
+* 
+* Example:
+* formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
+* formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
+* formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
