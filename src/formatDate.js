01a1968f0c5862d4d6c5fad389d71363b22f0881
Виктор Зинченко <zenia1@i.ua>
2020-06-22 21:19:30 +0300

    SolutionRecover

--- src/formatDate.js
+++ src/formatDate.js
@@ -4,12 +4,12 @@
 function formatDate(date, fromFormat, toFormat) {
   if (date.includes(fromFormat[3])) {
     const dateSplit = date.split(fromFormat[3]);
-    let day = 'DD';
-    let monts = 'MM';
-    let year = 'YYYY';
+    let day = 'D';
+    let monts = 'M';
+    let year = 'Y';
 
     for (let i = 0; i < fromFormat.length; i++) {
-      switch (fromFormat[i]) {
+      switch (fromFormat[i][0]) {
         case day: day = dateSplit[i];
           break;
         case monts: monts = dateSplit[i];
