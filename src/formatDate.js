de2dc1d9a7094d83f60ce538a03862dbfa0ccce7
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-08-07 01:35:03 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,16 +1,6 @@
 'use strict';
 
 /**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
- *
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
@@ -19,7 +9,36 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const arrDate = date.split(fromFormat[3]);
+  const result = [];
+
+  for (let i = 0; i < arrDate.length; i++) {
+    if (toFormat.indexOf('DD') === i) {
+      result.push(arrDate[fromFormat.indexOf('DD')]);
+    }
+
+    if (toFormat.indexOf('MM') === i) {
+      result.push(arrDate[fromFormat.indexOf('MM')]);
+    }
+
+    if (toFormat.indexOf('YY') === i) {
+      if (fromFormat.includes('YY')) {
+        result.push(arrDate[fromFormat.indexOf('YY')]);
+      } else if (fromFormat.includes('YYYY')) {
+        result.push(arrDate[fromFormat.indexOf('YYYY')].slice(-2));
+      }
+    }
+
+    if (toFormat.indexOf('YYYY') === i) {
+      if (fromFormat.includes('YYYY')) {
+        result.push(arrDate[fromFormat.indexOf('YYYY')]);
+      } else if (fromFormat.includes('YY')) {
+        result.push(`20${arrDate[fromFormat.indexOf('YY')]}`);
+      }
+    }
+  }
+
+  return (result.join(toFormat[3]));
 }
 
 module.exports = formatDate;
