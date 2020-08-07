967e6e20aeb453da5172c9daf9397dbd89d22561
nanccyy <lidiapriymak@gmail.com>
2020-08-08 00:50:28 +0800

    solved

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,25 +1,78 @@
 'use strict';
 
-/**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
- *
- * @param {string} date
- * @param {string[]} fromFormat
- * @param {string[]} toFormat
- *
- * @returns {string}
- */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const [, , , separator] = fromFormat;
+  const [, , , finalSeparator] = toFormat;
+
+  const inputDate = date.split(separator);
+
+  let yearFrom;
+  let monthFrom;
+  let dayFrom;
+
+  let dayTo;
+  let monthTo;
+  let yearTo;
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    switch (fromFormat[i]) {
+      case 'YYYY':
+        yearFrom = i;
+        break;
+
+      case 'YY':
+        yearFrom = i;
+        break;
+
+      case 'MM':
+        monthFrom = i;
+        break;
+
+      case 'DD':
+        dayFrom = i;
+        break;
+    }
+  }
+
+  for (let i = 0; i < toFormat.length; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY':
+        yearTo = i;
+        break;
+
+      case 'YY':
+        yearTo = i;
+        break;
+
+      case 'MM':
+        monthTo = i;
+        break;
+
+      case 'DD':
+        dayTo = i;
+        break;
+    }
+  }
+
+  const finalDate = [];
+
+  if (fromFormat[yearFrom].length > toFormat[yearTo].length) {
+    inputDate[yearFrom] = inputDate[yearFrom].slice(2, 4);
+  }
+
+  if (toFormat[yearTo].length > fromFormat[yearFrom].length) {
+    if (+inputDate[yearFrom] > 22) {
+      inputDate[yearFrom] = '19' + inputDate[yearFrom];
+    } else {
+      inputDate[yearFrom] = '20' + inputDate[yearFrom];
+    }
+  }
+
+  finalDate[yearTo] = inputDate[yearFrom];
+  finalDate[monthTo] = inputDate[monthFrom];
+  finalDate[dayTo] = inputDate[dayFrom];
+
+  return finalDate.join(finalSeparator);
 }
 
 module.exports = formatDate;
