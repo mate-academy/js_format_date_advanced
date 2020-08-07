ad8d4b07cec2d2980d9f89e70ca57e663900f27a
Alisa <aliceblunt8@gmail.com>
2020-08-07 20:20:07 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 /**
@@ -5,7 +6,7 @@
  * and create a function for formatting dates.
  * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
  * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
+ *
  * Example:
  * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
  * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
@@ -19,7 +20,54 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const oldDate = date.split(oldSeparator);
+
+  const oldFormat = {};
+  const result = [];
+
+  // Create oldFormat {} from fromFormat []
+  for (let i = 0; i < oldDate.length; i++) {
+    oldFormat[fromFormat[i]] = oldDate[i];
+  }
+
+  // Check for 'DD' and 'MM' keys in oldFormat {}, if found push to result []
+  for (let i = 0; i < oldDate.length; i++) {
+    if (toFormat[i] in oldFormat) {
+      result.push(oldFormat[toFormat[i]]);
+    }
+  }
+
+  const newYearFormat = toFormat[2];
+
+  // Check if new format of year isn't already in oldFormat {}, if found return result []
+  if (newYearFormat in oldFormat) {
+    return result.join(newSeparator);
+  }
+
+  // Check for different formats of year
+  for (const key in oldFormat) {
+    if (key.startsWith('Y')) {
+      switch (key) {
+        case 'YY':
+          if (+oldFormat[key] > 20) {
+            result.push('20' + oldFormat[key]);
+          } else {
+            result.push('19' + oldFormat[key]);
+          }
+          break;
+
+        case 'YYYY':
+          result.push(oldFormat[key].slice(2));
+          break;
+
+        default: return 'Wrong year format!';
+      }
+    }
+  }
+
+  return result.join(newSeparator);
+};
 
 module.exports = formatDate;
