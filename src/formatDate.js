3303b0d1992683237d64404bcc427b761e0ac4ae
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-07 17:55:13 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,15 +1,7 @@
 'use strict';
 
 /**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
+ * Here was many issues because of task description line length
  *
  * @param {string} date
  * @param {string[]} fromFormat
@@ -19,7 +11,67 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let oldDivisor;
+
+  let oldDayIndex;
+  let oldYearIndex;
+  let oldMonthIndex;
+
+  let newDate = [];
+  let newDayIndex;
+  let newYearIndex;
+  let newMonthIndex;
+  let newDivisor;
+
+  // get indexes of old format
+  for (let i = 0; i < fromFormat.length; i++) {
+    if (fromFormat[i].includes('Y')) {
+      oldYearIndex = i;
+    } else if (fromFormat[i].includes('M')) {
+      oldMonthIndex = i;
+    } else if (fromFormat[i].includes('D')) {
+      oldDayIndex = i;
+    } else {
+      oldDivisor = fromFormat[i];
+    }
+  }
+
+  // get indexes of new format
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i].includes('Y')) {
+      newYearIndex = i;
+    } else if (toFormat[i].includes('M')) {
+      newMonthIndex = i;
+    } else if (toFormat[i].includes('D')) {
+      newDayIndex = i;
+    } else {
+      newDivisor = toFormat[i];
+    }
+  }
+
+  // get data to date variables
+  const day = date.split(oldDivisor)[oldDayIndex];
+  const month = date.split(oldDivisor)[oldMonthIndex];
+  let year = date.split(oldDivisor)[oldYearIndex];
+
+  // in case of old lenth of year and new length of year are different
+  if (toFormat[newYearIndex].length > year.length) {
+    if (+year > 20) {
+      year = '19' + year;
+    } else {
+      year = '20' + year;
+    }
+  } else {
+    year = year.slice(-toFormat[newYearIndex].length);
+  }
+
+  // make new date
+  newDate[newDayIndex] = day;
+  newDate[newMonthIndex] = month;
+  newDate[newYearIndex] = year;
+  newDate = newDate.join(newDivisor);
+
+  return newDate;
 }
 
 module.exports = formatDate;
