84c3232e1f957debf3d4c6b6ed0e01eab14fa987
Taras Sidak <tarasidak1990@gmail.com>
2020-08-07 15:58:02 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,25 +1,37 @@
 'use strict';
 
-/**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
- *
+/*
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const toDate = date.split(oldSeparator);
+  const newDate = [];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    for (let j = 0; j < fromFormat.length; j++) {
+      if (fromFormat[j] === toFormat[i]) {
+        newDate.push(toDate[j]);
+      }
+
+      if (fromFormat[j] === 'YY' && toFormat[i] === 'YYYY') {
+        newDate.push('19' + toDate[j].slice(2, 4));
+      }
+
+      if (fromFormat[j] === 'YYYY' && toFormat[i] === 'YY') {
+        newDate.push(toDate[j].slice(2, 4));
+      }
+    }
+  }
+
+  const result = newDate.join(newSeparator);
+
+  return result;
 }
 
 module.exports = formatDate;
