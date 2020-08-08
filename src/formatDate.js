f0ca43f8b941ad6158b823f783447a808635332f
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-08 19:39:28 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -20,6 +20,38 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const newDate = [];
+  const value = date.split(fromFormat[fromFormat.length - 1]);
+  const obj = {
+    [fromFormat[0]]: value[0],
+    [fromFormat[1]]: value[1],
+    [fromFormat[2]]: value[2],
+  };
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (obj.hasOwnProperty(toFormat[i])) {
+      newDate.push(obj[toFormat[i]]);
+    }
+  }
+
+  if (obj.hasOwnProperty('YY') === toFormat.includes('YY')
+  || obj.hasOwnProperty('YYYY') === toFormat.includes('YYYY')) {
+    return newDate.join(toFormat[toFormat.length - 1]);
+  }
+
+  if (obj.hasOwnProperty('YY')) {
+    if (Number(obj.YY) > 20) {
+      newDate.push('19' + obj.YY);
+    } else {
+      newDate.push('20' + obj.YY);
+    }
+  }
+
+  if (obj.hasOwnProperty('YYYY')) {
+    newDate.push(obj.YYYY.split('').splice(2, 2).join(''));
+  }
+
+  return newDate.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
