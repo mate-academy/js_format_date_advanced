81944282f62d960a94180310ffe6f5b6118430ad
Kostya Sabada <kostyasabada@gmail.com>
2020-08-08 12:01:55 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,15 +1,7 @@
 'use strict';
 
 /**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
+
  *
  * @param {string} date
  * @param {string[]} fromFormat
@@ -19,7 +11,57 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldArr = date.split(fromFormat[fromFormat.length - 1]);
+  let year;
+  let month;
+  let day;
+  const newDateArr = [];
+
+  for (let i = 0; i < fromFormat.length; ++i) {
+    switch (fromFormat[i]) {
+      case 'YY':
+      case 'YYYY':
+        year = oldArr[i];
+        break;
+      case 'MM':
+        month = oldArr[i];
+        break;
+      case 'DD':
+        day = oldArr[i];
+    }
+  }
+
+  for (let i = 0; i < fromFormat.length; ++i) {
+    for (let j = 0; j < toFormat.length; ++j) {
+      if (fromFormat[i] === 'YYYY' && toFormat[j] === 'YY') {
+        year = year.split('').slice(2).join('');
+      }
+
+      if (fromFormat[i] === 'YY' && toFormat[j] === 'YYYY') {
+        if (year > 14) {
+          year = `19${year}`;
+        } else {
+          year = `20${year}`;
+        }
+      }
+    }
+  }
+
+  for (let j = 0; j < toFormat.length; ++j) {
+    switch (toFormat[j]) {
+      case 'YY':
+      case 'YYYY':
+        newDateArr.push(year);
+        break;
+      case 'MM':
+        newDateArr.push(month);
+        break;
+      case 'DD':
+        newDateArr.push(day);
+    }
+  }
+
+  return newDateArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
