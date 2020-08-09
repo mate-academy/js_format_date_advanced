c585e500657060586c34421d98a36120fda7f5b1
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-09 13:32:47 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,25 +1,53 @@
 'use strict';
 
 /**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
- *
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
  *
  * @returns {string}
+ * Lint: "Line exceeds the maximum comment line length of 80"
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const resultArr = [];
+  const dateArr = date.split(fromFormat[3]);
+  const yearIndex = fromFormat.indexOf('YYYY');
+  const yearIndexShort = fromFormat.indexOf('YY');
+  const monthIndex = fromFormat.indexOf('MM');
+  const dayIndex = fromFormat.indexOf('DD');
+
+  for (const key of toFormat) {
+    if (key === 'DD') {
+      resultArr.push(dateArr[dayIndex]);
+    }
+
+    if (key === 'MM') {
+      resultArr.push(dateArr[monthIndex]);
+    }
+
+    if (key === 'YYYY') {
+      if (dateArr[yearIndex]) {
+        resultArr.push(dateArr[yearIndex]);
+      } else {
+        if (+dateArr[yearIndexShort] > 20) {
+          resultArr.push(`19${dateArr[yearIndexShort]}`);
+        } else {
+          resultArr.push(`20${dateArr[yearIndexShort]}`);
+        }
+      }
+    }
+
+    if (key === 'YY') {
+      if (dateArr[yearIndexShort]) {
+        resultArr.push(dateArr[yearIndexShort]);
+      } else {
+        resultArr.push(dateArr[yearIndex].slice(2));
+      }
+    }
+  }
+
+  return resultArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
