7fb612d68ebbc22569d7904cd7487249ac7c2c83
nanccyy <lidiapriymak@gmail.com>
2020-08-09 23:06:47 +0800

    changed to objects

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,78 +1,57 @@
 'use strict';
 
 function formatDate(date, fromFormat, toFormat) {
-  const [, , , separator] = fromFormat;
-  const [, , , finalSeparator] = toFormat;
+  const [, , , sep] = fromFormat;
+  const [, , , finalSep] = toFormat;
 
-  const inputDate = date.split(separator);
+  const finalFormat = toFormat.slice(0, 3);
+  const initialFormat = fromFormat.slice(0, 3);
 
-  let yearFrom;
-  let monthFrom;
-  let dayFrom;
+  const fromPattern = {};
+  const toPattern = {};
+  const initialDate = date.split(sep);
 
-  let dayTo;
-  let monthTo;
-  let yearTo;
+  fromPattern.MM = initialDate[initialFormat.indexOf('MM')];
+  fromPattern.DD = initialDate[initialFormat.indexOf('DD')];
 
-  for (let i = 0; i < fromFormat.length; i++) {
-    switch (fromFormat[i]) {
-      case 'YYYY':
-        yearFrom = i;
-        break;
-
-      case 'YY':
-        yearFrom = i;
-        break;
-
-      case 'MM':
-        monthFrom = i;
-        break;
-
-      case 'DD':
-        dayFrom = i;
-        break;
-    }
-  }
-
-  for (let i = 0; i < toFormat.length; i++) {
-    switch (toFormat[i]) {
-      case 'YYYY':
-        yearTo = i;
-        break;
-
-      case 'YY':
-        yearTo = i;
-        break;
-
-      case 'MM':
-        monthTo = i;
-        break;
-
-      case 'DD':
-        dayTo = i;
-        break;
-    }
-  }
-
-  const finalDate = [];
-
-  if (fromFormat[yearFrom].length > toFormat[yearTo].length) {
-    inputDate[yearFrom] = inputDate[yearFrom].slice(2, 4);
-  }
-
-  if (toFormat[yearTo].length > fromFormat[yearFrom].length) {
-    if (+inputDate[yearFrom] > 22) {
-      inputDate[yearFrom] = '19' + inputDate[yearFrom];
+  if (initialFormat.includes('YYYY')) {
+    if (finalFormat.includes('YY')) {
+      fromPattern.YY = initialDate[initialFormat.indexOf('YYYY')].slice(2, 4);
     } else {
-      inputDate[yearFrom] = '20' + inputDate[yearFrom];
+      fromPattern.YY = initialDate[initialFormat.indexOf('YYYY')];
     }
   }
 
-  finalDate[yearTo] = inputDate[yearFrom];
-  finalDate[monthTo] = inputDate[monthFrom];
-  finalDate[dayTo] = inputDate[dayFrom];
+  if (initialFormat.includes('YY')) {
+    if (finalFormat.includes('YYYY')) {
+      if (+initialDate[initialFormat.indexOf('YY')] > 22) {
+        fromPattern.YY = '19' + initialDate[initialFormat.indexOf('YY')];
+      } else {
+        fromPattern.YY = '20' + initialDate[initialFormat.indexOf('YY')];
+      }
+    } else {
+      fromPattern.YY = initialDate[initialFormat.indexOf('YY')];
+    }
+  }
 
-  return finalDate.join(finalSeparator);
+  for (let pattern of finalFormat) {
+    if (pattern === 'YYYY') {
+      pattern = 'YY';
+    }
+    toPattern[pattern] = null;
+  }
+
+  for (const dateFrom in fromPattern) {
+    for (const dateTo in toPattern) {
+      if (dateFrom === dateTo) {
+        toPattern[dateTo] = fromPattern[dateFrom];
+      }
+    }
+  }
+
+  const finalDate = Object.values(toPattern).join(finalSep);
+
+  return finalDate;
 }
 
 module.exports = formatDate;
