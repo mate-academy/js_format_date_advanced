f8d586bf2eb05087da49b2797ae233d86ec04231
Alex <oivannikov>
2020-08-09 23:54:33 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,25 +1,24 @@
 'use strict';
 
-/**
- * Time flies, standards change. Let's get rid of the routine of changing the date format,
- * and create a function for formatting dates.
- * Create a `formatDate` function that accepts the `date` string, the old `fromFormat` array variable,
- * and the new `toFormat` array variable. Function returns given date in `toFormat` format.
- * 
- * Example:
- * formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/20'
- * formatDate('2021-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YY', '/']) // '18/02/21'
- * formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']) // '18.02.1997'
- *
- * @param {string} date
- * @param {string[]} fromFormat
- * @param {string[]} toFormat
- *
- * @returns {string}
- */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const arr = date.split(fromFormat[3]);
+  const year = arr[fromFormat.indexOf('YYYY')]
+    || (parseInt(arr[fromFormat.indexOf('YY')]) <= 19
+      ? '20' : '19')
+    + arr[fromFormat.indexOf('YY')];
+
+  const parsedDate = {
+    'DD': arr[fromFormat.indexOf('DD')],
+    'MM': arr[fromFormat.indexOf('MM')],
+    'YYYY': year,
+    'YY': year.slice(2),
+  };
+
+  return [
+    parsedDate[toFormat[0]],
+    parsedDate[toFormat[1]],
+    parsedDate[toFormat[2]],
+  ].join(toFormat[3]);
 }
 
 module.exports = formatDate;
