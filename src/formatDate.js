15e19c2f0442ddc7b47d16dae021cebdf0bd0be0
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-10 16:54:25 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,50 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const oldDelim = fromFormat[3];
+  const newDelim = toFormat[3];
+  const dateToArr = date.split(oldDelim);
+  const toFormatArray = [];
+  const day = dateToArr[fromFormat.indexOf('DD')];
+  const month = dateToArr[fromFormat.indexOf('MM')];
+  const year = (fromFormat.includes('YY'))
+    ? dateToArr[fromFormat.indexOf('YY')]
+    : dateToArr[fromFormat.indexOf('YYYY')];
+
+  for (let i = 0; i < dateToArr.length; i++) {
+    switch (toFormat[i]) {
+      case 'DD':
+        toFormatArray.push(day);
+        break;
+
+      case 'MM':
+        toFormatArray.push(month);
+        break;
+
+      case 'YY':
+        if (fromFormat.includes('YY')) {
+          toFormatArray.push(year);
+        } else {
+          toFormatArray.push(dateToArr[fromFormat.indexOf('YYYY')].slice(2, 4));
+        }
+        break;
+
+      case 'YYYY':
+        // if (dateToArr[fromFormat.indexOf('YYYY')] > -1) {
+        if (dateToArr[fromFormat.indexOf('YYYY')] > -1) {
+          toFormatArray.push(year);
+        } else {
+          if (dateToArr[fromFormat.indexOf('YY')] >= 30) {
+            toFormatArray.push('19' + year);
+          } else {
+            toFormatArray.push('20' + year);
+          }
+        }
+        break;
+    }
+  }
+
+  return toFormatArray.join(newDelim);
 }
 
 module.exports = formatDate;
