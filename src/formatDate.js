d76a03c485e7417ebba93345a6eaa694f02a7247
VoduDay <andriy.maniak@gmail.com>
2020-09-26 14:47:51 +0300

    formatedDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,39 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSepatator = fromFormat.pop();
+  const toSeparator = toFormat.pop();
+
+  const splitedDate = date.split(fromSepatator);
+
+  const dateToFormat = {};
+  const newFormatDate = [];
+
+  for (let formatPoint = 0; formatPoint < fromFormat.length; formatPoint++) {
+    dateToFormat[fromFormat[formatPoint][0]] = splitedDate[formatPoint];
+  }
+
+  const dateYearToFormat = dateToFormat['Y'];
+
+  for (const format of toFormat) {
+    if (format[0] === 'Y' && format.length !== dateYearToFormat.length) {
+      if (format.length > dateYearToFormat.length) {
+        if (dateYearToFormat >= 30) {
+          newFormatDate.push(19 + dateYearToFormat);
+        } else {
+          newFormatDate.push(20 + dateYearToFormat);
+        }
+      } else {
+        newFormatDate.push(dateYearToFormat.slice(2));
+      }
+    } else {
+      newFormatDate.push(dateToFormat[format[0]]);
+    }
+  }
+
+  const newFormatDateString = newFormatDate.join(toSeparator);
+
+  return newFormatDateString;
 }
 
 module.exports = formatDate;
