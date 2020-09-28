67a29ce2cf05a50f217fa773e3d51a62a33a16ee
Alina <alina90565@gmail.com>
2020-09-28 11:43:43 +0300

    fixed constants name

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,30 +51,30 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const splitedDate = date.split(fromFormat[3]);
-  const objDate = {
+  const mapDate = {
     [fromFormat[0]]: splitedDate[0],
     [fromFormat[1]]: splitedDate[1],
     [fromFormat[2]]: splitedDate[2],
   };
 
-  const arrOfNewDate = [];
+  const newDate = [];
 
   for (const typeDate of toFormat.slice(0, 3)) {
-    if (objDate.hasOwnProperty(typeDate)) {
-      arrOfNewDate.push(objDate[typeDate]);
+    if (mapDate.hasOwnProperty(typeDate)) {
+      newDate.push(mapDate[typeDate]);
       continue;
     }
 
     if (typeDate === 'YYYY') {
-      arrOfNewDate.push(
-        objDate.YY < 30 ? `20${objDate.YY}` : `19${objDate.YY}`
+      newDate.push(
+        mapDate.YY < 30 ? `20${mapDate.YY}` : `19${mapDate.YY}`
       );
     } else {
-      arrOfNewDate.push(objDate.YYYY.slice(-2));
+      newDate.push(mapDate.YYYY.slice(-2));
     }
   }
 
-  return arrOfNewDate.join(toFormat[3]);
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
