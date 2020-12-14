bd89b1ce9d1533c23480aed42c24c4f69f2936a2
Lora Levitska <larakad94@gmail.com>
2020-12-14 21:15:00 +0200

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,56 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const value = date.split(fromFormat[fromFormat.length - 1]);
+  let revers;
+
+  if (toFormat[0][0] !== fromFormat[0][0]) {
+    revers = value.reverse();
+  }
+
+  if (toFormat[0][0] === 'D' || toFormat[0][0] === 'M') {
+    if (revers !== undefined) {
+      if (fromFormat[0].length < toFormat[2].length) {
+        revers[2] < 30
+          ? revers[2] = '20' + revers[2]
+          : revers[2] = '19' + revers[2];
+      } else if (fromFormat[0].length > toFormat[2].length) {
+        revers[2] = revers[2].slice(2);
+      }
+    } else {
+      if (fromFormat[2].length < toFormat[2].length) {
+        value[2] < 30
+          ? value[2] = '20' + value[2]
+          : value[2] = '19' + value[2];
+      } else if (fromFormat[2].length > toFormat[2].length) {
+        value[2] = value[2].slice(2);
+      }
+    }
+  } else {
+    if (revers !== undefined) {
+      if (fromFormat[2].length < toFormat[0].length) {
+        revers[0] < 30
+          ? revers[0] = '20' + revers[0]
+          : revers[0] = '19' + revers[0];
+      } else if (fromFormat[0].length > toFormat[0].length) {
+        revers[0] = revers[0].slice(2);
+      }
+    } else {
+      if (fromFormat[0].length < toFormat[0].length) {
+        value[0] < 30
+          ? value[0] = '20' + value[0]
+          : value[0] = '19' + value[0];
+      } else if (fromFormat[0].length > toFormat[0].length) {
+        value[0] = value[0].slice(2);
+      }
+    }
+  }
+
+  if (revers !== undefined) {
+    return revers.join(toFormat[fromFormat.length - 1]);
+  } else {
+    return value.join(toFormat[fromFormat.length - 1]);
+  }
 }
 
 module.exports = formatDate;
