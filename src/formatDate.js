98332235a1556f7ad45eefbb8b0c23115ac5a304
Lora Levitska <larakad94@gmail.com>
2020-12-15 12:49:21 +0200

    optimized

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,55 +51,24 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const value = date.split(fromFormat[fromFormat.length - 1]);
-  let revers;
 
   if (toFormat[0][0] !== fromFormat[0][0]) {
-    revers = value.reverse();
+    value.reverse();
   }
 
-  if (toFormat[0][0] === 'D' || toFormat[0][0] === 'M') {
-    if (revers !== undefined) {
-      if (fromFormat[0].length < toFormat[2].length) {
-        revers[2] < 30
-          ? revers[2] = '20' + revers[2]
-          : revers[2] = '19' + revers[2];
-      } else if (fromFormat[0].length > toFormat[2].length) {
-        revers[2] = revers[2].slice(2);
-      }
-    } else {
-      if (fromFormat[2].length < toFormat[2].length) {
-        value[2] < 30
-          ? value[2] = '20' + value[2]
-          : value[2] = '19' + value[2];
-      } else if (fromFormat[2].length > toFormat[2].length) {
-        value[2] = value[2].slice(2);
-      }
-    }
-  } else {
-    if (revers !== undefined) {
-      if (fromFormat[2].length < toFormat[0].length) {
-        revers[0] < 30
-          ? revers[0] = '20' + revers[0]
-          : revers[0] = '19' + revers[0];
-      } else if (fromFormat[0].length > toFormat[0].length) {
-        revers[0] = revers[0].slice(2);
-      }
-    } else {
-      if (fromFormat[0].length < toFormat[0].length) {
-        value[0] < 30
-          ? value[0] = '20' + value[0]
-          : value[0] = '19' + value[0];
-      } else if (fromFormat[0].length > toFormat[0].length) {
-        value[0] = value[0].slice(2);
-      }
-    }
+  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    const index = toFormat.indexOf('YYYY');
+
+    value[index] < 30
+      ? value[index] = '20' + value[index]
+      : value[index] = '19' + value[index];
+  } else if (toFormat.includes('YY') && fromFormat.includes('YYYY')) {
+    const index = toFormat.indexOf('YY');
+
+    value[index] = value[index].slice(2);
   }
 
-  if (revers !== undefined) {
-    return revers.join(toFormat[fromFormat.length - 1]);
-  } else {
-    return value.join(toFormat[fromFormat.length - 1]);
-  }
+  return value.join(toFormat[fromFormat.length - 1]);
 }
 
 module.exports = formatDate;
