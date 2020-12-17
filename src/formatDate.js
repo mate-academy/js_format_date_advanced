8ba9fcba0f2b40f081bb8cbc52c5010832df9832
Viktor Morhun <victor.morgun16@gmail.com>
2020-12-17 14:53:18 +0200

    Revert "first solution"
    This reverts commit b66ba2a5a308f565dbca7eb4452c4e2e1d056132.

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,29 +1,7 @@
 'use strict';
 
 function formatDate(date, fromFormat, toFormat) {
-  const result = date.split(fromFormat[fromFormat.length - 1]);
-
-  if (toFormat[0][0] !== fromFormat[0][0]) {
-    result.reverse();
-  }
-
-  if (toFormat.includes('YY') && fromFormat.includes('YYYY')) {
-    result[toFormat.indexOf('YY')] = result[toFormat.indexOf('YY')].slice(2);
-
-    return result.join(toFormat[toFormat.length - 1]);
-  }
-
-  if (toFormat.includes('YYYY') && fromFormat.includes('YY')) {
-    (result[toFormat.indexOf('YYYY')] < 30)
-
-      ? result[toFormat.indexOf('YYYY')]
-        = '20' + result[toFormat.indexOf('YYYY')]
-
-      : result[toFormat.indexOf('YYYY')]
-        = '19' + result[toFormat.indexOf('YYYY')];
-  }
-
-  return result.join(toFormat[toFormat.length - 1]);
+  // write code here
 }
 
 module.exports = formatDate;
