28a296df513d5b16d8bd52885e19cfacab1d7535
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-02-17 11:57:01 +0200

    result

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,46 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const year = fromFormat.indexOf('YYYY');
+  const shortYear = fromFormat.indexOf('YY');
+  const month = fromFormat.indexOf('MM');
+  const day = fromFormat.indexOf('DD');
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const dateSplited = date.split(oldSeparator);
+  const dateResult = [];
+
+  for (const key of toFormat) {
+    if (key === 'DD') {
+      dateResult.push(dateSplited[day]);
+    }
+
+    if (key === 'MM') {
+      dateResult.push(dateSplited[month]);
+    }
+
+    if (key === 'YY') {
+      if (dateSplited[shortYear]) {
+        dateResult.push(dateSplited[shortYear]);
+      } else {
+        dateResult.push(dateSplited[year].slice(2));
+      }
+    }
+
+    if (key === 'YYYY') {
+      if (dateSplited[year]) {
+        dateResult.push(dateSplited[year]);
+      } else {
+        if (+dateSplited[shortYear] < 30) {
+          dateResult.push(`20${dateSplited[shortYear]}`);
+        } else {
+          dateResult.push(`19${dateSplited[shortYear]}`);
+        }
+      }
+    }
+  }
+
+  return dateResult.join(newSeparator);
 }
 
 module.exports = formatDate;
