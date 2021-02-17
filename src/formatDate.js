262e9e6aaf124cf50024a6cf7718b0a747dc06e6
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-02-18 01:15:48 +0200

    some minor fix)

--- src/formatDate.js
+++ src/formatDate.js
@@ -6,7 +6,7 @@
  * the old `fromFormat` array and the new `toFormat` array. Function returns
  * given date in new format.
  *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
+ * indexOfYearFrom from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
  *
@@ -51,40 +51,46 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
-  const year = fromFormat.indexOf('YYYY');
-  const shortYear = fromFormat.indexOf('YY');
-  const month = fromFormat.indexOf('MM');
-  const day = fromFormat.indexOf('DD');
   const oldSeparator = fromFormat[3];
   const newSeparator = toFormat[3];
   const dateSplited = date.split(oldSeparator);
   const dateResult = [];
 
-  for (const key of toFormat) {
-    if (key === 'DD') {
-      dateResult.push(dateSplited[day]);
+  const indexOfYearFrom = fromFormat.indexOf('YYYY');
+  const indexOfShortYearFrom = fromFormat.indexOf('YY');
+  const indexOfMonthFrom = fromFormat.indexOf('MM');
+  const indexOfDayFrom = fromFormat.indexOf('DD');
+
+  const indexOfYearTo = dateSplited[indexOfYearFrom];
+  const indexOfShortYearTo = dateSplited[indexOfShortYearFrom];
+  const indexOfMonthTo = dateSplited[indexOfMonthFrom];
+  const indexOfDayTo = dateSplited[indexOfDayFrom];
+
+  for (const element of toFormat) {
+    if (element === 'DD') {
+      dateResult.push(indexOfDayTo);
     }
 
-    if (key === 'MM') {
-      dateResult.push(dateSplited[month]);
+    if (element === 'MM') {
+      dateResult.push(indexOfMonthTo);
     }
 
-    if (key === 'YY') {
-      if (dateSplited[shortYear]) {
-        dateResult.push(dateSplited[shortYear]);
+    if (element === 'YY') {
+      if (indexOfShortYearTo) {
+        dateResult.push(indexOfShortYearTo);
       } else {
-        dateResult.push(dateSplited[year].slice(2));
+        dateResult.push(indexOfYearTo.slice(2));
       }
     }
 
-    if (key === 'YYYY') {
-      if (dateSplited[year]) {
-        dateResult.push(dateSplited[year]);
+    if (element === 'YYYY') {
+      if (indexOfYearTo) {
+        dateResult.push(indexOfYearTo);
       } else {
-        if (+dateSplited[shortYear] < 30) {
-          dateResult.push(`20${dateSplited[shortYear]}`);
+        if (+indexOfShortYearTo < 30) {
+          dateResult.push(`20${indexOfShortYearTo}`);
         } else {
-          dateResult.push(`19${dateSplited[shortYear]}`);
+          dateResult.push(`19${indexOfShortYearTo}`);
         }
       }
     }
