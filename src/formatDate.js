0160426c526e49f35fcb7ee2071fa59fe0737ddd
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-04-25 23:10:56 +0300

    ready for check

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,75 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const days = 'DD';
+  const month = 'MM';
+  const year2 = 'YY';
+  const year4 = 'YYYY';
+
+  const daysOld = fromFormat.indexOf(days);
+  const monthOld = fromFormat.indexOf(month);
+  const year2Old = fromFormat.indexOf(year2);
+  const year4Old = fromFormat.indexOf(year4);
+
+  const daysNew = toFormat.indexOf(days);
+  const monthNew = toFormat.indexOf(month);
+  const year2New = toFormat.indexOf(year2);
+  const year4New = toFormat.indexOf(year4);
+
+  let result = '';
+  const sheme = [];
+
+  // find date array
+
+  const dateArray = date.split(fromFormat[3]);
+
+  // dateArray[0] = fromFormat[0];
+  // dateArray[1] = fromFormat[1];
+  // dateArray[2] = fromFormat[2];
+
+  // make new date format
+
+  sheme[daysNew] = dateArray[daysOld];
+  sheme[monthNew] = dateArray[monthOld];
+
+  if (year2Old === -1) {
+    if (year2New === -1) {
+      sheme[year4New] = dateArray[year4Old];
+    }
+
+    if (year2New !== -1) {
+      sheme[year2New] = (dateArray[year4Old] / 100).toString().split('.')[1];
+      // 1234 --> 12.34 --> ( dateArray[year4Old] )
+    }
+  }
+
+  if (year4Old === -1) {
+    if (year4New === -1) {
+      sheme[year2New] = dateArray[year2Old];
+    }
+
+    if (year4New !== -1) {
+      if (+dateArray[year2Old] < +'30') { // '20/02/18'  2020
+        sheme[year4New] = +('20' + dateArray[year2Old]);
+      }
+
+      if (dateArray[year2Old] === '00') {
+        sheme[year4New] = +('20' + dateArray[year2Old]);
+      }
+
+      if (dateArray[year2Old] === '30') { // '30.02.18'  1930
+        sheme[year4New] = +('19' + dateArray[year2Old]);
+      }
+
+      if (+dateArray[year2Old] > +'30') { // '97/02/18', 1997
+        sheme[year4New] = +('19' + dateArray[year2Old]);
+      }
+    }
+  }
+
+  result = sheme.join(toFormat[3]);
+
+  return result;
 }
 
 module.exports = formatDate;
