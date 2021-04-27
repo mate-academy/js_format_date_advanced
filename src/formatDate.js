90bff9887b419add832539b970da8d7ae4e9630e
Igor Stasiv <igorstsv@gmail.com>
2021-04-27 19:42:50 +0300

    New solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,50 +50,54 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  function changeYearFormat(yearFormat, currentDate, index) {
-    if (yearFormat[index].length === 4) {
-      currentDate[index] = currentDate[index].substr(2);
-
-      return currentDate;
+  function changeYearFormat(year, requiredLength) {
+    if (year.length === requiredLength) {
+      return year;
     }
 
-    if (currentDate[index] < 30) {
-      currentDate[index] = '20' + currentDate[index];
-    } else {
-      currentDate[index] = '19' + currentDate[index];
+    if (requiredLength === 2) {
+      return year.slice(2);
     }
 
-    return currentDate;
+    return year < 30 ? '20' + year : '19' + year;
   }
 
-  const separator = toFormat[3];
   const dateSplited = date.split(fromFormat[3]);
+  const newDate = {};
 
-  if (fromFormat[0] === toFormat[0] && fromFormat[2] === toFormat[2]) {
-    return dateSplited.join(separator);
+  for (let i = 0; i < dateSplited.length; i++) {
+    switch (fromFormat[i]) {
+      case 'YY':
+      case 'YYYY':
+        newDate.year = dateSplited[i];
+        break;
+      case 'MM':
+        newDate.month = dateSplited[i];
+        break;
+      case 'DD':
+        newDate.day = dateSplited[i];
+        break;
+    }
   }
 
-  if (fromFormat[0] === toFormat[2] && fromFormat[2] === toFormat[0]) {
-    return dateSplited.reverse().join(separator);
+  for (let i = 0; i < dateSplited.length; i++) {
+    switch (toFormat[i]) {
+      case 'YY':
+        dateSplited[i] = changeYearFormat(newDate.year, 2);
+        break;
+      case 'YYYY':
+        dateSplited[i] = changeYearFormat(newDate.year, 4);
+        break;
+      case 'MM':
+        dateSplited[i] = newDate.month;
+        break;
+      case 'DD':
+        dateSplited[i] = newDate.day;
+        break;
+    }
   }
 
-  if (fromFormat[0] === toFormat[0] && fromFormat[2] !== toFormat[2]) {
-    const newDateFormat = changeYearFormat(fromFormat, dateSplited, 2);
-
-    return newDateFormat.join(separator);
-  }
-
-  if (fromFormat[0] !== toFormat[0] && fromFormat[2] === toFormat[2]) {
-    const newDateFormat = changeYearFormat(fromFormat, dateSplited, 0);
-
-    return newDateFormat.join(separator);
-  }
-
-  if (fromFormat[0] !== toFormat[2] && fromFormat[2] === toFormat[0]) {
-    const newDateFormat = changeYearFormat(fromFormat, dateSplited, 0);
-
-    return newDateFormat.reverse().join(separator);
-  }
+  return dateSplited.join(toFormat[3]);
 }
 
 module.exports = formatDate;
