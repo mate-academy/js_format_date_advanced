40e42615eda95ad63a5d5ae02bcccb31b9a719b0
Sergei <serheifil@gmail.com>
2021-06-30 13:16:06 +0300

    variables were added

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,8 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const parts = date.split(fromFormat[3]);
+  const separator = toFormat[3];
+  const yearsLimit = 30;
   const result = [];
 
   result[toFormat.indexOf('DD')] = parts.splice(fromFormat.indexOf('DD'), 1);
@@ -61,12 +63,12 @@ function formatDate(date, fromFormat, toFormat) {
   if (toFormat.includes('YY')) {
     result[toFormat.indexOf('YY')] = year;
 
-    return result.join(toFormat[3]);
+    return result.join(separator);
   }
 
-  result[toFormat.indexOf('YYYY')] = (+year < 30 ? '20' : '19') + year;
+  result[toFormat.indexOf('YYYY')] = (+year < yearsLimit ? '20' : '19') + year;
 
-  return result.join(toFormat[3]);
+  return result.join(separator);
 }
 
 module.exports = formatDate;
