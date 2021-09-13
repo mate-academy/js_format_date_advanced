10820340f21a15ea05572504e82e59f86071ec0a
Dina Samoilova <s13.dina@gmail.com>
2021-09-13 22:15:13 +0300

    Solution after review

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,31 +50,98 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const [, , , oldSeparator] = fromFormat;
-  const [, , , newSeparator] = toFormat;
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
   const arrDate = date.split(oldSeparator);
   const correctDateArr = [];
+  // version 2
+  // const oldFormat = {
+  //   [fromFormat[0]]: arrDate[0],
+  //   [fromFormat[1]]: arrDate[1],
+  //   [fromFormat[2]]: arrDate[2],
+  // };
+  const newFormat = {
+    [toFormat[0]]: 0,
+    [toFormat[1]]: 0,
+    [toFormat[2]]: 0,
+  };
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    for (let j = 0; j < fromFormat.length - 1; j++) {
-      if (toFormat[i] === fromFormat[j]) {
-        correctDateArr[i] = arrDate[j];
+  // version 3
+  //
+  for (const key in newFormat) {
+    for (let i = 0; i < fromFormat.length - 1; i++) {
+      if (key === fromFormat[i]) {
+        newFormat[key] = arrDate[i];
+        correctDateArr.push(newFormat[key]);
         break;
       } else {
-        if (toFormat[i] === 'YYYY' && fromFormat[j] === 'YY') {
-          if (arrDate[j] < 30) {
-            correctDateArr[i] = '20' + arrDate[j];
+        if (key === 'YYYY' && fromFormat[i] === 'YY') {
+          if (arrDate[i] < 30) {
+            correctDateArr.push('20' + arrDate[i]);
+            break;
           } else {
-            correctDateArr[i] = '19' + arrDate[j];
+            correctDateArr.push('19' + arrDate[i]);
+            break;
           }
         }
 
-        if (toFormat[i] === 'YY' && fromFormat[j] === 'YYYY') {
-          correctDateArr[i] = arrDate[j].slice(2);
+        if (key === 'YY' && fromFormat[i] === 'YYYY') {
+          correctDateArr.push(arrDate[i].slice(2));
+          break;
         }
       }
     }
   }
+
+  // version 3
+  //
+  // for (const key in newFormat) {
+  //   for (const oldKey in oldFormat) {
+  //     if (key === oldKey) {
+  //       newFormat[key] = oldFormat[oldKey];
+  //       correctDateArr.push(newFormat[key]);
+  //       break;
+  //     } else {
+  //       if (key === 'YYYY' && oldKey === 'YY') {
+  //         if (oldFormat[oldKey] < 30) {
+  //           correctDateArr.push('20' + oldFormat[oldKey]);
+  //           break;
+  //         } else {
+  //           correctDateArr.push('19' + oldFormat[oldKey]);
+  //           break;
+  //         }
+  //       }
+
+  //       if (key === 'YY' && oldKey === 'YYYY') {
+  //         correctDateArr.push(oldFormat[oldKey].slice(2));
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
+
+  // version 1
+  //
+  // for (let i = 0; i < toFormat.length - 1; i++) {
+  //   for (let j = 0; j < fromFormat.length - 1; j++) {
+  //     if (toFormat[i] === fromFormat[j]) {
+  //       correctDateArr[i] = arrDate[j];
+  //       break;
+  //     } else {
+  //       if (toFormat[i] === 'YYYY' && fromFormat[j] === 'YY') {
+  //         if (arrDate[j] < 30) {
+  //           correctDateArr[i] = '20' + arrDate[j];
+  //         } else {
+  //           correctDateArr[i] = '19' + arrDate[j];
+  //         }
+  //       }
+
+  //       if (toFormat[i] === 'YY' && fromFormat[j] === 'YYYY') {
+  //         correctDateArr[i] = arrDate[j].slice(2);
+  //       }
+  //     }
+  //   }
+  // }
 
   return correctDateArr.join(newSeparator);
 }
