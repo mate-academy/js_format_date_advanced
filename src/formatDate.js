4bac4ec55fba1d8cdc74bffca06bed98bff4373f
Yuriy Titov <yuranus@gmail.com>
2021-10-24 17:37:25 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,42 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const arrResult = [];
+
+  const getIndex = arr =>
+    arr.indexOf('YYYY') === -1
+      ? arr.indexOf('YY')
+      : arr.indexOf('YYYY');
+
+  const getValue = (from, to, realValue) => {
+    if (from.length === 2 && to.length === 4) {
+      return Number(realValue) < 30
+        ? `20${realValue}`
+        : `19${realValue}`;
+    }
+
+    if (from.length === 4 && to.length === 2) {
+      return realValue.slice(-2);
+    }
+
+    return realValue;
+  };
+
+  const splited = date.split(fromFormat[3]);
+
+  [...fromFormat]
+    .slice(0, 3)
+    .forEach((el, index) => {
+      if (toFormat.indexOf(el) === -1) {
+        const newIndex = getIndex(toFormat);
+
+        arrResult[newIndex] = getValue(el, toFormat[newIndex], splited[index]);
+      } else {
+        arrResult[toFormat.indexOf(el)] = splited[index];
+      }
+    });
+
+  return arrResult.join(toFormat[3]);
 }
 
 module.exports = formatDate;
