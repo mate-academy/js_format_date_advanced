220a4eb8931d83b8fab535364dd9064590e951f5
Daryna Dudar <keks9ra1286@gmail.com>
2021-11-14 14:28:16 +0200

    format date

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,47 +1,6 @@
 'use strict';
 
 /**
- *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
- *
- * Examples:
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['DD', 'MM', 'YYYY', '.'],
- * ) // '18.02.2020'
- *
- * formatDate(
- *   '18-02-2020',
- *   ['DD', 'MM', 'YYYY', '-'],
- *   ['DD', 'MM', 'YY', '/'],
- * ) // '18/02/20'
- *
- * formatDate(
- *   '20/02/18',
- *   ['YY', 'MM', 'DD', '/'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '97/02/18',
- *   ['YY', 'MM', 'DD', '/'],
- *   ['DD', 'MM', 'YYYY', '.'],
- * ) // '18.02.1997'
- *
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
@@ -50,7 +9,70 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const notFormattedDate = date.split(fromFormat[3]);
+  const newDate = [];
+
+  let yy, yyyy, month, day;
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    switch (fromFormat[i]) {
+      case 'YYYY': {
+        yyyy = notFormattedDate[i];
+        break;
+      }
+
+      case 'YY': {
+        yy = notFormattedDate[i];
+        break;
+      }
+
+      case 'MM': {
+        month = notFormattedDate[i];
+        break;
+      }
+
+      case 'DD': {
+        day = notFormattedDate[i];
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY': {
+        if (yy < 30) {
+          newDate.push(`20${yy}`);
+        } else if (yy >= 30) {
+          newDate.push(`19${yy}`);
+        } else {
+          newDate.push(yyyy);
+        }
+        break;
+      }
+
+      case 'YY': {
+        if (yy === undefined) {
+          newDate.push(yyyy.split('').slice(2).join(''));
+        } else {
+          newDate.push(yy);
+        }
+        break;
+      }
+
+      case 'MM': {
+        newDate.push(month);
+        break;
+      }
+
+      case 'DD': {
+        newDate.push(day);
+        break;
+      }
+    }
+  }
+
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
