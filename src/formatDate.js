649dd85678f00e6a26b912ad16ddc9183d0c0c6b
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-02 21:02:09 +0200

    FormatDate method was implemented

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,78 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateArray = date.split(fromFormat[fromFormat.length - 1]);
+  const separator = toFormat[toFormat.length - 1];
+  let year;
+  let month;
+  let day;
+  let receivedYearFormat;
+  let resultYearFormat;
+  let resultYearIndex;
+  let resultMonthIndex;
+  let resultDayIndex;
+
+  for (const elem of toFormat) {
+    if (elem === 'YY' || elem === 'YYYY') {
+      resultYearFormat = elem;
+
+      resultYearIndex = toFormat.indexOf(elem);
+    }
+
+    if (elem === 'MM') {
+      resultMonthIndex = toFormat.indexOf(elem);
+    }
+
+    if (elem === 'DD') {
+      resultDayIndex = toFormat.indexOf(elem);
+    }
+  }
+
+  for (const elem of fromFormat) {
+    if (elem === 'YY' || elem === 'YYYY') {
+      receivedYearFormat = elem;
+
+      year = dateArray[fromFormat.indexOf(elem)];
+    }
+
+    if (elem === 'MM') {
+      month = dateArray[fromFormat.indexOf(elem)];
+    }
+
+    if (elem === 'DD') {
+      day = dateArray[fromFormat.indexOf(elem)];
+    }
+  }
+
+  switch (resultYearFormat) {
+    case 'YYYY':
+      if (receivedYearFormat === 'YY') {
+        switch (parseInt(year) < 30) {
+          case true:
+            year = '20' + year;
+            break;
+
+          case false:
+            year = '19' + year;
+            break;
+        }
+      }
+      break;
+
+    case 'YY':
+      if (receivedYearFormat === 'YYYY') {
+        year = year.slice(2);
+      }
+      break;
+  }
+
+  dateArray[resultYearIndex] = year;
+  dateArray[resultMonthIndex] = month;
+  dateArray[resultDayIndex] = day;
+
+  const resultDate = dateArray.join(separator);
+
+  return resultDate;
 }
 
 module.exports = formatDate;
