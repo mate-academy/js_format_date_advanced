2218883ac4a047a0f7a2364c919926165115ba87
Inna <dragotaiv@gmail.com>
2022-03-23 23:24:36 +0200

    date

--- src/formatDate.js
+++ src/formatDate.js
@@ -2,10 +2,12 @@
 
 /**
  *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
+ * date format. Create a `formatDate` function that accepts the `date`
+ *  string,
  * the old `fromFormat` array and the new `toFormat` array. Function returns
  * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
+ *   The function can change a separator, reorder the date
+ *  parts of convert a
  * year from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
@@ -50,7 +52,28 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const dateNew = [];
 
+  const dateArray = date.split(fromFormat[3]);
+
+  for (let i = 0; i <= 2; i++) {
+    if (toFormat.indexOf(fromFormat[i]) !== -1) {
+      dateNew[toFormat.indexOf(fromFormat[i])] = dateArray[i];
+      continue;
+    }
+
+    if (dateArray[i].length > 2) {
+      dateNew[toFormat.indexOf('YY')] = dateArray[i].slice(2);
+      continue;
+    }
+
+    if (dateArray[i] < 30) {
+      dateNew[toFormat.indexOf('YYYY')] = '20' + dateArray[i];
+    } else {
+      dateNew[toFormat.indexOf('YYYY')] = '19' + dateArray[i];
+    }
+  }
+
+  return dateNew.join(toFormat[3]);
+}
 module.exports = formatDate;
