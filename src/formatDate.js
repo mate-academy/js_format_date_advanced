c4f23ed021c6d09b0a35956ea1aaaa8593af7fd3
Artem <zuller123@gmail.com>
2022-04-07 20:36:50 +0300

    solution2

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,16 +1,8 @@
 'use strict';
 
 /**
- *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
  *
- * Examples:
+ *  * Examples:
  *
  * formatDate(
  *   '2020-02-18',
@@ -42,6 +34,15 @@
  *   ['DD', 'MM', 'YYYY', '.'],
  * ) // '18.02.1997'
  *
+ *   Time flies, standards change. Let's get rid of the routine of changing the
+ * date format. Create a `formatDate` function that accepts the `date` string,
+ * the old `fromFormat` array and the new `toFormat` array. Function returns
+ * given date in new format.
+ *   The function can change a separator, reorder the date parts of convert a
+ * year from 4 digits to 2 digits and back.
+ *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
+ *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
+ *
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
@@ -50,7 +51,30 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateFix = [];
+  const dateBefore = date.split(fromFormat[3]);
+  const longYear = dateBefore[fromFormat.indexOf('YYYY')];
+  const shortYear = dateBefore[fromFormat.indexOf('YY')];
+  const century = 30;
+
+  if (longYear && toFormat.includes('YY')) {
+    dateFix[toFormat.indexOf('YY')] = longYear.slice(2);
+  }
+
+  if (toFormat.includes('YYYY')) {
+    dateFix[toFormat.indexOf('YYYY')] = shortYear >= century
+      ? '19' + shortYear : '20' + shortYear;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    const index = toFormat.indexOf(fromFormat[i]);
+
+    if (index !== -1) {
+      dateFix[index] = dateBefore[i];
+    }
+  }
+
+  return dateFix.join(toFormat[3]);
 }
 
 module.exports = formatDate;
