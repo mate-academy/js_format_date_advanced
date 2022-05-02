0f0e1222a6568d983812eb5cd896b3ee88ef4625
Swazi Kunene <kuneneswazi18@gmail.com>
2022-05-02 10:49:01 +0200

    used object instead

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,61 +50,46 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const dateArray = date.split(fromFormat[fromFormat.length - 1]);
-  const resultArray = Array(3);
-  let month, day, year;
+  const fromSeperator = fromFormat[fromFormat.length - 1];
+  const toSeperator = toFormat[toFormat.length - 1];
+  const dateArray = date.split(fromSeperator);
+  const resultArray = [];
 
-  for (let i = 0; i < fromFormat.length - 1; i++) {
-    switch (fromFormat[i]) {
-      case 'YY':
-        if (+dateArray[i] < 30) {
-          year = Number('20' + dateArray[i]);
+  const dateObj = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateObj[fromFormat[i]] = dateArray[i];
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const value = toFormat[i];
+
+    switch (true) {
+      case (value === 'MM' || value === 'DD'):
+        resultArray.push(dateObj[value]);
+        break;
+
+      case ((value === 'YY' || value === 'YYYY') && value in dateObj):
+        resultArray.push(dateObj[value]);
+        break;
+
+      case (value === 'YY' && 'YYYY' in dateObj):
+        resultArray.push((dateObj.YYYY).slice(2));
+
+        break;
+
+      case (value === 'YYYY' && 'YY' in dateObj):
+        if (dateObj.YY < 30) {
+          resultArray.push('20' + dateObj.YY);
         } else {
-          year = Number('19' + dateArray[i]);
+          resultArray.push('19' + dateObj.YY);
         }
-        break;
 
-      case 'YYYY':
-        year = dateArray[i];
         break;
-
-      case 'MM':
-        month = dateArray[i];
-        break;
-
-      case 'DD':
-        day = dateArray[i];
-        break;
-
-      default:
-        throw new Error('invalid data');
     }
   }
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    switch (toFormat[i]) {
-      case 'YY':
-        resultArray[i] = year.slice(2);
-        break;
-
-      case 'YYYY':
-        resultArray[i] = year;
-        break;
-
-      case 'MM':
-        resultArray[i] = month;
-        break;
-
-      case 'DD':
-        resultArray[i] = day;
-        break;
-
-      default:
-        throw new Error('invalid data');
-    }
-  }
-
-  return resultArray.join(toFormat[toFormat.length - 1]);
+  return resultArray.join(toSeperator);
 }
 
 module.exports = formatDate;
