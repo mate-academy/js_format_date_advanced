4a8249850c1cea05458a72d2221c1a9dfa9bb2dd
Oles Partyka <oles.partyka.27@gmail.com>
2022-05-13 08:47:34 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,30 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const result = [];
+  const dataObj = {};
+  const dataArr = date.split(fromFormat[fromFormat.length - 1]);
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    if (fromFormat[i] === 'YY') {
+      if (Number(dataArr[i]) > 22) {
+        dataObj.YYYY = 19 + dataArr[i];
+      } else {
+        dataObj.YYYY = 20 + dataArr[i];
+      }
+    }
+
+    if (fromFormat[i] === 'YYYY') {
+      dataObj.YY = dataArr[i].slice(-2);
+    }
+    dataObj[fromFormat[i]] = dataArr[i];
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    result[i] = dataObj[toFormat[i]];
+  }
+
+  return result.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
