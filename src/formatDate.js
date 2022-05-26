0480bf20d17dcc8361c6583a69a60cf299903d06
Dmytro Lebedchenko <103206316+dmytro-lebedchenko@users.noreply.github.com>
2022-05-26 16:03:24 +0300

    Update formatDate.js

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,17 +8,16 @@ function formatDate(date, fromFormat, toFormat) {
 
   for (let i = 0; i < 3; i++) {
     dateCollection[fromFormat[i]] = fromDate[i];
+    toDate[toFormat.indexOf(fromFormat[i])] = fromDate[i];
+  }
 
-    for (const key in dateCollection) {
-      if (key) {
-        toDate[toFormat.indexOf(fromFormat[i])] = fromDate[i];
-      }
-
-      if (key.includes('YY') && dateCollection[key].length > 2) {
+  for (const key in dateCollection) {
+    if (key.includes('YY')) {
+      if (dateCollection[key].length > 2) {
         toDate[toFormat.indexOf('YY')] = dateCollection[key].slice(2);
-      } else if (key.includes('YY') && dateCollection[key] >= 30) {
+      } else if (dateCollection[key] >= 30) {
         toDate[toFormat.indexOf('YYYY')] = `19${dateCollection[key]}`;
-      } else if (key.includes('YY') && dateCollection[key] < 30) {
+      } else {
         toDate[toFormat.indexOf('YYYY')] = `20${dateCollection[key]}`;
       }
     }
