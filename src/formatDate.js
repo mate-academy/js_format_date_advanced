91a39ededcf9d0cce36d5ccc000bd8a6b79d48d8
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-09 14:50:44 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -48,9 +48,40 @@
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const inputDate = {};
+  const orderedArr = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    const type = fromFormat[i][0].toLocaleLowerCase();
+    const value = date.split(fromFormat[3])[i];
+
+    inputDate[type] = value;
+
+    if (type === 'y') {
+      inputDate.yearFormat = fromFormat[i].length;
+    }
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    const type = toFormat[i][0].toLocaleLowerCase();
+
+    if (type === 'y') {
+      const toYearFormat = toFormat[i].length;
+
+      if (toYearFormat < inputDate.yearFormat) {
+        inputDate.y = inputDate.y.slice(2, 4);
+      } else if (toYearFormat > inputDate.yearFormat) {
+        const century = (+inputDate.y < 30) ? '20' : '19';
+
+        inputDate.y = century + inputDate.y;
+      }
+    }
+
+    orderedArr.push(inputDate[type]);
+  }
+
+  return orderedArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
