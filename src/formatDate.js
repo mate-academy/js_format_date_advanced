d7766dee4d597f537e03170261cf193c2d96f04f
Stas Yasinchyk <grodh06@gmail.com>
2022-06-13 12:02:59 +0300

    solved

--- src/formatDate.js
+++ src/formatDate.js
@@ -48,9 +48,46 @@
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const newDate = [];
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const divideDate = date.split(oldSeparator);
+  const dateMap = {};
+
+  for (let i = 0; i < divideDate.length; i++) {
+    dateMap[fromFormat[i]] = divideDate[i];
+  }
+
+  if (!dateMap['YY']) {
+    dateMap['YY'] = dateMap['YYYY'].slice(-2);
+  }
+
+  if (!dateMap['YYYY']) {
+    dateMap['YYYY'] = +dateMap['YY'] >= 30 ? `19${dateMap['YY']}` : `20${dateMap['YY']}`;
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    switch (toFormat[i]) {
+      case 'DD':
+        newDate[i] = dateMap['DD'];
+        break;
+
+      case 'MM':
+        newDate[i] = dateMap['MM'];
+        break;
+
+      case 'YY':
+        newDate[i] = dateMap['YY'];
+        break;
+
+      default:
+        newDate[i] = dateMap['YYYY'];
+        break;
+    }
+  }
+
+  return newDate.join(newSeparator);
 }
 
 module.exports = formatDate;
