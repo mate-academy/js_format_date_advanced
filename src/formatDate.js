67f398d94d635784147882bae3b81ad4fd716bf9
Oleg Vetrov <oleg94038@gmail.com>
2022-07-06 20:31:50 +0300

    Added formatDate function

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,45 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const yearFull = date.split(fromFormat[3])[fromFormat.indexOf('YYYY')];
+  let year = date.split(fromFormat[3])[fromFormat.indexOf('YY')];
+  const month = date.split(fromFormat[3])[fromFormat.indexOf('MM')];
+  const day = date.split(fromFormat[3])[fromFormat.indexOf('DD')];
+  const formattedDate = [];
+
+  if (yearFull !== undefined) {
+    year = yearFull;
+  }
+
+  if (year !== undefined && year.length === 2) {
+    if (year >= 30) {
+      year = '19' + year;
+    } else {
+      year = '20' + year;
+    }
+  }
+
+  for (let i = 0; i <= toFormat.length - 2; i++) {
+    switch (toFormat[i]) {
+      case 'DD':
+        formattedDate.push(day);
+        break;
+
+      case 'MM':
+        formattedDate.push(month);
+        break;
+
+      case 'YYYY':
+        formattedDate.push(year);
+        break;
+
+      case 'YY':
+        formattedDate.push(year.substring(2, 4));
+        break;
+    }
+  }
+
+  return formattedDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
