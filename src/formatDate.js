32266f89d190dbe46aeb993e96e20aec80eab836
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-07-15 18:58:43 +0300

    Solution after 1st review

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,26 +50,29 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const fromFormatArray = date.split(fromFormat[fromFormat.length - 1]);
-  const toFormatArray = [];
+  const dateObject = {};
+  const dateArray = date.split(fromFormat[fromFormat.length - 1]);
+  const resultDate = [];
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    for (let j = 0; j < toFormat.length - 1; j++) {
-      if (fromFormat[i] === toFormat[j]) {
-        toFormatArray[j] = fromFormatArray[i];
-      } else if (fromFormat[i].includes('Y') && toFormat[j].includes('Y')) {
-        if (fromFormat[i].length > toFormat[j].length) {
-          toFormatArray[j] = fromFormatArray[i].slice(toFormat[j].length);
-        } else if (+fromFormatArray[i] < 30) {
-          toFormatArray[j] = '20' + fromFormatArray[i];
-        } else {
-          toFormatArray[j] = '19' + fromFormatArray[i];
-        }
-      }
-    }
+    dateObject[fromFormat[i]] = dateArray[i];
   }
 
-  return toFormatArray.join(toFormat[toFormat.length - 1]);
+  if ('YYYY' in dateObject) {
+    dateObject.YY = dateObject.YYYY.slice(2);
+  }
+
+  if ('YY' in dateObject) {
+    dateObject.YYYY = dateObject.YY < 30
+      ? '20' + dateObject.YY
+      : '19' + dateObject.YY;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    resultDate.push(dateObject[toFormat[i]]);
+  }
+
+  return resultDate.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
