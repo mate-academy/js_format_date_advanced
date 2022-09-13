a9ffa3fe6d73fd59a31d5dea7ee0bffb530c46dc
grafit-off <graf.off.it@gmail.com>
2022-09-13 16:15:24 +0300

    feat: date formatter interpretation

--- src/formatDate.js
+++ src/formatDate.js
@@ -48,9 +48,38 @@
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+
+  const oldDateArr = date.split(oldSeparator);
+  const newDateArr = new Array(3);
+
+  for (let i = 0; i < newDateArr.length; i++) {
+    let oldDateItem = oldDateArr[i];
+    const oldFormatItem = fromFormat[i];
+    let newFormatIndex = toFormat.indexOf(oldFormatItem);
+
+    if (newFormatIndex < 0) {
+      newFormatIndex = oldFormatItem === 'YY' ? toFormat.indexOf('YYYY')
+        : toFormat.indexOf('YY');
+
+      oldDateItem = formatYearToAlternative(oldDateItem);
+    }
+
+    newDateArr[newFormatIndex] = oldDateItem;
+  }
+
+  return newDateArr.join(newSeparator);
+}
+
+function formatYearToAlternative(year) {
+  if (year.length !== 2) {
+    return year.slice(-2);
+  }
+
+  return year < 30 ? 20 + year : 19 + year;
 }
 
 module.exports = formatDate;
