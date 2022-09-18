e1d64864768000c741bc7eed490e761f88271d1b
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-09-18 13:07:40 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -11,7 +11,7 @@
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
  *
  * Examples:
- *
+ * 2020-02-18 [YYYY]
  * formatDate(
  *   '2020-02-18',
  *   ['YYYY', 'MM', 'DD', '-'],
@@ -51,6 +51,53 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const newDate = [];
+  const splitDate = date.split(fromFormat[3]);
+  let newDay = '';
+  let newMonth = '';
+  let newYear = '';
+
+  for (let i = 0; i < 3; i++) {
+    if (fromFormat[i] === 'DD') {
+      newDay = splitDate[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      newMonth = splitDate[i];
+    }
+
+    if (fromFormat[i] === 'YYYY') {
+      newYear = splitDate[i];
+    }
+
+    if (fromFormat[i] === 'YY' && +splitDate[i] < 30) {
+      newYear = '20' + splitDate[i];
+    }
+
+    if (fromFormat[i] === 'YY' && +splitDate[i] >= 30) {
+      newYear = '19' + splitDate[i];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i] === 'DD') {
+      newDate[i] = newDay;
+    }
+
+    if (toFormat[i] === 'MM') {
+      newDate[i] = newMonth;
+    }
+
+    if (toFormat[i] === 'YYYY') {
+      newDate[i] = newYear;
+    }
+
+    if (toFormat[i] === 'YY') {
+      newDate[i] = newYear.slice(-2);
+    }
+  }
+
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
