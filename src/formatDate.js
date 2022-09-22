0aa075f23cde9b446e2973b0bfceb9b6539e461f
RomaVarkov <varkovroma@gmail.com>
2022-09-22 12:44:37 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -3,8 +3,8 @@
 /**
  *   Time flies, standards change. Let's get rid of the routine of changing the
  * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
+ * the old `fromFormat` outputDateay and the new `toFormat` outputDateay.
+ * Function returnsgiven date in new format.
  *   The function can change a separator, reorder the date parts of convert a
  * year from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
@@ -50,53 +50,53 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const obj = {};
-  const arr = [];
-  const dateToArr = date.split(fromFormat[3]);
+  const format = {};
+  const outputDate = [];
+  const dateTooutputDate = date.split(fromFormat[3]);
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
     if (fromFormat[i] === 'DD') {
-      obj.day = dateToArr[i];
+      format.day = dateTooutputDate[i];
     }
 
     if (fromFormat[i] === 'MM') {
-      obj.month = dateToArr[i];
+      format.month = dateTooutputDate[i];
     }
 
     if (fromFormat[i] === 'YY') {
-      obj.year = dateToArr[i];
+      format.year = dateTooutputDate[i];
     }
 
     if (fromFormat[i] === 'YYYY') {
-      obj.year = dateToArr[i].slice(-2);
+      format.year = dateTooutputDate[i].slice(-2);
     }
   }
 
   for (let i = 0; i < toFormat.length - 1; i++) {
     if (toFormat[i] === 'DD') {
-      arr.push(obj.day);
+      outputDate.push(format.day);
     }
 
     if (toFormat[i] === 'MM') {
-      arr.push(obj.month);
+      outputDate.push(format.month);
     }
 
     if (toFormat[i] === 'YY') {
-      arr.push(obj.year);
+      outputDate.push(format.year);
     }
 
     if (toFormat[i] === 'YYYY') {
-      if (obj.year < 30) {
-        obj.year = '20' + obj.year;
+      if (format.year < 30) {
+        format.year = '20' + format.year;
       } else {
-        obj.year = '19' + obj.year;
+        format.year = '19' + format.year;
       }
 
-      arr.push(obj.year);
+      outputDate.push(format.year);
     }
   }
 
-  return arr.join(toFormat[3]);
+  return outputDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
