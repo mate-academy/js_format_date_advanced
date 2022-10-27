a292ae543fa2de033bb0877b3d304e0535d7b9d0
mitattt <garkavivii@gmail.com>
2022-10-27 20:51:12 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,14 +1,7 @@
 'use strict';
 
 /**
- *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
+
  *
  * Examples:
  *
@@ -48,9 +41,42 @@
  *
  * @returns {string}
  */
+//  The function can change a separator, reorder the date parts of convert a
+//  * year from 4 digits to 2 digits and back.
+//  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
+//  *   When converting from
+// *   Time flies, standards change. Let's get rid of the routine of changing
+//      the
+// * date format. Create a `formatDate` function that accepts the `date` string,
+// * the old `fromFormat` array and the new `toFormat` array. Function returns
+// * given date in new format.
+// *   YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const newDate = date.split(fromFormat[3]);
+  const day = newDate[fromFormat.indexOf('DD')];
+  const month = newDate[fromFormat.indexOf('MM')];
+  const year = newDate[fromFormat.indexOf('YYYY')]
+  || newDate[fromFormat.indexOf('YY')];
+
+  const newFormat = [...toFormat];
+  const separator = newFormat[3];
+  const century = (year < 30) ? 20 : 19;
+
+  newFormat[newFormat.indexOf('DD')] = day;
+  newFormat[newFormat.indexOf('MM')] = month;
+
+  newFormat[newFormat.indexOf('YYYY')] = (year.length === 4)
+    ? year
+    : century + year;
+
+  newFormat[newFormat.indexOf('YY')] = (year.length === 2)
+    ? year
+    : year.slice(2);
+
+  const finalDate = newFormat.splice(0, 3).join(separator);
+
+  return finalDate;
 }
 
 module.exports = formatDate;
