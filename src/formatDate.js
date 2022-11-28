22b2b17b6402cb887698dd6c2a90af8997e78861
Sergii Mishchenko <serge.mishchenko@gmail.com>
2022-11-28 17:26:41 +0300

    Format Date Advanced Added

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,47 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  // if (fromFormat[0] === 'YYYY' && toFormat[0] === 'YYYY') {
+  //   return date.split(fromFormat[3]).join(toFormat[3]);
+  // }
+
+  // if (fromFormat[0] === 'YYYY' && toFormat[0] === 'DD') {
+  //   return date.split(fromFormat[3]).reverse().join(toFormat[3]);
+  // }
+
+  const [data1, data2, data3] = date.split(fromFormat[3]);
+
+  fromFormat[0] = `${fromFormat[0]}` + `${data1}`;
+  fromFormat[1] = `${fromFormat[1]}` + `${data2}`;
+  fromFormat[2] = `${fromFormat[2]}` + `${data3}`;
+
+  for (let i = 0; i < 3; i++) {
+    for (let y = 0; y < 3; y++) {
+      if (toFormat[i] === 'YYYY' && fromFormat[y].length === 4) {
+        if (Number(fromFormat[y].slice(2)) < 30) {
+          toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `20` + `${fromFormat[y].slice(2)}`;
+        } else {
+          toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `19` + `${fromFormat[y].slice(2)}`;
+        }
+      } else if (toFormat[i] === 'YY' && fromFormat[y].length === 8) {
+        toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `${fromFormat[y].slice(6)}`;
+      } else {
+        if (toFormat[i].slice(0, 1) === fromFormat[y].slice(0, 1)) {
+          toFormat[i] = fromFormat[y];
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i].length > 6) {
+      toFormat[i] = toFormat[i].slice(4);
+    } else {
+      toFormat[i] = toFormat[i].slice(2);
+    }
+  }
+
+  return `${toFormat[0]}${toFormat[3]}${toFormat[1]}${toFormat[3]}${toFormat[2]}`;
 }
 
 module.exports = formatDate;
