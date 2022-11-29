d3015d4a375c9940b1434a2a054feda07c6860d8
Denis Bokov <denisbokov1703@gmail.com>
2022-11-29 20:57:41 +0100

    Format date ADV

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,65 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  let oldSeparator = '';
+  let newSeparator = '';
+  let oldYearIndex = '';
+  let oldYearLength = '';
+  let oldMonthIndex = '';
+  let oldDayIndex = '';
+  const newDate = [];
+
+  for (const itemFrom of fromFormat) {
+    if (itemFrom.includes('Y')) {
+      oldYearIndex = fromFormat.indexOf(itemFrom);
+      oldYearLength = itemFrom.length;
+    }
+
+    if (itemFrom.includes('M')) {
+      oldMonthIndex = fromFormat.indexOf(itemFrom);
+    }
+
+    if (itemFrom.includes('D')) {
+      oldDayIndex = fromFormat.indexOf(itemFrom);
+    }
+
+    if (!(itemFrom.toLowerCase() !== itemFrom.toUpperCase())) {
+      oldSeparator = itemFrom;
+    }
+  }
+
+  const oldDateArray = date.split(`${oldSeparator}`);
+  const shortYear = oldDateArray[oldYearIndex].slice(-2);
+  const longYear = (shortYear < 30) ? `20${shortYear}` : `19${shortYear}`;
+
+  for (const itemTo of toFormat) {
+    if (itemTo.includes('D')) {
+      newDate[toFormat.indexOf(itemTo)] = oldDateArray[oldDayIndex];
+    }
+
+    if (itemTo.includes('M')) {
+      newDate[toFormat.indexOf(itemTo)] = oldDateArray[oldMonthIndex];
+    }
+
+    if (itemTo.includes('Y')) {
+      switch (oldYearLength) {
+        case '2':
+          newDate[toFormat.indexOf(itemTo)]
+            = (itemTo.length === 2) ? shortYear : longYear;
+          break;
+
+        default:
+          newDate[toFormat.indexOf(itemTo)]
+            = (itemTo.length === 4) ? longYear : shortYear;
+      }
+    }
+
+    if (!(itemTo.toLowerCase() !== itemTo.toUpperCase())) {
+      newSeparator = itemTo;
+    }
+  }
+
+  return `${newDate.join(newSeparator)}`;
 }
 
 module.exports = formatDate;
