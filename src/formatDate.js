b3ddd4ca537e1345713f9bb8cd2906856f90fbb1
Sergii Mishchenko <serge.mishchenko@gmail.com>
2022-11-29 13:06:04 +0300

    Bug fixed

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,47 +50,42 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // if (fromFormat[0] === 'YYYY' && toFormat[0] === 'YYYY') {
-  //   return date.split(fromFormat[3]).join(toFormat[3]);
-  // }
+  const arrOld = [...fromFormat];
+  const arrNew = [...toFormat];
 
-  // if (fromFormat[0] === 'YYYY' && toFormat[0] === 'DD') {
-  //   return date.split(fromFormat[3]).reverse().join(toFormat[3]);
-  // }
+  const [data1, data2, data3] = date.split(arrOld[3]);
 
-  const [data1, data2, data3] = date.split(fromFormat[3]);
-
-  fromFormat[0] = `${fromFormat[0]}` + `${data1}`;
-  fromFormat[1] = `${fromFormat[1]}` + `${data2}`;
-  fromFormat[2] = `${fromFormat[2]}` + `${data3}`;
+  arrOld[0] = `${arrOld[0]}` + `${data1}`;
+  arrOld[1] = `${arrOld[1]}` + `${data2}`;
+  arrOld[2] = `${arrOld[2]}` + `${data3}`;
 
   for (let i = 0; i < 3; i++) {
     for (let y = 0; y < 3; y++) {
-      if (toFormat[i] === 'YYYY' && fromFormat[y].length === 4) {
-        if (Number(fromFormat[y].slice(2)) < 30) {
-          toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `20` + `${fromFormat[y].slice(2)}`;
+      if (arrNew[i] === 'YYYY' && arrOld[y].length === 4) {
+        if (Number(arrOld[y].slice(2)) < 30) {
+          arrNew[i] = `${arrOld[y].slice(0, 2)}` + `20` + `${arrOld[y].slice(2)}`;
         } else {
-          toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `19` + `${fromFormat[y].slice(2)}`;
+          arrNew[i] = `${arrOld[y].slice(0, 2)}` + `19` + `${arrOld[y].slice(2)}`;
         }
-      } else if (toFormat[i] === 'YY' && fromFormat[y].length === 8) {
-        toFormat[i] = `${fromFormat[y].slice(0, 2)}` + `${fromFormat[y].slice(6)}`;
+      } else if (arrNew[i] === 'YY' && arrOld[y].length === 8) {
+        arrNew[i] = `${arrOld[y].slice(0, 2)}` + `${arrOld[y].slice(6)}`;
       } else {
-        if (toFormat[i].slice(0, 1) === fromFormat[y].slice(0, 1)) {
-          toFormat[i] = fromFormat[y];
+        if (arrNew[i].slice(0, 1) === arrOld[y].slice(0, 1)) {
+          arrNew[i] = arrOld[y];
         }
       }
     }
   }
 
   for (let i = 0; i < 3; i++) {
-    if (toFormat[i].length > 6) {
-      toFormat[i] = toFormat[i].slice(4);
+    if (arrNew[i].length > 6) {
+      arrNew[i] = arrNew[i].slice(4);
     } else {
-      toFormat[i] = toFormat[i].slice(2);
+      arrNew[i] = arrNew[i].slice(2);
     }
   }
 
-  return `${toFormat[0]}${toFormat[3]}${toFormat[1]}${toFormat[3]}${toFormat[2]}`;
+  return `${arrNew[0]}${arrNew[3]}${arrNew[1]}${arrNew[3]}${arrNew[2]}`;
 }
 
 module.exports = formatDate;
