43831544286d3c2fa4c81a55309420485543d1c7
Maksym Levchenko <llllewvvaa@gmail.com>
2022-11-30 11:04:49 +0200

    result

--- src/formatDate.js
+++ src/formatDate.js
@@ -49,7 +49,7 @@
  * @returns {string}
  */
 
- function formatDate(date, fromFormat, toFormat) {
+function formatDate(date, fromFormat, toFormat) {
   const fullFormatDate = {};
   const newDate = [];
   const oldDate = date.split(fromFormat[3]);
