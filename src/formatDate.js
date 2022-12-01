2433909eea5bf20b537b25dcca3772aa58c576b4
Oleh Yablunovskyi <lifserv@gmail.com>
2022-12-01 20:38:39 +0200

    Requested changes and some other fixes were made

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,30 +51,36 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const splittedDate = date.split(fromFormat[3]);
-  const mapObject = {};
+  const shortYear = 'YY';
+  const longYear = 'YYYY';
+  const dateMap = {};
   const toFormatShort = [];
 
   for (let i = 0; i < 3; i++) {
-    mapObject[fromFormat[i].slice(0, 1)] = splittedDate[i];
+    dateMap[fromFormat[i].slice(0, 1)] = splittedDate[i];
     toFormatShort.push(toFormat[i].slice(0, 1));
   }
 
-  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
-    mapObject['Y'] = mapObject['Y'].slice(2, 4);
+  if (fromFormat.includes(longYear) && toFormat.includes(shortYear)) {
+    dateMap['Y'] = dateMap['Y'].slice(2, 4);
   }
 
-  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
-    if (mapObject['Y'] < 30) {
-      mapObject['Y'] = '20' + mapObject['Y'];
-    } else {
-      mapObject['Y'] = '19' + mapObject['Y'];
+  if (fromFormat.includes(shortYear) && toFormat.includes(longYear)) {
+    switch (dateMap['Y'] < 30) {
+      case (true):
+        dateMap['Y'] = '20' + dateMap['Y'];
+        break;
+
+      case (false):
+        dateMap['Y'] = '19' + dateMap['Y'];
+        break;
     }
   }
 
   const newDate = [];
 
   for (const item of toFormatShort) {
-    newDate.push(mapObject[item]);
+    newDate.push(dateMap[item]);
   }
 
   return newDate.join(toFormat[3]);
