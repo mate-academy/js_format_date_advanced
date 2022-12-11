4c96c5085010b8ec823c1fbf702ce311590e9bf0
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2022-12-11 13:58:34 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,6 +50,60 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
+  let day;
+  let month;
+  let year;
+
+  const dateWithDots = date.split(fromFormat[3]);
+  const result = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    switch (true) {
+      case fromFormat[i] === 'DD':
+        day = dateWithDots[i];
+        break;
+
+      case fromFormat[i] === 'MM':
+        month = dateWithDots[i];
+        break;
+
+      case fromFormat[i] === 'YY' || fromFormat[i] === 'YYYY':
+        year = dateWithDots[i];
+        break;
+    }
+  }
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i] === 'YY') {
+      if (year.length === 2) {
+        result.push(year);
+      } else {
+        result.push(year[2] + year[3]);
+      }
+    }
+
+    if (toFormat[i] === 'MM') {
+      result.push(month);
+    }
+
+    if (toFormat[i] === 'DD') {
+      result.push(day);
+    }
+
+    if (toFormat[i] === 'YYYY') {
+      if (year.length === 4) {
+        result.push(year);
+      }
+
+      if (year.length === 2 && +year < 30) {
+        result.push('20' + `${year}`);
+      } else if (year.length === 2 && +year >= 30) {
+        result.push('19' + `${year}`);
+      }
+    }
+  }
+
+  return result.join(toFormat[3]);
   // write code here
 }
 
