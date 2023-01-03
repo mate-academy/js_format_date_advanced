61d64dbfaeb0179c43bce15daf99a3f8931c6136
Sviatoslav <sv_dubov@ukr.net>
2023-01-03 18:56:48 +0200

    Fix naming

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,32 +51,29 @@
 
 function formatDate(date, fromFormat, toFormat) {
   let separator = fromFormat[3];
-  const dateArr = date.split(separator);
-  const dateObj = {};
+  const splittedDate = date.split(separator);
+  const objDate = {};
   const formattedDate = [];
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    const fromF = fromFormat[i];
-    const initDate = dateArr[i];
+    const initDate = splittedDate[i];
 
-    if (fromF === 'YY') {
+    if (fromFormat[i] === 'YY') {
       if (initDate >= 30) {
-        dateObj['YYYY'] = '19' + initDate;
+        objDate['YYYY'] = '19' + initDate;
       } else {
-        dateObj['YYYY'] = '20' + initDate;
+        objDate['YYYY'] = '20' + initDate;
       }
     }
 
-    dateObj[fromF] = initDate;
+    objDate[fromFormat[i]] = initDate;
   }
 
   for (let i = 0; i < toFormat.length - 1; i++) {
-    const toF = toFormat[i];
-
-    if (toF === 'YY') {
-      formattedDate.push(dateObj['YYYY'].slice(2));
+    if (toFormat[i] === 'YY') {
+      formattedDate.push(objDate['YYYY'].slice(2));
     } else {
-      formattedDate.push(dateObj[toF]);
+      formattedDate.push(objDate[toFormat[i]]);
     }
   }
   separator = toFormat[3];
