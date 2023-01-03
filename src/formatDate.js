5e3cceaa1ed2ac5d7364606363b7142c24622fc9
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-01-03 12:22:44 +0200

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,34 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separatorInput = fromFormat[3];
+  const separatorOutput = toFormat[3];
+  const dateArr = date.split([separatorInput]);
+
+  const formatAndDate = {};
+  const formatedDate = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    formatAndDate[fromFormat[i]] = dateArr[i];
+  }
+
+  if (formatAndDate.hasOwnProperty('YYYY')) {
+    formatAndDate.YY = formatAndDate.YYYY.slice(2);
+  }
+
+  if (formatAndDate.YY < 30) {
+    formatAndDate.YYYY = `20${formatAndDate.YY}`;
+  } else {
+    formatAndDate.YYYY = `19${formatAndDate.YY}`;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    const key = toFormat[i];
+
+    formatedDate.push(formatAndDate[key]);
+  }
+
+  return formatedDate.join([separatorOutput]);
 }
 
 module.exports = formatDate;
