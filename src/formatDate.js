43aa0ff78b0702d7f6072032b0d0315b8a39ec24
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-01-30 22:56:38 +0200

    The tests were completed successfully

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,63 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let normalizeDate = date.split(fromFormat[3]);
+  const finallyDate = [];
+  let day = '';
+  let month = '';
+  let year = '';
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    switch (fromFormat[i]) {
+      case 'YY' :
+        year = normalizeDate[i];
+        break;
+
+      case 'YYYY' :
+        year = normalizeDate[i].slice(2);
+        break;
+
+      case 'DD' :
+        day = normalizeDate[i];
+        break;
+
+      case 'MM' :
+        month = normalizeDate[i];
+        break;
+    }
+  }
+
+  normalizeDate = [day, month, year];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY' :
+        finallyDate[i] = formatYear(year);
+        break;
+
+      case 'YY' :
+        finallyDate[i] = year;
+        break;
+
+      case 'MM' :
+        finallyDate[i] = month;
+        break;
+
+      case 'DD' :
+        finallyDate[i] = day;
+        break;
+    }
+  }
+
+  return finallyDate.join(toFormat[3]);
+}
+
+function formatYear(year) {
+  if (+year < 30) {
+    return `20${year}`;
+  } else {
+    return `19${year}`;
+  }
 }
 
 module.exports = formatDate;
