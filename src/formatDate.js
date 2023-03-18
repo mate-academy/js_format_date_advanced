e0b1a0ae0bb3458484ff109096c427865659d4a4
Mark Boiko <boikomark555@gmail.com>
2023-03-18 17:22:05 +0200

    create function format Date

--- src/formatDate.js
+++ src/formatDate.js
@@ -3,7 +3,7 @@
 /**
  *   Time flies, standards change. Let's get rid of the routine of changing the
  * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
+ * the old `fromFormat` correctFormatay and the new `toFormat` correctFormatay.
  * given date in new format.
  *   The function can change a separator, reorder the date parts of convert a
  * year from 4 digits to 2 digits and back.
@@ -50,7 +50,46 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separatorFromFormat = fromFormat.pop();
+  const separatorToFormat = toFormat.pop();
+  const numbersOfDate = date.split(separatorFromFormat);
+  const currentFormat = {};
+  const correctFormat = [];
+  let i = 0;
+
+  for (const format of fromFormat) {
+    currentFormat[format] = numbersOfDate[i];
+    i++;
+  }
+
+  let yearsFormat = 0;
+
+  for (const newFormat of toFormat) {
+    if (!fromFormat.includes(newFormat) && newFormat === 'YY') {
+      yearsFormat = currentFormat['YYYY'] % 100;
+      correctFormat.push(yearsFormat);
+      continue;
+    }
+
+    if (!fromFormat.includes(newFormat) && newFormat === 'YYYY') {
+      yearsFormat = convertYear(currentFormat['YY']);
+      correctFormat.push(yearsFormat);
+      continue;
+    }
+    correctFormat.push(currentFormat[newFormat]);
+  }
+
+  const result = correctFormat.join(separatorToFormat);
+
+  return result;
+}
+
+function convertYear(year) {
+  if (year < 30) {
+    return '20' + year;
+  } else {
+    return '19' + year;
+  }
 }
 
 module.exports = formatDate;
