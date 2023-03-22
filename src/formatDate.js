4a73881eda3ef18dc65e2e3b303e9ff1aa16b35b
Anna Latusha <annapop03072701@gmail.com>
2023-03-22 11:13:27 +0200

    Change variables name

--- src/formatDate.js
+++ src/formatDate.js
@@ -5,8 +5,6 @@
  * date format. Create a `formatDate` function that accepts the `date` string,
  * the old `fromFormat` array and the new `toFormat` array. Function returns
  * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
  *
@@ -51,22 +49,23 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const formatLength = fromFormat.length;
-  const partsOfDate = date.split(fromFormat[formatLength - 1]);
+  const partOfDatesOfDate = date.split(fromFormat[formatLength - 1]);
+  let formattedDate = '';
   let year = 0;
-  let shortYear = 0;
+  let yearFormatted = 0;
   let i = 0;
 
   while (i < formatLength) {
     switch (fromFormat[i]) {
       case 'YYYY':
-        year = parseInt(partsOfDate[i], 10);
-        shortYear = year % 100;
+        year = parseInt(partOfDatesOfDate[i], 10);
+        yearFormatted = year % 100;
         i = formatLength;
         break;
 
       case 'YY':
-        shortYear = parseInt(partsOfDate[i], 10);
-        year = shortYear < 30 ? 2000 + shortYear : 1900 + shortYear;
+        yearFormatted = parseInt(partOfDatesOfDate[i], 10);
+        year = yearFormatted < 30 ? 2000 + yearFormatted : 1900 + yearFormatted;
         i = formatLength;
         break;
 
@@ -75,26 +74,24 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
-  let formattedDate = '';
-
   for (i = 0; i < formatLength; i++) {
-    const part = toFormat[i];
+    const partOfDate = toFormat[i];
 
-    switch (part) {
+    switch (partOfDate) {
       case 'YYYY':
         formattedDate += year;
         break;
 
       case 'YY':
-        formattedDate += shortYear.toString().padStart(2, '0');
+        formattedDate += yearFormatted.toString().padStart(2, '0');
         break;
 
       case 'MM':
-        formattedDate += partsOfDate[fromFormat.indexOf('MM')];
+        formattedDate += partOfDatesOfDate[fromFormat.indexOf('MM')];
         break;
 
       case 'DD':
-        formattedDate += partsOfDate[fromFormat.indexOf('DD')];
+        formattedDate += partOfDatesOfDate[fromFormat.indexOf('DD')];
         break;
 
       default:
