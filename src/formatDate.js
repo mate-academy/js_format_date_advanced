099a7e6ae6fd176166b5289626d4e9c74e9a2a9d
Yuri Shkrobut <georgebest2409@gmail.com>
2023-03-28 15:01:13 +0300

    Simple variables were changed to object

--- src/formatDate.js
+++ src/formatDate.js
@@ -6,7 +6,7 @@
  * the old `fromFormat` array and the new `toFormat` array. Function returns
  * given date in new format.
  *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
+ * dateObject.year from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
  *
@@ -51,24 +51,22 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const dateArr = date.split(fromFormat[fromFormat.length - 1]);
-  let year = '';
-  let month = '';
-  let day = '';
+  const dateObject = {};
   const arr = [];
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
     switch (fromFormat[i]) {
       case 'YYYY':
       case 'YY':
-        year = dateArr[i];
+        dateObject.year = dateArr[i];
         break;
 
       case 'MM':
-        month = dateArr[i];
+        dateObject.month = dateArr[i];
         break;
 
       case 'DD':
-        day = dateArr[i];
+        dateObject.day = dateArr[i];
         break;
     }
   }
@@ -76,32 +74,32 @@ function formatDate(date, fromFormat, toFormat) {
   for (let i = 0; i < toFormat.length - 1; i++) {
     switch (toFormat[i]) {
       case 'MM':
-        arr.push(month);
+        arr.push(dateObject.month);
         break;
 
       case 'DD':
-        arr.push(day);
+        arr.push(dateObject.day);
         break;
 
       case 'YY':
-        if (year.length === 4) {
-          arr.push(year[2] + year[3]);
+        if (dateObject.year.length === 4) {
+          arr.push(dateObject.year[2] + dateObject.year[3]);
           continue;
         }
-        arr.push(year);
+        arr.push(dateObject.year);
         break;
 
       case 'YYYY':
-        if (year.length === 2 && year < 30) {
-          arr.push(`20${year}`);
+        if (dateObject.year.length === 2 && dateObject.year < 30) {
+          arr.push(`20${dateObject.year}`);
           continue;
         }
 
-        if (year.length === 2 && year >= 30) {
-          arr.push(`19${year}`);
+        if (dateObject.year.length === 2 && dateObject.year >= 30) {
+          arr.push(`19${dateObject.year}`);
           continue;
         }
-        arr.push(year);
+        arr.push(dateObject.year);
         break;
     }
   }
