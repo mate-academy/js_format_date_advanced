a30f70d485e9c50b0530a5604e2be6d85eb02055
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-04-05 23:07:25 +0300

    formatDate done

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,35 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const partsDate = date.split(`${fromFormat[3]}`);
+  const dateObj = {};
+  let result = [];
+
+  for (let i = 0; i < partsDate.length; i++) {
+    dateObj[fromFormat[i]] = partsDate[i];
+  }
+
+  if (dateObj.hasOwnProperty('YYYY')) {
+    dateObj.YY = dateObj.YYYY.slice(2);
+  }
+
+  if (dateObj.YY < 30) {
+    dateObj.YYYY = '20' + dateObj.YY;
+  } else {
+    dateObj.YYYY = '19' + dateObj.YY;
+  }
+
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    for (const part in dateObj) {
+      if (toFormat[i] === part) {
+        result.push(dateObj[part]);
+      }
+    }
+  }
+
+  return result.join(`${toFormat[3]}`);
+
 }
 
 module.exports = formatDate;
