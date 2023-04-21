664d449d91f2e98fc76644de47f18bed3dc2595c
Yelyzaveta <molochiza@gmail.com>
2023-04-21 16:00:41 +0300

    fixed solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -55,21 +55,28 @@ function formatDate(date, fromFormat, toFormat) {
   const dateYear = dateArr[fromFormat.indexOf('YYYY')]
     || dateArr[fromFormat.indexOf('YY')];
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    switch (toFormat[i]) {
+  for (const format of toFormat) {
+    switch (format) {
       case 'DD':
       case 'MM':
-        newFormat.push(dateArr[fromFormat.indexOf(toFormat[i])]);
+        newFormat.push(dateArr[fromFormat.indexOf(format)]);
         break;
 
-      default:
-        if (toFormat[i].length < dateYear.length) {
-          newFormat.push(dateYear.slice(2, 4));
-        } else if (toFormat[i].length > dateYear.length) {
+      case 'YYYY':
+        if (format.length > dateYear.length) {
           newFormat.push((dateYear < 30 ? 20 : 19) + dateYear);
         } else {
           newFormat.push(dateYear);
         }
+        break;
+
+      case 'YY':
+        if (format.length < dateYear.length) {
+          newFormat.push(dateYear.slice(2, 4));
+        } else {
+          newFormat.push(dateYear);
+        }
+        break;
     }
   }
 
