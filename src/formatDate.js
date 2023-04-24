1886c4654e5a386aa3a2c8802d1921c7b7d638a6
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-04-25 01:19:45 +0200

    FormatDate solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -55,6 +55,9 @@ function formatDate(date, fromFormat, toFormat) {
   const formatedDate = [];
   let countY = 0;
 
+  // create an abject with a key 'y'- year 'm' - month 'd' - day
+  // and value from dateArr which relates to key,
+  // also in if statement we find out the lengh of year
   for (let i = 0; i < dateArr.length; i++) {
     dateObj[fromFormat[i][0]] = dateArr[i];
 
@@ -63,10 +66,15 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
+  // if the length of a year in date > length of Y in pattern,
+  // we make date = last 2 digits;
   if (dateObj.Y.length > countY) {
     dateObj.Y = dateObj.Y.slice(-2);
   }
 
+  // if the length of a year in date < length of Y in pattern,
+  // we have to check if the date < 30, we add 20 in the beginning,
+  // otherwise add 19
   if (dateObj.Y.length < countY) {
     if (+dateArr[0] < 30) {
       dateObj.Y = '20' + dateArr[0];
@@ -75,10 +83,14 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
+  // so now we have formatted date in the object, and we need to push it
+  // to array in the right order
   for (let i = 0; i < toFormat.length - 1; i++) {
     formatedDate.push(dateObj[toFormat[i][0]]);
   }
 
+  // return the string with the correct separator by taking
+  // the last element from pattern
   return formatedDate.join(toFormat.at(-1));
 }
 
