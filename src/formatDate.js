97cb902a9ad81913132bfc127d316d68eeddc0d5
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-07 16:28:05 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,53 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
-}
+  const dateArr = date.split(fromFormat[fromFormat.length - 1]);
+  const separator = toFormat[toFormat.length - 1];
+  const formatOfDate = [];
+  const obj = {};
+  let str = '';
 
+  for (let i = 0; i < fromFormat.length; i++) {
+    const element = fromFormat[i];
+
+    obj[element] = dateArr[i];
+    formatOfDate.push(obj);
+  }
+
+  for (const item of toFormat) {
+    if (item === 'YY') {
+      if (fromFormat.includes(item)) {
+        str += obj.YY + separator;
+      } else {
+        const lastDate = obj.YYYY.slice(-2);
+
+        str += lastDate + separator;
+      }
+    }
+
+    if (item === 'YYYY') {
+      if (fromFormat.includes(item)) {
+        str += obj.YYYY + separator;
+      } else {
+        if (obj.YY < 30) {
+          str += '20' + obj.YY + separator;
+        } else {
+          str += '19' + obj.YY + separator;
+        }
+      }
+    }
+
+    if (item === 'MM') {
+      str += obj.MM + separator;
+    }
+
+    if (item === 'DD') {
+      str += obj.DD + separator;
+    }
+  }
+
+  const strDate = str.slice(0, -1);
+
+  return strDate;
+}
 module.exports = formatDate;
