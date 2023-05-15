5c87227a6140f91ccafdd5c94d67f5897a98e8b7
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-05-15 20:44:53 +0300

    format date

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,26 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+
+  const arrDate = date.split(fromFormat[3]);
+  const result = [];
+
+  result[toFormat.indexOf('DD')] = arrDate[fromFormat.indexOf('DD')];
+  result[toFormat.indexOf('MM')] = arrDate[fromFormat.indexOf('MM')];
+
+  let year = (fromFormat.indexOf('YYYY') === -1)
+    ? arrDate[fromFormat.indexOf('YY')]
+    : arrDate[fromFormat.indexOf('YYYY')];
+
+  if (toFormat.indexOf('YYYY') === -1) {
+    year = (year.length > 2) ? year.slice(2, 4) : year;
+    result[toFormat.indexOf('YY')] = year;
+  } else {
+    year = (year.length === 2) ? (year < 30) ? `20${year}` : `19${year}` : year;
+    result[toFormat.indexOf('YYYY')] = year;
+  }
+
+  return result.join(toFormat[3]);
 }
 
 module.exports = formatDate;
