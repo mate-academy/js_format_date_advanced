0c31612b98148f45d2edd47e945ff6801dd79b45
Bohdan <6alfabravo9@gmail.com>
2023-05-15 19:34:47 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,58 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const oldDate = date.split(fromSeparator);
+  const newDate = [];
+
+  let year;
+  let month;
+  let day;
+
+  for (const index in fromFormat) {
+    const toFormatPart = fromFormat[index];
+    const oldDatePart = oldDate[index];
+
+    // get date values from old format
+    if (toFormatPart.includes('YY')) {
+      year = oldDatePart;
+    }
+
+    if (toFormatPart.includes('MM')) {
+      month = oldDatePart;
+    }
+
+    if (toFormatPart.includes('DD')) {
+      day = oldDatePart;
+    }
+  }
+
+  // check for required format of year
+  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    year = year < 30 ? `20${year}` : `19${year}`;
+  }
+
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    year = year < 30 ? year - 2000 : year - 1900;
+  }
+
+  // set date values into new format
+  for (const toFormatPart of toFormat) {
+    if (toFormatPart.includes('YY')) {
+      newDate.push(year);
+    }
+
+    if (toFormatPart.includes('MM')) {
+      newDate.push(month);
+    }
+
+    if (toFormatPart.includes('DD')) {
+      newDate.push(day);
+    }
+  }
+
+  return newDate.join(toSeparator);
 }
 
 module.exports = formatDate;
