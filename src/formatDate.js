034926595d83bac492ba388f8af6240ed6d2db88
Maksym Kostetskyi <mkost@gmail.com>
2023-05-17 16:59:05 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,46 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const sepatorOld = fromFormat[3];
+  const sepatorNew = toFormat[3];
+  const dateSplitOld = date.split(sepatorOld);
+  const dateSplitNew = [];
+  let day;
+  let month;
+  let year;
+  let result = '';
+
+  for (let i = 0; i < dateSplitOld.length; i++) {
+    if (fromFormat[i] === 'DD') {
+      day = dateSplitOld[i];
+    } else if (fromFormat[i] === 'MM') {
+      month = dateSplitOld[i];
+    } else if (fromFormat[i] === 'YY' && toFormat[i] === 'YYYY') {
+      if (dateSplitOld[i] < 30) {
+        year = '20' + dateSplitOld[i];
+      } else {
+        year = '19' + dateSplitOld[i];
+      }
+    } else if (fromFormat[i] === 'YYYY' && toFormat[i] === 'YY') {
+      year = dateSplitOld[i].slice(2);
+    } else {
+      year = dateSplitOld[i];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i] === 'DD') {
+      dateSplitNew[i] = day;
+    } else if (toFormat[i] === 'MM') {
+      dateSplitNew[i] = month;
+    } else {
+      dateSplitNew[i] = year;
+    }
+  }
+
+  result = dateSplitNew.join(sepatorNew);
+
+  return result;
 }
 
 module.exports = formatDate;
