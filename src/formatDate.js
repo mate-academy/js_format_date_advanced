b02b7b13bdae930875c89b802bebe058a188855b
Dmytro Levchenko <levchenkod91@gmail.com>
2023-05-19 18:55:12 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,47 +1,6 @@
 'use strict';
 
 /**
- *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
- *
- * Examples:
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['DD', 'MM', 'YYYY', '.'],
- * ) // '18.02.2020'
- *
- * formatDate(
- *   '18-02-2020',
- *   ['DD', 'MM', 'YYYY', '-'],
- *   ['DD', 'MM', 'YY', '/'],
- * ) // '18/02/20'
- *
- * formatDate(
- *   '20/02/18',
- *   ['YY', 'MM', 'DD', '/'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '97/02/18',
- *   ['YY', 'MM', 'DD', '/'],
- *   ['DD', 'MM', 'YYYY', '.'],
- * ) // '18.02.1997'
- *
  * @param {string} date
  * @param {string[]} fromFormat
  * @param {string[]} toFormat
@@ -50,7 +9,30 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldSep = fromFormat.pop();
+  const newSep = toFormat.pop();
+  const dateArr = date.split(oldSep);
+  const newDate = [];
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    for (let j = 0; j < toFormat.length; j++) {
+      if (fromFormat[i] === toFormat[j]) {
+        newDate[j] = dateArr[i];
+      }
+
+      if (fromFormat[i] === 'YYYY' && toFormat[j] === 'YY') {
+        newDate[j] = dateArr[i].slice(2);
+      }
+
+      if (fromFormat[i] === 'YY' && toFormat[j] === 'YYYY') {
+        newDate[j] = dateArr[i] < 30
+          ? `20${dateArr[i]}`
+          : `19${dateArr[i]}`;
+      }
+    }
+  }
+
+  return newDate.join(newSep);
 }
 
 module.exports = formatDate;
