5e3c25643d41687eea61413d759e2b906a41aa8c
grinjk1993 <grinjk1993@gmail.com>
2023-06-09 16:31:33 +0300

    fixed

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,22 +1,25 @@
 'use strict';
 
 function formatDate(date, fromFormat, toFormat) {
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const arrDate = date.split(fromSeparator);
   const newDate = [];
   const dateInfo = {};
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    dateInfo[fromFormat[i]] = date.split(fromFormat[fromFormat.length - 1])[i];
+    dateInfo[fromFormat[i]] = arrDate[i];
   }
 
-  if (!dateInfo.YYYY && +dateInfo.YY >= 30) {
+  if (+dateInfo.YY >= 30) {
     dateInfo.YYYY = `19${dateInfo.YY}`;
   }
 
-  if (!dateInfo.YYYY && +dateInfo.YY < 30) {
+  if (+dateInfo.YY < 30) {
     dateInfo.YYYY = `20${dateInfo.YY}`;
   }
 
-  if (!dateInfo.YY && +dateInfo.YYYY >= 30) {
+  if (+dateInfo.YYYY >= 30) {
     dateInfo.YY = dateInfo.YYYY.slice(2);
   }
 
@@ -24,7 +27,7 @@ function formatDate(date, fromFormat, toFormat) {
     newDate.push(dateInfo[toFormat[i]]);
   }
 
-  return newDate.join(toFormat[toFormat.length - 1]);
+  return newDate.join(toSeparator);
 }
 
 module.exports = formatDate;
