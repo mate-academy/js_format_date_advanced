946c7e8d737c87729aa0f4a73718bbeaf60d9599
Diana <diana.gs202@gmail.com>
2023-06-14 15:42:02 +0300

    improve code

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,33 +51,38 @@
 
 function formatDate(date, fromFormat, toFormat) {
   const arrayFromDate = date.split(fromFormat[fromFormat.length - 1]);
-  const dateExample = date.split(fromFormat[fromFormat.length - 1]);
+  const newDate = [];
   let index;
 
+  checkYear(arrayFromDate, fromFormat, toFormat);
+
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    if (fromFormat[i] === 'YYYY' && toFormat.includes('YY')) {
-      index = toFormat.indexOf('YY');
-      arrayFromDate[index] = dateExample[i].slice(2);
-      continue;
-    } else if (fromFormat[i] === 'YY' && toFormat.includes('YYYY')) {
-      index = toFormat.indexOf('YYYY');
-
-      if (+dateExample[i] < 30) {
-        dateExample[i] = `20${dateExample[i]}`;
-      } else {
-        dateExample[i] = `19${dateExample[i]}`;
-      }
-
-      arrayFromDate[index] = dateExample[i];
-      continue;
-    }
-
     index = toFormat.indexOf(fromFormat[i]);
 
-    arrayFromDate[index] = dateExample[i];
+    newDate[index] = arrayFromDate[i];
   }
 
-  return arrayFromDate.join(toFormat[toFormat.length - 1]);
+  return newDate.join(toFormat[toFormat.length - 1]);
+}
+
+function checkYear(arrayFromDate, fromFormat, toFormat) {
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    const indexOfYearFrom = fromFormat.indexOf('YYYY');
+    const indexOfYearTo = toFormat.indexOf('YY');
+
+    toFormat[indexOfYearTo] = 'YYYY';
+
+    arrayFromDate[indexOfYearFrom] = arrayFromDate[indexOfYearFrom].slice(2);
+  } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    const indexOfYearFrom = fromFormat.indexOf('YY');
+    const indexOfYearTo = toFormat.indexOf('YYYY');
+
+    toFormat[indexOfYearTo] = 'YY';
+
+    arrayFromDate[indexOfYearFrom] = arrayFromDate[indexOfYearFrom] < 30
+      ? arrayFromDate[indexOfYearFrom] = `20${arrayFromDate[indexOfYearFrom]}`
+      : arrayFromDate[indexOfYearFrom] = `19${arrayFromDate[indexOfYearFrom]}`;
+  }
 }
 
 module.exports = formatDate;
