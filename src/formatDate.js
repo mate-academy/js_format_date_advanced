e2ba7fe6d90e9a5e19b93a3f580e862ccfffeb14
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-14 12:18:50 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -54,16 +54,33 @@ function formatDate(date, fromFormat, toFormat) {
   const newSeparator = toFormat.pop();
   const arrayDate = date.split(oldSeparator);
   const resultArr = [];
+  let day, month, year;
 
   for (let i = 0; i < fromFormat.length; i++) {
-    for (let k = 0; k < toFormat.length; k++) {
-      if (fromFormat[i].startsWith('Y') && toFormat[k].startsWith('Y')) {
-        resultArr[k] = typeOfYear(arrayDate[i], toFormat[k]);
-      }
+    if (fromFormat[i].startsWith('Y')) {
+      year = arrayDate[i];
+    }
 
-      if (fromFormat[i] === toFormat[k]) {
-        resultArr[k] = (arrayDate[i]);
-      }
+    if (fromFormat[i].startsWith('M')) {
+      month = arrayDate[i];
+    }
+
+    if (fromFormat[i].startsWith('D')) {
+      day = arrayDate[i];
+    }
+  }
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i].startsWith('Y')) {
+      resultArr[i] = typeOfYear(year, toFormat[i]);
+    }
+
+    if (toFormat[i].startsWith('M')) {
+      resultArr[i] = month;
+    }
+
+    if (toFormat[i].startsWith('D')) {
+      resultArr[i] = day;
     }
   }
 
