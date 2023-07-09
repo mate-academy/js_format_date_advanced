07189fc9e586de9fc2cd4c056da54b02dbd9370e
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-07-09 17:26:26 +0300

    FormatDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,31 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateArr = date.split(fromFormat[3]);
+  const dateObj = {};
+  const NewFormat = [];
+
+  for (let i = 0; i < dateArr.length; i++) {
+    if (fromFormat[i] === 'YY') {
+      if (dateArr[i] < '30') {
+        dateArr[i] = '20'.concat(dateArr[i]);
+      } else {
+        dateArr[i] = '19'.concat(dateArr[i]);
+      }
+      fromFormat[i] = 'YYYY';
+    }
+    dateObj[fromFormat[i]] = dateArr[i];
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i] === 'YY') {
+      NewFormat.push(dateObj.YYYY.slice(2));
+    } else {
+      NewFormat.push(dateObj[toFormat[i]]);
+    }
+  }
+
+  return NewFormat.join(toFormat[3]);
 }
 
 module.exports = formatDate;
