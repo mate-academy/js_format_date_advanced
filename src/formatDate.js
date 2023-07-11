d57c1a96b88d77cd14b6f14e6946a3b1842f5373
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-07-11 18:09:30 +0300

    format date v2

--- src/formatDate.js
+++ src/formatDate.js
@@ -54,13 +54,16 @@ function formatDate(date, fromFormat, toFormat) {
   let yearIndex = 0;
   let monthIndex = 0;
   let dayIndex = 0;
+  const shortYearFormat = 'YY';
+  const longYearFormat = 'YYYY';
+  const maxYearCentury = 30;
 
   const result = [0, 0, 0];
 
   for (let i = 0; i <= 2; i++) {
-    if (fromFormat[i] === 'YY') {
+    if (fromFormat[i] === shortYearFormat) {
       yearIndex = i;
-    } else if (fromFormat[i] === 'YYYY') {
+    } else if (fromFormat[i] === longYearFormat) {
       yearIndex = i;
     }
 
@@ -74,17 +77,17 @@ function formatDate(date, fromFormat, toFormat) {
   }
 
   for (let k = 0; k <= 2; k++) {
-    if (toFormat[k] === 'YY') {
-      if (fromFormat[yearIndex] === 'YYYY') {
+    if (toFormat[k] === shortYearFormat) {
+      if (fromFormat[yearIndex] === longYearFormat) {
         result[k] = dateParts[yearIndex].slice(2);
       } else {
         result[k] = dateParts[yearIndex];
       }
-    } else if (toFormat[k] === 'YYYY') {
-      if (fromFormat[yearIndex] === 'YY') {
-        if (dateParts[yearIndex] >= 30) {
+    } else if (toFormat[k] === longYearFormat) {
+      if (fromFormat[yearIndex] === shortYearFormat) {
+        if (dateParts[yearIndex] >= maxYearCentury) {
           result[k] = 19 + dateParts[yearIndex];
-        } else if (dateParts[yearIndex] < 30) {
+        } else if (dateParts[yearIndex] < maxYearCentury) {
           result[k] = 20 + dateParts[yearIndex];
         }
       } else {
