a06311c33d895d49a384f8673c1e9c4e659bb62b
Andrew <deyneka2003@gmail.com>
2023-08-03 01:07:41 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,32 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const BORDER_OF_THE_AGES = 30;
+  const LAST_CENTURY = 19;
+  const PRESENT_CENTURY = 20;
+  const fromFormatObj = {};
+  const separateValue = date.split(fromFormat[3]);
+  const resultFormatDate = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    fromFormatObj[fromFormat[i]] = separateValue[i];
+  }
+
+  if (fromFormatObj.hasOwnProperty('YYYY')) {
+    fromFormatObj.YY = fromFormatObj.YYYY.slice(2);
+  }
+
+  if (fromFormatObj.YY < BORDER_OF_THE_AGES) {
+    fromFormatObj['YYYY'] = PRESENT_CENTURY + fromFormatObj.YY;
+  } else {
+    fromFormatObj.YYYY = LAST_CENTURY + fromFormatObj.YY;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    resultFormatDate.push(fromFormatObj[toFormat[i]]);
+  }
+
+  return resultFormatDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
