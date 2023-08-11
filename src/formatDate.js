febbd46fd78388b7f925b8717a4d9cd926125544
myName <vlad.svorin2008@gmail.com>
2023-08-11 20:31:35 +0200

    add fix

--- src/formatDate.js
+++ src/formatDate.js
@@ -67,21 +67,21 @@ function formatDate(date, fromFormat, toFormat) {
   const IF_TO_FORMAT_HAVE_YY = toFormat.includes('YY');
   const IF_TO_FORMAT_HAVE_YYYY = toFormat.includes('YYYY');
 
-  const TO_FORMAT_YYYY_INDEX = toFormat.indexOf('YYYY');
-  const TO_FORMAT_YY_INDEX = toFormat.indexOf('YY');
-  const FROM_FORMAT_YYYY_INDEX = fromFormat.indexOf('YYYY');
-  const FROM_FORMAT_YY_INDEX = fromFormat.indexOf('YY');
+  const TO_FT_YYYY_INDEX = toFormat.indexOf('YYYY');
+  const TO_FT_YY_INDEX = toFormat.indexOf('YY');
+  const FROM_FT_YYYY_INDEX = fromFormat.indexOf('YYYY');
+  const FROM_FT_YY_INDEX = fromFormat.indexOf('YY');
 
   if (IF_FROM_FORMAT_HAVE_YYYY && IF_TO_FORMAT_HAVE_YY) {
-    correctDate[TO_FORMAT_YY_INDEX]
-      = eachDate[FROM_FORMAT_YYYY_INDEX] % 100;
+    correctDate[TO_FT_YY_INDEX]
+      = eachDate[FROM_FT_YYYY_INDEX] % 100;
   }
 
   if (IF_TO_FORMAT_HAVE_YYYY && IF_FROM_FORMAT_HAVE_YY) {
-    const century = eachDate[FROM_FORMAT_YY_INDEX] < 30 ? '20' : '19';
+    const century = eachDate[FROM_FT_YY_INDEX] < 30 ? '20' : '19';
 
-    correctDate[TO_FORMAT_YYYY_INDEX]
-      = century + eachDate[FROM_FORMAT_YY_INDEX];
+    correctDate[TO_FT_YYYY_INDEX]
+      = century + eachDate[FROM_FT_YY_INDEX];
   }
 
   return correctDate.join(NEW_SEPARATOR);
