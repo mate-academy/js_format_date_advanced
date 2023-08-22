f9dd6d0bbbc65e14230215a63ca3dd460d3fb0b2
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-08-22 16:15:46 +0200

    darokrk sugestions

--- src/formatDate.js
+++ src/formatDate.js
@@ -49,21 +49,20 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-
   const calendar = {
     formatYear(format) {
       if (this.hasOwnProperty('YYYY') && format === 'YY') {
         this['YY'] = this['YYYY'].slice(2, 4);
         delete this['YYYY'];
       } else if (this.hasOwnProperty('YY') && format === 'YYYY') {
-        this['YYYY'] = this['YY'] <= 20 ? '20' + this['YY'] : '19' + this['YY'];
+        this['YYYY'] = this['YY'] <= 20 ? `20${this['YY']}` : `19${this['YY']}`;
         delete this['YY'];
       }
     },
   };
 
-  const toSeparator = toFormat[3];
-  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const fromSeparator = fromFormat[fromFormat.length - 1];
   const newDate = date.split(fromSeparator);
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
