2cf1915bb5b53ad45f798a5be921c8fa7707d26a
Misha Koshilka <mkosilka@gmail.com>
2023-08-31 01:35:52 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,60 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let result = [];
+
+  const partsDate = date.split(fromFormat[fromFormat.length - 1]);
+
+  const info = {};
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    switch (fromFormat[i]) {
+      case 'YY':
+      case 'YYYY':
+        info.year = partsDate[i];
+        break;
+      case 'MM':
+        info.month = partsDate[i];
+        break;
+      case 'DD':
+        info.day = partsDate[i];
+        break;
+    }
+  }
+
+  let currentYear;
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i] === 'YYYY' && info.year.length === 4) {
+      result.push(info.year);
+    } else if (toFormat[i] === 'YYYY' && info.year.length === 2) {
+      if (info.year < 30) {
+        currentYear = 20;
+      } else {
+        currentYear = 19;
+      }
+
+      result.push(currentYear + info.year);
+    } else if (toFormat[i] === 'DD') {
+      result.push(info.day);
+    } else if (toFormat[i] === 'MM') {
+      result.push(info.month);
+    } else if (toFormat[i] === 'YY') {
+      result.push(info.year.slice(-2));
+    }
+
+    if (i < toFormat.length - 1) {
+      result.push(toFormat[toFormat.length - 1]);
+    }
+  }
+
+  result = result.join('');
+
+  if (result[-1] === result[-2]) {
+    result = result.slice(0, -1);
+  }
+
+  return result;
 }
 
 module.exports = formatDate;
