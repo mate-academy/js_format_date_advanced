d6aab659cf9941fba24fba75614b9b921f2608d1
Butovych <dreamextreme29@gmail.com>
2023-08-31 18:50:48 +0300

    solution task

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,45 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separator = toFormat[3];
+  const dateToArray = date.split(fromFormat[3]);
+  const fromDates = {};
+  const toDates = {};
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    fromDates[fromFormat[i]] = dateToArray[i];
+    toDates[toFormat[i]] = 0;
+  }
+
+  let keyYearFrom = '';
+
+  for (const key in fromDates) {
+    if (key.includes('YY')) {
+      keyYearFrom = key;
+    }
+  }
+
+  let keyYearTo = '';
+
+  for (const key in toDates) {
+    if (key.includes('YY')) {
+      keyYearTo = key;
+    }
+  }
+
+  if (keyYearFrom.length > keyYearTo.length) {
+    fromDates[keyYearTo] = fromDates[keyYearFrom].toString().slice(2);
+  }
+
+  if (keyYearFrom.length < keyYearTo.length && fromDates[keyYearFrom] < 30) {
+    fromDates[keyYearTo] = `20${fromDates[keyYearFrom]}`;
+  }
+
+  if (keyYearFrom.length < keyYearTo.length && fromDates[keyYearFrom] >= 30) {
+    fromDates[keyYearTo] = `19${fromDates[keyYearFrom]}`;
+  }
+
+  return `${fromDates[toFormat[0]]}${separator}` + `${fromDates[toFormat[1]]}${separator}` + `${fromDates[toFormat[2]]}`;
 }
 
 module.exports = formatDate;
