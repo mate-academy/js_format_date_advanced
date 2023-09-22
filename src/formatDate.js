dc796b4eb93c1d77222052afc37d573b0446b0df
Oksana Drozdyk <80383357+o-drozzdyk@users.noreply.github.com>
2023-09-22 11:09:21 +0300

    Update formatDate.js

--- src/formatDate.js
+++ src/formatDate.js
@@ -57,14 +57,11 @@ function formatDate(date, fromFormat, toFormat) {
   const MONTH_MARK = 'MM';
 
   const givenDate = date.split(separator);
-  let day;
-  let month;
-  let year;
 
-let day = '';
-let month = '';
-let year = '';
-let oldYearFormat = '';
+  let day = '';
+  let month = '';
+  let year = '';
+  let oldYearFormat = '';
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
     switch (fromFormat[i]) {
