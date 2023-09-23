f70843f0a32cf1cbc804c66720f6f2599e9bbaa5
Oleksandr Chumakov <Chumakvs2001@gmail.com>
2023-09-23 22:01:02 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,45 +1,43 @@
-'use strict';
-
 /**
- *   Time flies, standards change. Let's get rid of the routine of changing the
+ * Time flies, standards change. Let's get rid of the routine of changing the
  * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
+ * the old `fromFormat` array, and the new `toFormat` array. Function returns
+ * the given date in the new format.
+ * The function can change a separator, reorder the date parts or convert a
  * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
+ * When converting from YYYY to YY, just use the last 2 digits (1997 -> 97).
+ * When converting from YY to YYYY, use 20YY if YY < 30 and 19YY otherwise.
  *
  * Examples:
  *
  * formatDate(
  *   '2020-02-18',
  *   ['YYYY', 'MM', 'DD', '-'],
- *   ['YYYY', 'MM', 'DD', '.'],
+ *   ['YYYY', 'MM', 'DD', '.']
  * ) // '2020.02.18'
  *
  * formatDate(
  *   '2020-02-18',
  *   ['YYYY', 'MM', 'DD', '-'],
- *   ['DD', 'MM', 'YYYY', '.'],
+ *   ['DD', 'MM', 'YYYY', '.']
  * ) // '18.02.2020'
  *
  * formatDate(
  *   '18-02-2020',
  *   ['DD', 'MM', 'YYYY', '-'],
- *   ['DD', 'MM', 'YY', '/'],
+ *   ['DD', 'MM', 'YY', '/']
  * ) // '18/02/20'
  *
  * formatDate(
  *   '20/02/18',
  *   ['YY', 'MM', 'DD', '/'],
- *   ['YYYY', 'MM', 'DD', '.'],
+ *   ['YYYY', 'MM', 'DD', '.']
  * ) // '2020.02.18'
  *
  * formatDate(
  *   '97/02/18',
  *   ['YY', 'MM', 'DD', '/'],
- *   ['DD', 'MM', 'YYYY', '.'],
+ *   ['DD', 'MM', 'YYYY', '.']
  * ) // '18.02.1997'
  *
  * @param {string} date
@@ -48,76 +46,38 @@
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
-  const oldDataArray = date.split(fromFormat[3]);
-  let newFormatDate = '';
-  let year = '';
-  let month = '';
-  let day = '';
-  const separator = toFormat[3];
+  const resultDate = [];
+  const dateParts = date.split(fromFormat[fromFormat.length - 1]);
+  const slicedFormat = toFormat.slice(0, 3);
 
-  for (let i = 0; i < 3; i++) {
-    switch (fromFormat[i][0]) {
-      case ('Y'):
-        year = oldDataArray[i];
-        break;
+  for (let i = 0; i < slicedFormat.length; i++) {
+    const formatPart = toFormat[i];
+    let prop = formatPart;
 
-      case ('M'):
-        month = oldDataArray[i];
-        break;
-
-      case ('D'):
-        day = oldDataArray[i];
-        break;
-
-      default:
-        return;
+    if (formatPart === 'YYYY') {
+      prop = 'YY';
     }
+
+    const fromIndex = fromFormat.findIndex((value) => value.includes(prop));
+    let newProp = dateParts[fromIndex];
+
+    if (formatPart === 'YYYY' && newProp.length === 2) {
+      if (newProp < 30) {
+        newProp = `20${newProp}`;
+      } else {
+        newProp = `19${newProp}`;
+      }
+    }
+
+    if (formatPart === 'YY' && newProp.length === 4) {
+      newProp = newProp.slice(2);
+    }
+
+    resultDate.push(newProp);
   }
 
-  for (let i = 0; i < 3; i++) {
-    switch (toFormat[i][0]) {
-      case ('Y'):
-        newFormatDate += normalizedYear(year, toFormat[i]);
-        break;
-
-      case ('M'):
-        newFormatDate += month;
-        break;
-
-      case ('D'):
-        newFormatDate += day;
-        break;
-
-      default:
-        return;
-    }
-
-    if (i !== 2) {
-      newFormatDate += separator;
-    }
-  }
-
-  return newFormatDate;
+  return resultDate.join(toFormat[toFormat.length - 1]);
 }
-
-const normalizedYear = (data, newFormat) => {
-  let yearCorrected = data;
-
-  if (newFormat.length === 2 && data.length === 4) {
-    return data.slice(2);
-  };
-
-  if (newFormat.length === 4 && data.length === 2) {
-    if (data < 30) {
-      yearCorrected = '20' + data;
-    } else {
-      yearCorrected = '19' + data;
-    };
-  };
-
-  return yearCorrected;
-};
 
 module.exports = formatDate;
