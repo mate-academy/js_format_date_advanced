20302439edcac60eab2c99adcacdad3436d55dd5
Sakhno Andrii <andreysakhno1986@gmail.com>
2023-10-18 15:41:07 +0300

    Solution - added constants

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,13 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
+  const YEAR_FORMAT_TWO_CHARS = 'YY';
+  const YEAR_FORMAT_FOUR_CHARS = 'YYYY';
+  const XX_CENTURE = '19';
+  const XXI_CENTURE = '20';
+  const XXI_CENTURE_MAX_YEAR = 30;
   const SEPARATOR_INDEX = 3;
+
   const fromSeparator = fromFormat[SEPARATOR_INDEX];
   const toSeparator = toFormat[SEPARATOR_INDEX];
   const toFormatKeys = toFormat.slice(0, SEPARATOR_INDEX);
@@ -70,13 +76,14 @@ function formatDate(date, fromFormat, toFormat) {
       return dateWithFormat[formatKey];
     }
 
-    const year = dateWithFormat['YYYY'] || dateWithFormat['YY'];
+    const year = dateWithFormat[YEAR_FORMAT_TWO_CHARS]
+      || dateWithFormat[YEAR_FORMAT_FOUR_CHARS];
 
-    if (formatKey === 'YY') {
+    if (formatKey === YEAR_FORMAT_TWO_CHARS) {
       return year.slice(2);
     }
 
-    const centuary = (+year < 30) ? '20' : '19';
+    const centuary = (+year < XXI_CENTURE_MAX_YEAR) ? XXI_CENTURE : XX_CENTURE;
 
     return centuary + year;
   });
