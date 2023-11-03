9198d72270c2368adf6780406d1d370a1658599f
Mykhailo Loniak <loniakmykhail@gmail.com>
2023-11-03 16:46:33 +0100

    fix

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,8 +50,8 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const key = fromFormat[3];
-  const arr = date.split(key);
+  const separator = fromFormat[3];
+  const arrDate = date.split(separator);
 
   const dayIndex = fromFormat.indexOf('DD');
   const monthIndex = fromFormat.indexOf('MM');
@@ -61,9 +61,9 @@ function formatDate(date, fromFormat, toFormat) {
     yearIndex = fromFormat.indexOf('YYYY');
   }
 
-  const day = arr[dayIndex];
-  const month = arr[monthIndex];
-  let year = arr[yearIndex];
+  const day = arrDate[dayIndex];
+  const month = arrDate[monthIndex];
+  let year = arrDate[yearIndex];
 
   if (year.length === 2) {
     if (year < 30) {
@@ -73,18 +73,26 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
-  if (toFormat.includes('YY')) {
-    year = year.slice(-2);
+  const formattedDate = [];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    switch (toFormat[i]) {
+      case 'DD':
+        formattedDate.push(day);
+        break;
+      case 'MM':
+        formattedDate.push(month);
+        break;
+      case 'YYYY':
+        formattedDate.push(year);
+        break;
+      case 'YY':
+        formattedDate.push(year.slice(-2));
+        break;
+    }
   }
 
-  switch (toFormat[0]) {
-    case 'DD':
-      return +day + toFormat[3] + month + toFormat[3] + year;
-    case 'MM':
-      return month + toFormat[3] + +day + toFormat[3] + year;
-    default:
-      return year + toFormat[3] + month + toFormat[3] + +day;
-  }
+  return formattedDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
