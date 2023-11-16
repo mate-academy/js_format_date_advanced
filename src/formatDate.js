fa0c10b041d57a31056d743a290d9bbcb514d940
Wojciech Sęk <wojtasskate@wp.pl>
2023-11-16 18:02:13 +0100

    version 1

--- src/formatDate.js
+++ src/formatDate.js
@@ -5,7 +5,7 @@
  * date format. Create a `formatDate` function that accepts the `date` string,
  * the old `fromFormat` array and the new `toFormat` array. Function returns
  * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
+ *   The function can change a separator, reorder the date parts or convert a
  * year from 4 digits to 2 digits and back.
  *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
  *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
@@ -50,7 +50,72 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const separatedDate = date.split(`${fromSeparator}`);
+  const dateToObject = {
+    day: '',
+    year: '',
+    month: '',
+  };
+  const dateArray = [];
+
+  /* sents to another function to fill in object */
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    fillDateObject(fromFormat[i], i);
+  }
+
+  /* now creates new date from object */
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    fillDateArray(toFormat[i], i);
+  }
+
+  return dateArray.join(`${toSeparator}`);
+
+  /* change YY year to YYYY year */
+  function makeYear(yearToChange) {
+    if (+yearToChange >= 30) {
+      return '19' + yearToChange;
+    }
+
+    return '20' + yearToChange;
+  }
+
+  /* fills in objectDate with data */
+  function fillDateArray(dateFormatPart) {
+    switch (dateFormatPart) {
+      case 'DD':
+        dateArray.push(dateToObject.day);
+        break;
+      case 'MM':
+        dateArray.push(dateToObject.month);
+        break;
+      case 'YYYY':
+        dateArray.push(dateToObject.year);
+        break;
+      default:
+        dateArray.push(dateToObject.year.split('').splice(2, 3).join('')); ;
+    }
+  }
+  /* changes object values according to new format */
+
+  function fillDateObject(dateFormatPart, index) {
+    switch (dateFormatPart) {
+      case 'DD':
+        dateToObject.day = separatedDate[index];
+        break;
+      case 'MM':
+        dateToObject.month = separatedDate[index];
+        break;
+      case 'YYYY':
+        dateToObject.year = separatedDate[index];
+        break;
+      default:
+        dateToObject.year = makeYear(separatedDate[index]);
+    }
+  };
+};
 
 module.exports = formatDate;
