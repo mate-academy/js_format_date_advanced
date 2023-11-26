5ecd9d1772e826f1cf6f7e7c8635352f56087779
Volodymyr Liubyn <vovalu88@gmail.com>
2023-11-26 14:02:18 +0200

    FormateDates try#1

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,32 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const dateArray = date.split(fromFormat[fromFormat.length - 1]);
+  const dateObject = {};
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    dateObject[fromFormat[i]] = dateArray[i];
+  };
+
+  if (!('YY' in dateObject)) {
+    dateObject['YY'] = dateObject['YYYY'].slice(2);
+  };
+
+  if (!('YYYY' in dateObject)) {
+    if (dateObject['YY'] < '30') {
+      dateObject['YYYY'] = '20' + [dateObject['YY']];
+    } else {
+      dateObject['YYYY'] = '19' + [dateObject['YY']];
+    }
+  };
+
+  const convertArray = [];
+
+  for (let j = 0; j < toFormat.length - 1; j++) {
+    convertArray.push(dateObject[toFormat[j]]);
+  }
+
+  return convertArray.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
