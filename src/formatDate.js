2cbea69f4d46d294c036654b4d1051a75918cab3
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2023-12-03 20:02:49 +0100

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -51,6 +51,46 @@
 
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  // const [year, month, day] = date.split('');
+  let year = '';
+  let month = '';
+  let day = '';
+  const separator = fromFormat[3];
+
+  const findIndex = (dateFormat) => {
+    const dayIndex = dateFormat.indexOf('DD');
+    const monthIndex = dateFormat.indexOf('MM');
+    const yearIndex1 = dateFormat.indexOf('YY');
+    const yearIndex2 = dateFormat.indexOf('YYYY');
+    const yearIndex = yearIndex1 === -1 ? yearIndex2 : yearIndex1;
+    const yearLength = yearIndex1 === -1 ? 4 : 2;
+
+    return [dayIndex, monthIndex, yearIndex, yearLength];
+  };
+
+  const indexFromFormat = findIndex(fromFormat);
+  const indexToFormat = findIndex(toFormat);
+
+  const dateArray = date.split(separator);
+
+  day = dateArray[indexFromFormat[0]];
+  month = dateArray[indexFromFormat[1]];
+  year = dateArray[indexFromFormat[2]];
+
+  if (indexFromFormat[3] === 4 && indexToFormat[3] === 2) {
+    year = year.slice(2, 4);
+  }
+
+  if (indexFromFormat[3] === 2 && indexToFormat[3] === 4) {
+    year = Number(year) < 30 ? '20' + year : '19' + year;
+  }
+
+  const dmyArray = [day, month, year];
+  const resultArray = [dmyArray[indexToFormat.indexOf(0)],
+    dmyArray[indexToFormat.indexOf(1)],
+    dmyArray[indexToFormat.indexOf(2)]];
+
+  return resultArray.join(toFormat[3]);
 }
 
 module.exports = formatDate;
