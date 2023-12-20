a23dfbf66c8e6a8f96faffaa580b704cab24dd14
Nikita <2004nnikita@gmail.com>
2023-12-20 21:37:49 +0100

    2

--- src/formatDate.js
+++ src/formatDate.js
@@ -48,31 +48,24 @@
  *
  * @returns {string}
  */
-
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-  const fromFormatArr = date.split(`${fromFormat[3]}`)
+  const fromFormatArr = date.split(`${fromFormat[3]}`);
   const finalArrToStr = [];
   const separator = toFormat.pop();
+
   for (let i = 0; i < toFormat.length; i++) {
-    for (let j = 0; j < fromFormat.length - 1; j++ ) {
-      if (toFormat[i] === fromFormat[j]) {
-        finalArrToStr.push(fromFormatArr[j]);
-        break;
-      }
-      if (toFormat[i] === 'YYYY' && finalArrToStr.length !== 3) {
-       if (fromFormat[j] === 'YY' && (+fromFormatArr[j]) < 30) {
-        finalArrToStr.push(`20${fromFormatArr[j]}`);
-        break;
-       } else {
-        finalArrToStr.push(`19${fromFormatArr[j]}`);
-       }
-      }
-      if (toFormat[i] === 'YY' && finalArrToStr.length !== 3) {
-        if (fromFormat[j] === 'YYYY') {
-         finalArrToStr.push(`${fromFormatArr[j].slice(2)}`);
-        }
-       }
+    const toFormatElement = toFormat[i];
+    const fromIndex = fromFormat.indexOf(toFormatElement);
+
+    if (fromIndex !== -1) {
+      finalArrToStr.push(fromFormatArr[fromIndex]);
+    } else if (toFormatElement === 'YYYY' && finalArrToStr.length !== 3) {
+      const yearIndex = fromFormat.indexOf('YY');
+      const yearValue = +fromFormatArr[yearIndex];
+      finalArrToStr.push(yearValue < 30 ? `20${yearValue}` : `19${yearValue}`);
+    } else if (toFormatElement === 'YY' && finalArrToStr.length !== 3) {
+      const yearIndex = fromFormat.indexOf('YYYY');
+      finalArrToStr.push(`${fromFormatArr[yearIndex].slice(2)}`);
     }
   }
 
