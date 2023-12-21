ab181cfd24a220b9b0a14e0999ac0dc2ea758d6d
Voloshynd <Voloshynd13@gmail.com>
2023-12-21 12:48:46 +0100

    added variables with meaningful

--- src/formatDate.js
+++ src/formatDate.js
@@ -4,38 +4,43 @@ function formatDate(date, fromFormat, toFormat) {
   const ARRAY_DATE = date.split(fromFormat[fromFormat.length - 1]);
   const NEW_SEPARATOR = toFormat[toFormat.length - 1];
   const NEW_FORMAT_DATE = [];
+  const DAY_FORMAT = 'DD';
+  const MONTH_FORMAT = 'MM';
+  const SHORT_FORMAT_YEAR = 'YY';
+  const LONG_FORMAT_YEAR = 'YYYY';
+
   let day = '';
   let month = '';
   let year = '';
 
   for (let i = 0; i < ARRAY_DATE.length; i++) {
-    if (fromFormat[i].includes('D')) {
+    if (fromFormat[i].includes(DAY_FORMAT)) {
       day = ARRAY_DATE[i];
     }
 
-    if (fromFormat[i].includes('M')) {
+    if (fromFormat[i].includes(MONTH_FORMAT)) {
       month = ARRAY_DATE[i];
     }
 
-    if (fromFormat[i].includes('Y')) {
+    if (fromFormat[i].includes(SHORT_FORMAT_YEAR)) {
       year = ARRAY_DATE[i];
     }
   }
 
   for (let i = 0; i < ARRAY_DATE.length; i++) {
-    if (toFormat[i].includes('D')) {
+    if (toFormat[i].includes(DAY_FORMAT)) {
       NEW_FORMAT_DATE[i] = day;
     }
 
-    if (toFormat[i].includes('M')) {
+    if (toFormat[i].includes(MONTH_FORMAT)) {
       NEW_FORMAT_DATE[i] = month;
     }
 
-    if (toFormat[i] === 'YY') {
+    if (toFormat[i] === SHORT_FORMAT_YEAR) {
       NEW_FORMAT_DATE[i] = year.slice(-2);
     }
 
-    if (toFormat[i] === 'YYYY' && +year < 100) {
+    if (toFormat[i] === LONG_FORMAT_YEAR && +year < 100) {
       if (+year < 30) {
         year = `20${year}`;
       } else {
@@ -43,7 +48,7 @@ function formatDate(date, fromFormat, toFormat) {
       }
     }
 
-    if (toFormat[i] === 'YYYY') {
+    if (toFormat[i] === LONG_FORMAT_YEAR) {
       NEW_FORMAT_DATE[i] = year;
     }
   }
