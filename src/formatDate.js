275b42432b2b6c377211ba18d05d232ace8dc5aa
Hlib Kaponkin <glebkapenkin@gmail.com>
2024-01-07 14:32:17 +0200

    fixed code

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,13 +50,14 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const OldDate = date.split(fromFormat[3]);
+  const oldDate = date.split(fromFormat[3]);
   const newDate = [];
   const oldFormat = {};
+  const dateLength = 3;
   let year;
 
-  for (let i = 0; i < 3; i++) {
-    oldFormat[fromFormat[i]] = OldDate[i];
+  for (let i = 0; i < dateLength; i++) {
+    oldFormat[fromFormat[i]] = oldDate[i];
   }
 
   if (oldFormat.YYYY) {
@@ -65,11 +66,11 @@ function formatDate(date, fromFormat, toFormat) {
     year = oldFormat.YY;
   }
 
-  if (fromFormat.includes('YYYY') & toFormat.includes('YY')) {
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
     year = oldFormat.YYYY.slice(2);
   }
 
-  if (fromFormat.includes('YY') & toFormat.includes('YYYY')) {
+  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
     if (year >= 30) {
       year = 19 + year;
     } else {
@@ -77,7 +78,7 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < dateLength; i++) {
     if (toFormat.indexOf('DD') === i) {
       newDate.push(oldFormat.DD);
     }
