c5a1522ce642531a8c0675f579c1ab1655b6528c
Andrew K <Fisdyi@gmail.com>
2024-01-30 17:01:53 +0200

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,50 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldDate = date.split(fromFormat[3]);
+  const newDate = [];
+
+  const ddIndexFrom = fromFormat.indexOf('DD');
+  const ddIndexTo = toFormat.indexOf('DD');
+
+  newDate[ddIndexTo] = oldDate[ddIndexFrom];
+
+  const mmIndexFrom = fromFormat.indexOf('MM');
+  const mmIndexTo = toFormat.indexOf('MM');
+
+  newDate[mmIndexTo] = oldDate[mmIndexFrom];
+
+  let yyIndexFrom;
+  let yyIndexTo;
+
+  switch (true) {
+    case fromFormat.includes('YYYY') && toFormat.includes('YYYY'):
+      yyIndexFrom = fromFormat.indexOf('YYYY');
+      yyIndexTo = toFormat.indexOf('YYYY');
+
+      newDate[yyIndexTo] = oldDate[yyIndexFrom];
+      break;
+
+    case fromFormat.includes('YY') && toFormat.includes('YYYY'):
+      yyIndexFrom = fromFormat.indexOf('YY');
+      yyIndexTo = toFormat.indexOf('YYYY');
+
+      if (+oldDate[yyIndexFrom] < 30) {
+        newDate[yyIndexTo] = '20' + oldDate[yyIndexFrom];
+      } else {
+        newDate[yyIndexTo] = '19' + oldDate[yyIndexFrom];
+      }
+      break;
+
+    case fromFormat.includes('YYYY') && toFormat.includes('YY'):
+      yyIndexFrom = fromFormat.indexOf('YYYY');
+      yyIndexTo = toFormat.indexOf('YY');
+
+      newDate[yyIndexTo] = oldDate[yyIndexFrom].slice(-2);
+      break;
+  }
+
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
