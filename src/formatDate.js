cff290785dcc9640d4908203af169d6a97f71d91
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-05 18:07:25 +0200

    resolve task

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,66 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const FROM_SEPARATOR = fromFormat[fromFormat.length - 1];
+  const TO_SEPARATOR = toFormat[toFormat.length - 1];
+
+  const DAY = 'DD';
+  const MONTH = 'MM';
+  const SHORT_YEAR = 'YY';
+  const FULL_YEAR = 'YYYY';
+
+  let dayValue;
+  let monthValue;
+  let yearValue;
+
+  const dateArray = date.split(FROM_SEPARATOR);
+  const result = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    switch (fromFormat[i]) {
+      case DAY:
+        dayValue = dateArray[i];
+        break;
+      case MONTH:
+        monthValue = dateArray[i];
+        break;
+      case SHORT_YEAR:
+        yearValue = dateArray[i];
+        break;
+      case FULL_YEAR:
+        yearValue = dateArray[i];
+        break;
+    }
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    switch (toFormat[i]) {
+      case DAY:
+        result.push(dayValue);
+        break;
+      case MONTH:
+        result.push(monthValue);
+        break;
+      case SHORT_YEAR:
+        if (yearValue.length > 2) {
+          yearValue = yearValue.slice(2);
+        }
+        result.push(yearValue);
+        break;
+      case FULL_YEAR:
+        if (yearValue.length < 4) {
+          if (+yearValue < 30) {
+            yearValue = `20${yearValue}`;
+          } else {
+            yearValue = `19${yearValue}`;
+          }
+        }
+        result.push(yearValue);
+        break;
+    }
+  }
+
+  return result.join(TO_SEPARATOR);
 }
 
 module.exports = formatDate;
