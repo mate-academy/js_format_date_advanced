59185fef14370808cc666c1f1116ba6b386f3d6f
Igor Novikov <novikov.igor.20@gmail.com>
2024-02-18 23:38:15 +0200

    format_date

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,34 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separator = fromFormat[fromFormat.length - 1];
+  const dateParts = date.split(separator);
+  const changeDataParts = [];
+  const dateYear = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateYear[fromFormat[i]] = dateParts[i];
+  }
+
+  if (fromFormat.includes('YYYY')) {
+    dateYear['YY'] = dateYear['YYYY'].slice(-2);
+  }
+
+  if (fromFormat.includes('YY')) {
+    const dateYearShort = parseInt(dateYear['YY']);
+
+    dateYear['YYYY'] = dateYearShort < 30
+      ? `20${dateYear['YY']}`
+      : `19${dateYear['YY']}`;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    changeDataParts.push(dateYear[toFormat[i]]);
+  }
+
+  const formattedDate = changeDataParts.join(toFormat[toFormat.length - 1]);
+
+  return formattedDate;
 }
 
 module.exports = formatDate;
