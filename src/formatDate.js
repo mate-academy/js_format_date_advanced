2fa109d852c0be7114f71049b5be897271861974
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-03-02 11:37:33 +0200

    added needed implementation

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,65 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const SEPARATOR_INDEX = 3;
+  const DAYS_FORMAT = 'DD';
+  const MONTH_FORMAT = 'MM';
+  const TWENTY = '20';
+  const NINETEEN = '19';
+  const SHORT_YEAR_FORMAT = 'YY';
+  const LONG_YEAR_FORMAT = 'YYYY';
+  const SHORT_YEAR_FORMAT_LENGTH = 2;
+  const LONG_YEAR_FORMAT_LENGTH = 4;
+  const partsOfDate = date.split(fromFormat[SEPARATOR_INDEX]);
+  const resultArray = [];
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i] === SHORT_YEAR_FORMAT || toFormat[i] === LONG_YEAR_FORMAT) {
+      const indexOfYear =
+        fromFormat.indexOf(SHORT_YEAR_FORMAT) !== -1
+          ? fromFormat.indexOf(SHORT_YEAR_FORMAT)
+          : fromFormat.indexOf(LONG_YEAR_FORMAT);
+      const yearFromDate = partsOfDate[indexOfYear];
+
+      if (
+        toFormat[i].length === SHORT_YEAR_FORMAT_LENGTH &&
+        yearFromDate.length === LONG_YEAR_FORMAT_LENGTH
+      ) {
+        resultArray[i] = yearFromDate.slice(SHORT_YEAR_FORMAT_LENGTH);
+        continue;
+      }
+
+      if (
+        toFormat[i].length === LONG_YEAR_FORMAT_LENGTH &&
+        yearFromDate.length === SHORT_YEAR_FORMAT_LENGTH
+      ) {
+        if (+yearFromDate < 30) {
+          resultArray[i] = TWENTY + yearFromDate;
+        } else {
+          resultArray[i] = NINETEEN + yearFromDate;
+        }
+        continue;
+      }
+
+      resultArray[i] = yearFromDate;
+    }
+
+    if (toFormat[i] === MONTH_FORMAT) {
+      const indexOfMonth = fromFormat.indexOf(MONTH_FORMAT);
+      const monthFromDate = partsOfDate[indexOfMonth];
+
+      resultArray[i] = monthFromDate;
+    }
+
+    if (toFormat[i] === DAYS_FORMAT) {
+      const indexOfDays = fromFormat.indexOf(DAYS_FORMAT);
+      const daysFromDate = partsOfDate[indexOfDays];
+
+      resultArray[i] = daysFromDate;
+    }
+  }
+
+  return resultArray.join(toFormat[SEPARATOR_INDEX]);
 }
 
 module.exports = formatDate;
