3c4eba292f417f8c2400c526e187e48351cfe321
andreinik1 <125960028+andreinik1@users.noreply.github.com>
2024-03-03 18:52:39 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -50,7 +50,48 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const splittedData = date.split(`${fromFormat[3]}`);
+  const newData = new Array(toFormat.length).fill('');
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    const dataElement = fromFormat[i];
+
+    if (dataElement === 'DD') {
+      const dayIndexToFinding = i;
+      const dayIndexToAdding = toFormat.indexOf('DD');
+
+      newData[dayIndexToAdding] = splittedData[dayIndexToFinding];
+    } else if (dataElement === 'MM') {
+      const monthIndexToFinding = i;
+      const monthIndexToAdding = toFormat.indexOf('MM');
+
+      newData[monthIndexToAdding] = splittedData[monthIndexToFinding];
+    } else if (dataElement === 'YY' || dataElement === 'YYYY') {
+      let year = splittedData[i];
+
+      for (let j = 0; j < toFormat.length; j++) {
+        const yearFormatElement = toFormat[j];
+
+        if (yearFormatElement === 'YY') {
+          const yearIndexToAdding = j;
+
+          newData[yearIndexToAdding] = year.slice(-2);
+        } else if (yearFormatElement === 'YYYY') {
+          const yearIndexToAdding = j;
+
+          if (year.length === 2 && year < 30) {
+            year = `20${year}`;
+          } else if (year.length === 2) {
+            year = `19${year}`;
+          }
+
+          newData[yearIndexToAdding] = year;
+        }
+      }
+    }
+  }
+
+  return newData.join(`${toFormat[3]}`).slice(0, -1);
 }
 
 module.exports = formatDate;
