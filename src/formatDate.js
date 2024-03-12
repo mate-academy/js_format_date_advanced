36b49f470d554ef061b0a2b91ba823b2c84bd0e4
dmytrochernega <dimadneprfdb@gmail.coma>
2024-03-12 20:14:09 +0200

    d

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,44 +1,7 @@
 'use strict';
-
-/**
+ /**
  *   Time flies, standards change. Let's get rid of the routine of changing the
- * date format. Create a `formatDate` function that accepts the `date` string,
- * the old `fromFormat` array and the new `toFormat` array. Function returns
- * given date in new format.
- *   The function can change a separator, reorder the date parts of convert a
- * year from 4 digits to 2 digits and back.
- *   When converting from YYYY to YY just use 2 last digit (1997 -> 97).
- *   When converting from YY to YYYY use 20YY if YY < 30 and 19YY otherwise.
- *
- * Examples:
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '2020-02-18',
- *   ['YYYY', 'MM', 'DD', '-'],
- *   ['DD', 'MM', 'YYYY', '.'],
- * ) // '18.02.2020'
- *
- * formatDate(
- *   '18-02-2020',
- *   ['DD', 'MM', 'YYYY', '-'],
- *   ['DD', 'MM', 'YY', '/'],
- * ) // '18/02/20'
- *
- * formatDate(
- *   '20/02/18',
- *   ['YY', 'MM', 'DD', '/'],
- *   ['YYYY', 'MM', 'DD', '.'],
- * ) // '2020.02.18'
- *
- * formatDate(
- *   '97/02/18',
- *   ['YY', 'MM', 'DD', '/'],
+@@ -42,15 +42,59 @@
  *   ['DD', 'MM', 'YYYY', '.'],
  * ) // '18.02.1997'
  *
@@ -49,8 +12,39 @@
  * @returns {string}
  */
 
-function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  function formatDate(date, fromFormat, toFormat) {
+    const currentDateArr = date.split(fromFormat[fromFormat.length - 1]);
+
+    const getIndex = (arr, type) => arr.findIndex(el => el.includes(type));
+
+    const oldYearFormatIndex = getIndex(fromFormat, 'Y')
+    const newYearFormatIndex = getIndex(toFormat, 'Y')
+
+    const oldMonthFormatIndex = getIndex(fromFormat, 'M')
+    const newMonthFormatIndex = getIndex(toFormat, 'M')
+
+    const oldDayFormatIndex = getIndex(fromFormat, 'D')
+    const newDayFormatIndex = getIndex(toFormat, 'D')
+
+    let year = currentDateArr[oldYearFormatIndex];
+
+    if (fromFormat[oldYearFormatIndex].length > toFormat[newYearFormatIndex].length) {
+        year = year.slice(-2)
+    }   else if (fromFormat[oldYearFormatIndex].length < toFormat[newYearFormatIndex].length) {
+        year = `${year < 30 ? 20 : 19}${year}`
+     }
+        const res = []
+
+        res[newYearFormatIndex] = year
+        res[newMonthFormatIndex] = currentDateArr[oldMonthFormatIndex]
+        res[newDayFormatIndex] = currentDateArr[oldDayFormatIndex]
+
+        return res.join(toFormat[toFormat.length -1])
+
+
+    }
+
 
 module.exports = formatDate;
+
+   
