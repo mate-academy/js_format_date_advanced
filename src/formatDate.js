280a2f0618a50da1704ba9207bdffbb4cb5f2d18
Aleks-ars <dragon.tyraxes@gmail.com>
2024-03-15 14:03:43 +0200

    improved code

--- src/formatDate.js
+++ src/formatDate.js
@@ -11,50 +11,35 @@ function formatDate(date, fromFormat, toFormat) {
   const oldSeparator = fromFormat.slice(-1);
   const newSeparator = toFormat.slice(-1);
   const oldDate = date.split(oldSeparator);
-  let newFormatDate = [];
-  let dateIndex = [];
+  const newFormatDate = [];
+  const dateObject = {};
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    for (let f = 0; f < fromFormat.length - 1; f++) {
-      if (fromFormat[f] === toFormat[i]) {
-        dateIndex = fromFormat.indexOf(fromFormat[f]);
-
-        newFormatDate.push(oldDate.slice(dateIndex, dateIndex + 1));
-
-        if (newFormatDate.length === 3) {
-          newFormatDate = newFormatDate.join(newSeparator);
-
-          return newFormatDate;
-        }
-      }
-    }
+  // Convert the array to an object, it will simplify the
+  // movement of dates according to the new format.
+  for (let i = 0; i < oldDate.length; i++) {
+    dateObject[fromFormat[i]] = oldDate[i];
   }
 
-  let formatYear = '';
-
-  if (toFormat.indexOf('YY') === 2) {
-    dateIndex = fromFormat.indexOf('YYYY');
-    formatYear = oldDate.slice(dateIndex, dateIndex + 1);
-    formatYear = formatYear.join().slice(2);
-    newFormatDate.push(formatYear);
-  } else if (toFormat.indexOf('YYYY') === 0) {
-    dateIndex = fromFormat.indexOf('YY');
-    formatYear = oldDate.slice(dateIndex, dateIndex + 1);
-
-    if (formatYear < 30) {
-      formatYear.unshift('20');
-      formatYear = formatYear.join('');
-      newFormatDate.unshift(formatYear);
-    } else if (formatYear >= 30) {
-      formatYear.unshift('19');
-      formatYear = formatYear.join('');
-      newFormatDate.unshift(formatYear);
-    }
+  // I check whether the object has a year in 'YYYY' format
+  // and add it in 'YY' format if the new format requires it.
+  if (fromFormat.includes('YYYY')) {
+    dateObject.YY = dateObject.YYYY.slice(2);
   }
 
-  newFormatDate = newFormatDate.join(newSeparator);
+  // In the second check, I do the same as in the first, but I also
+  // check to determine which year to enter.
+  if (dateObject.YY < 30) {
+    dateObject.YYYY = 20 + dateObject.YY;
+  } else if (dateObject.YY >= 30) {
+    dateObject.YYYY = 19 + dateObject.YY;
+  }
 
-  return newFormatDate;
+  // Using a loop to place dates in the new format.
+  for (let n = 0; n < toFormat.length - 1; n++) {
+    newFormatDate.push(dateObject[toFormat[n]]);
+  }
+
+  return newFormatDate.join(newSeparator);
 }
 
 module.exports = formatDate;
