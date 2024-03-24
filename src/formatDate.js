92c7353a6091b4cf0a255fef6671469e4d977616
metrakon <metrakon@proton.me>
2024-03-24 20:59:52 +0200

    add_solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,38 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const newArr = [];
+  const dateSplit = date.split(fromFormat[3]);
+  const day = dateSplit[fromFormat.indexOf('DD')];
+  const month = dateSplit[fromFormat.indexOf('MM')];
+
+  if (fromFormat.indexOf('YYYY') !== -1 && toFormat.indexOf('YYYY') !== -1) {
+    newArr[toFormat.indexOf('YYYY')] = dateSplit[fromFormat.indexOf('YYYY')];
+  }
+
+  if (fromFormat.indexOf('YY') !== -1 && toFormat.indexOf('YYYY') !== -1) {
+    if (dateSplit[fromFormat.indexOf('YY')] < 30) {
+      newArr[toFormat.indexOf('YYYY')] =
+        '20' + dateSplit[fromFormat.indexOf('YY')];
+    } else {
+      newArr[toFormat.indexOf('YYYY')] =
+        '19' + dateSplit[fromFormat.indexOf('YY')];
+    }
+  }
+
+  if (fromFormat.indexOf('YYYY') !== -1 && toFormat.indexOf('YY') !== -1) {
+    newArr[toFormat.indexOf('YY')] =
+      dateSplit[fromFormat.indexOf('YYYY')].slice(-2);
+  }
+
+  if (fromFormat.indexOf('YY') !== -1 && toFormat.indexOf('YY') !== -1) {
+    newArr[toFormat.indexOf('YY')] = dateSplit[fromFormat.indexOf('YY')];
+  }
+
+  newArr[toFormat.indexOf('DD')] = day;
+  newArr[toFormat.indexOf('MM')] = month;
+
+  return newArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
