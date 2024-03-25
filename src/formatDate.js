051974dd3e2a9db828f376ba0c04308f798b8d84
Valentyn Sobol <valiksobol14@gmail.com>
2024-03-25 17:24:10 +0200

    have a problem with years

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,30 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldDate = date.split(fromFormat[fromFormat.length - 1]);
+  const obj = {};
+  const result = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    const key = fromFormat[i];
+    const value = oldDate[i];
+
+    obj[key] = value;
+  }
+
+  for (const part of toFormat) {
+    if (part === 'YY' && obj[part] < 30) {
+      result.push(`20${obj[part]}`);
+    } else if (part === 'YY' && obj[part] >= 30) {
+      result.push(`19${obj[part]}`);
+    } else if (part === 'YYYY' || part === 'DD' || part === 'MM') {
+      result.push(obj[part]);
+    }
+  }
+
+  const answer = result.join(toFormat[toFormat.length - 1]);
+
+  return answer;
 }
 
 module.exports = formatDate;
