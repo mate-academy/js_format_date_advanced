bd318103ddd8d6c458a7af27f9ac1c7bfbe587b6
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-03-28 15:45:35 +0200

    VB FormatDate Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,44 @@
  *
  * @returns {string}
  */
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const parts = date.split(fromFormat[fromFormat.length - 1]);
+  const dateObj = {};
+  const result = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    dateObj[fromFormat[i]] = parts[i];
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i] === 'YY' || toFormat[i] === 'YYYY') {
+      const prevYearFormat = dateObj['YY'] ? dateObj['YY'] : dateObj['YYYY'];
+      const newYearFormat = toFormat[i];
+
+      if (newYearFormat.length === 4 && prevYearFormat.length === 2) {
+        if (+prevYearFormat < 30) {
+          result.push(`20${prevYearFormat}`);
+        } else {
+          result.push(`19${prevYearFormat}`);
+        }
+
+        continue;
+      }
+
+      if (newYearFormat.length === 2 && prevYearFormat.length === 4) {
+        result.push(prevYearFormat.slice(2));
+        continue;
+      }
+
+      result.push(prevYearFormat);
+      continue;
+    }
+
+    result.push(dateObj[toFormat[i]]);
+  }
+
+  return result.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
