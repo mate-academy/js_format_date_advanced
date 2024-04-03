f413f8e7bbce3ec92700bf7b6b8c5fb978068cd9
denys <denys.sheremeta.web@gmail.com>
2024-04-03 13:46:45 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,14 +1,48 @@
-'use strict';
-
-/**
- * @param {string} date
- * @param {string[]} fromFormat
- * @param {string[]} toFormat
- *
- * @returns {string}
- */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldSeparator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const dateArr = date.split(oldSeparator);
+
+  const dayIndex = fromFormat.indexOf('DD');
+  const monthIndex = fromFormat.indexOf('MM');
+  const yearIndex = fromFormat.findIndex(
+    (format) => format === 'YY' || format === 'YYYY',
+  );
+
+  const day = dateArr[dayIndex];
+  const month = dateArr[monthIndex];
+  let year = dateArr[yearIndex];
+
+  const currentYear = new Date().getFullYear() % 100;
+
+  if (fromFormat[yearIndex] === 'YY') {
+    if (parseInt(year) <= currentYear) {
+      year = '20' + year.padStart(2, '0');
+    } else {
+      year = '19' + year.padStart(2, '0');
+    }
+  }
+
+  const result = [];
+
+  for (const el of toFormat) {
+    switch (el) {
+      case 'DD':
+        result.push(day);
+        break;
+      case 'MM':
+        result.push(month);
+        break;
+      case 'YYYY':
+        result.push(year);
+        break;
+      case 'YY':
+        result.push(year.slice(-2));
+        break;
+    }
+  }
+
+  return result.join(newSeparator);
 }
 
 module.exports = formatDate;
