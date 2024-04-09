23218e135c002f10ff1043b1ccfa44f738da397d
denys <denys.sheremeta.web@gmail.com>
2024-04-09 10:50:28 +0300

    implement constants

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const FORMATS = {
+  YEAR_LONG: 'YYYY',
+  YEAR_SHORT: 'YY',
+};
+
+const CENTURIES = {
+  TWENTIETH: '19',
+  TWENTY_FIRST: '20',
+};
+
 function formatDate(date, fromFormat, toFormat) {
   const oldSeparator = fromFormat[3];
   const newSeparator = toFormat[3];
@@ -8,7 +18,7 @@ function formatDate(date, fromFormat, toFormat) {
   const dayIndex = fromFormat.indexOf('DD');
   const monthIndex = fromFormat.indexOf('MM');
   const yearIndex = fromFormat.findIndex(
-    (format) => format === 'YY' || format === 'YYYY',
+    (format) => format === FORMATS.YEAR_SHORT || format === FORMATS.YEAR_LONG,
   );
 
   const day = dateArr[dayIndex];
@@ -17,11 +27,11 @@ function formatDate(date, fromFormat, toFormat) {
 
   const currentYear = new Date().getFullYear() % 100;
 
-  if (fromFormat[yearIndex] === 'YY') {
+  if (fromFormat[yearIndex] === FORMATS.YEAR_SHORT) {
     if (parseInt(year) <= currentYear) {
-      year = '20' + year.padStart(2, '0');
+      year = CENTURIES.TWENTY_FIRST + year.padStart(2, '0');
     } else {
-      year = '19' + year.padStart(2, '0');
+      year = CENTURIES.TWENTIETH + year.padStart(2, '0');
     }
   }
 
@@ -35,10 +45,10 @@ function formatDate(date, fromFormat, toFormat) {
       case 'MM':
         result.push(month);
         break;
-      case 'YYYY':
+      case FORMATS.YEAR_LONG:
         result.push(year);
         break;
-      case 'YY':
+      case FORMATS.YEAR_SHORT:
         result.push(year.slice(-2));
         break;
     }
