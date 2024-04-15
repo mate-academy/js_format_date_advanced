3967183bd5486d23f31dbced00272e5c88b810fe
Marina Avramenko <marina.avramenko123@gmail.com>
2024-04-15 23:10:39 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,29 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const result = [];
+  const object = {};
+  const splitDate = date.split(fromFormat[fromFormat.length - 1]);
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    object[fromFormat[i]] = splitDate[i];
+  }
+
+  if (object.hasOwnProperty('YYYY')) {
+    object.YY = object.YYYY.slice(2);
+  }
+
+  if (object.YY >= 30) {
+    object.YYYY = '19' + object.YY;
+  } else {
+    object.YYYY = '20' + object.YY;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    result.push(object[toFormat[i]]);
+  }
+
+  return result.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
