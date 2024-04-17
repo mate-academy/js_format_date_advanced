42cbe973e5b0eab9878d18ef1bab84f625cf84a6
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-17 15:29:42 +0300

    solution 2

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,36 +8,36 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  let resultDate = '';
-  const objectDateFormat = {};
-  const dateSplitted = date.split(fromFormat[fromFormat.length - 1]);
+  const SEPARATOR = fromFormat[fromFormat.length - 1];
+  const JOINER = toFormat[toFormat.length - 1];
+  const DATEPARTS = date.split(SEPARATOR);
 
-  for (let i = 0; i < fromFormat.length - 1; i++) {
-    const less = dateSplitted[i] < 30;
-    const more = dateSplitted[i] >= 30;
-
-    if (fromFormat[i] === 'YY' && toFormat.includes('YYYY') && less) {
-      objectDateFormat.YYYY = '20' + dateSplitted[i];
-    } else if (fromFormat[i] === 'YY' && toFormat.includes('YYYY') && more) {
-      objectDateFormat.YYYY = '19' + dateSplitted[i];
-    }
-
-    if (fromFormat[i] === 'YYYY' && toFormat.includes('YY')) {
-      objectDateFormat.YY = dateSplitted[i][2] + dateSplitted[i][3];
-    } else {
-      objectDateFormat[fromFormat[i]] = dateSplitted[i];
-    }
+  while (DATEPARTS.length > 1 && DATEPARTS[DATEPARTS.length - 1] === '') {
+    DATEPARTS.pop();
   }
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    if (i > 0) {
-      resultDate += toFormat[toFormat.length - 1];
-    }
+  const DATEMAP = {};
 
-    resultDate += objectDateFormat[toFormat[i]];
+  fromFormat.forEach((part, index) => {
+    DATEMAP[part] = DATEPARTS[index];
+  });
+
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    DATEMAP['YY'] = DATEMAP['YYYY'].slice(-2);
   }
 
-  return resultDate;
+  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    const FULLYEAR
+    = parseInt(DATEMAP['YY']) < 30
+      ? '20' + DATEMAP['YY']
+      : '19' + DATEMAP['YY'];
+
+    DATEMAP['YYYY'] = FULLYEAR;
+  }
+
+  const FORMATTEDDATE = toFormat.map((part) => DATEMAP[part]);
+
+  return FORMATTEDDATE.slice(0, -1).join(JOINER);
 }
 
 module.exports = formatDate;
