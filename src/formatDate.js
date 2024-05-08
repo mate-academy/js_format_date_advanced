9f274652d3b3af7ce688bbd5b54096dd54da983d
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-05-08 17:50:16 +0300

    add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,47 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const splited = date.split(fromFormat[3]);
 
+  splited.push(fromFormat[3]);
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+      const index = fromFormat.indexOf('YYYY');
+
+      fromFormat[index] = 'YY';
+
+      splited[index] = splited[index]
+        .split('')
+        .slice(2, splited.length)
+        .join('');
+    } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+      const index = fromFormat.indexOf('YY');
+
+      fromFormat[index] = 'YYYY';
+
+      if (splited[index] < 30) {
+        splited[index] = '20' + splited[index];
+      } else {
+        splited[index] = '19' + splited[index];
+      }
+    }
+
+    if (toFormat[i] === fromFormat[i]) {
+      toFormat[i] = splited[i];
+    } else {
+      for (let j = 0; j < fromFormat.length; j++) {
+        if (toFormat[i] === fromFormat[j]) {
+          toFormat[i] = splited[j];
+        }
+      }
+    }
+  }
+
+  const joiner = toFormat.slice(-1);
+
+  const result = toFormat.slice(0, 3).join(joiner[0]);
+
+  return result;
+}
 module.exports = formatDate;
