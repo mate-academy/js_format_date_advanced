e9c727aa1e46bdcd40d004269ae33f594cc4e5f3
Nataliia <gzodchie@gmail.com>
2024-05-22 00:24:14 +0300

    add format date task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,53 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateArr = date.split(fromFormat[3]);
+  let year;
+  let month;
+  let day;
+  const resArr = [];
+
+  for (let i = 0; i < 3; i++) {
+    if (fromFormat[i] === 'YYYY' || fromFormat[i] === 'YY') {
+      year = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'DD') {
+      day = dateArr[i];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i] === 'YYYY') {
+      if (year.length === 4) {
+        resArr[i] = year;
+      } else {
+        resArr[i] = year < 30 ? +('20' + year) : +('19' + year);
+      }
+    }
+
+    if (toFormat[i] === 'YY') {
+      if (year.length === 2) {
+        resArr[i] = year;
+      } else {
+        resArr[i] = year.slice(2);
+      }
+    }
+
+    if (toFormat[i] === 'MM') {
+      resArr[i] = month;
+    }
+
+    if (toFormat[i] === 'DD') {
+      resArr[i] = day;
+    }
+  }
+
+  return resArr.join(toFormat[3]);
 }
 
 module.exports = formatDate;
