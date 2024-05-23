04f5579c51e6b8315eca6bd6bc5c1b2df5525285
Khmeletskyy <khmeletsky@gmail.com>
2024-05-23 16:37:33 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -14,7 +14,6 @@ function formatDate(date, fromFormat, toFormat) {
   const dateParts = date.split(fromSeparator);
   let year, month, day;
 
-  // Extract date parts according to fromFormat
   fromFormat.forEach((format, index) => {
     if (format === 'YYYY' || format === 'YY') {
       year = dateParts[index];
@@ -25,28 +24,16 @@ function formatDate(date, fromFormat, toFormat) {
     }
   });
 
-  // Convert year formats if necessary
-  if (fromFormat[0] === 'YYYY' && toFormat[0] === 'YY') {
-    year = year.slice(-2);
-  } else if (fromFormat[0] === 'YY' && toFormat[0] === 'YYYY') {
-    year = parseInt(year, 10) < 30 ? '20' + year : '19' + year;
-  }
-
-  // Create a mapping for easy access to the date parts
   const dateMap = {
-    YYYY:
-      year.length === 2
-        ? parseInt(year, 10) < 30
-          ? '20' + year
-          : '19' + year
-        : year,
+    YYYY: year.length === 2 ? (year < '30' ? '20' + year : '19' + year) : year,
     YY: year.length === 4 ? year.slice(-2) : year,
     MM: month,
     DD: day,
   };
 
-  // Construct the new date format
-  const newDateParts = toFormat.slice(0, 3).map((format) => dateMap[format]);
+  const newDateParts = toFormat
+    .filter((format) => format !== toSeparator)
+    .map((format) => dateMap[format]);
 
   return newDateParts.join(toSeparator);
 }
