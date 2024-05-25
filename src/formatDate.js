d71d53d36214bbf003009ff47151bc7fe61a5013
Bohdan Klymeniuk <klymeniuk.bohdan@gmail.com>
2024-05-25 21:48:17 +0200

    made function formatDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,31 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateObject = {};
+  const separator = fromFormat[3];
+  const newSeparator = toFormat[3];
+  const splitedDate = date.split(separator);
+  const result = [];
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    dateObject[fromFormat[i]] = splitedDate[i];
+  }
+
+  if (dateObject.hasOwnProperty('YYYY')) {
+    dateObject.YY = dateObject.YYYY.slice(2);
+  }
+
+  if (dateObject.YY >= 30) {
+    dateObject.YYYY = '19' + dateObject.YY;
+  } else {
+    dateObject.YYYY = '20' + dateObject.YY;
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    result.push(dateObject[toFormat[i]]);
+  }
+
+  return result.join(newSeparator);
 }
 
 module.exports = formatDate;
