60fc2a2a2d05d6d80f1cedc73d54db5a63b2ae3a
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-06-13 17:53:00 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,66 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldYear = yearFormatAndPlace(fromFormat);
+  const newYear = yearFormatAndPlace(toFormat);
+
+  const formatedDate = [];
+  let year = '';
+
+  const dateParts = date.split(fromFormat[3]);
+  const newDateSeparator = toFormat[3];
+
+  const thresholdYear = 30;
+
+  if (oldYear.format !== newYear.format) {
+    if (oldYear.format.length < newYear.format.length) {
+      year =
+        dateParts[oldYear.place] < thresholdYear
+          ? `20${dateParts[oldYear.place]}`
+          : `19${dateParts[oldYear.place]}`;
+    } else {
+      year = dateParts[oldYear.place].slice(2);
+    }
+  } else {
+    year = dateParts[oldYear.place];
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY':
+      case 'YY':
+        formatedDate[i] = year;
+        break;
+      case 'MM':
+        formatedDate[i] = dateParts[fromFormat.indexOf('MM')];
+        break;
+      case 'DD':
+        formatedDate[i] = dateParts[fromFormat.indexOf('DD')];
+        break;
+    }
+  }
+
+  return formatedDate.join(newDateSeparator);
+}
+
+function yearFormatAndPlace(pattern) {
+  const longYearFormat = 'YYYY';
+  const shortYearFormat = 'YY';
+  let format = '';
+  let place = -1;
+
+  for (let i = 0; i < pattern.length; i++) {
+    if (pattern[i] === longYearFormat || pattern[i] === shortYearFormat) {
+      format = pattern[i];
+      place = i;
+      break;
+    }
+  }
+
+  return {
+    format,
+    place,
+  };
 }
 
 module.exports = formatDate;
