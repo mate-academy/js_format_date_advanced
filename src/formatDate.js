3b9075e8d078db356ee41e4002886af9638d223b
HunchakAndrii <andreygunchak1@gmail.com>
2024-06-19 22:26:29 +0300

    make correct formatDate function

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,61 @@
  *
  * @returns {string}
  */
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const newSeparator = toFormat[toFormat.length - 1];
+  const oldSeparator = fromFormat[fromFormat.length - 1];
+
+  const dateParts = date.split(oldSeparator);
+
+  const yearIndexFrom =
+    fromFormat.indexOf('YYYY') !== -1
+      ? fromFormat.indexOf('YYYY')
+      : fromFormat.indexOf('YY');
+  const yearIndexTo =
+    toFormat.indexOf('YYYY') !== -1
+      ? toFormat.indexOf('YYYY')
+      : toFormat.indexOf('YY');
+  const monthIndexFrom = fromFormat.indexOf('MM');
+  const dayIndexFrom = fromFormat.indexOf('DD');
+
+  let year = dateParts[yearIndexFrom];
+  const month = dateParts[monthIndexFrom];
+  const day = dateParts[dayIndexFrom];
+
+  if (fromFormat[yearIndexFrom] === 'YY' && toFormat[yearIndexTo] === 'YYYY') {
+    if (year < 30) {
+      year = `20${year}`;
+    } else {
+      year = `19${year}`;
+    }
+  }
+
+  if (fromFormat[yearIndexFrom] === 'YYYY' && toFormat[yearIndexTo] === 'YY') {
+    year = year.slice(-2);
+  }
+
+  let newDate = '';
+
+  for (const part of toFormat.slice(0, -1)) {
+    switch (part) {
+      case 'YYYY':
+        newDate += year;
+        break;
+      case 'YY':
+        newDate += year;
+        break;
+      case 'MM':
+        newDate += month;
+        break;
+      case 'DD':
+        newDate += day;
+        break;
+    }
+    newDate += newSeparator;
+  }
+
+  return newDate.slice(0, -1);
 }
 
 module.exports = formatDate;
