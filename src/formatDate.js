a49b06422f206585dda888c578673db2d2c8bc79
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-06-20 21:33:28 +0300

    Fornat Date KH

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,30 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const delimiterFrom = fromFormat[3];
+  const dateParts = date.split(delimiterFrom);
+
+  const dateObj = {};
+
+  fromFormat.slice(0, 3).forEach((part, index) => {
+    dateObj[part] = dateParts[index];
+  });
+
+  if (dateObj['YYYY']) {
+    dateObj['YY'] = dateObj['YYYY'].slice(2);
+  } else if (dateObj['YY']) {
+    const year = parseInt(dateObj['YY']);
+
+    dateObj['YYYY'] = year < 30 ? `20${dateObj['YY']}` : `19${dateObj['YY']}`;
+  }
+
+  const delimiterTo = toFormat[3];
+  const newDate = toFormat
+    .slice(0, 3)
+    .map((part) => dateObj[part])
+    .join(delimiterTo);
+
+  return newDate;
 }
 
 module.exports = formatDate;
