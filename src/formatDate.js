42d6dc58df9ab2eb5a4b3327768ce169997ad235
Arthur <avers.target@gmail.com>
2024-06-24 19:09:42 +0200

    version1

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,34 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separator = fromFormat[fromFormat.length - 1];
+  const dateParts = date.split(separator);
+  const dateComponents = {};
+  const newSeparator = toFormat[toFormat.length - 1];
+
+  fromFormat.forEach((el, index) => {
+    if (el !== separator) {
+      dateComponents[el] = dateParts[index];
+    }
+  });
+
+  if (dateComponents['YY']) {
+    dateComponents['YYYY'] =
+      dateComponents['YY'] < 30
+        ? '20' + dateComponents['YY']
+        : '19' + dateComponents['YY'];
+  }
+
+  if (dateComponents['YYYY'] && toFormat.includes('YY')) {
+    dateComponents['YY'] = dateComponents['YYYY'].slice(-2);
+  }
+
+  const formattedDate = toFormat
+    .filter((el) => el !== newSeparator)
+    .map((el) => dateComponents[el])
+    .join(newSeparator);
+
+  return formattedDate;
 }
 
 module.exports = formatDate;
