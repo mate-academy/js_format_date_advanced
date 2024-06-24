15b3482be9aeb21d35b2d51333dd853b6c501f0a
Oleksii Symonets <alex.simonets@gmail.com>
2024-06-24 23:16:11 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,32 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateSplit = date.split(fromFormat[3]);
+  const newDate = [];
+
+
+  for (let i = 0; toFormat.length - 1 > i; i++) {
+    const format = toFormat[i];
+    console.log(format)
+    console.log(newDate)
+
+    if (format === 'YYYY' && fromFormat.includes('YY')) {
+      let year = dateSplit[fromFormat.indexOf('YY')]
+      if (year < 30) {
+        year = '20' + year;
+      } else {
+        year = '19' + year;
+      }
+      newDate.push(year);
+    } else if (format === 'YY') {
+      newDate.push(dateSplit[fromFormat.indexOf('YYYY')].slice(-2));
+    } else {
+      newDate.push(dateSplit[fromFormat.indexOf(format)]);
+    }
+  }
+  console.log(newDate)
+
+  return newDate.join(toFormat[3])
 }
 
 module.exports = formatDate;
