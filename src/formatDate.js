151e1c5340d2e3d84e33788cf093ed9ad49ec7d9
Hamid Faiza <hamid.faiza811@gmail.com>
2024-06-27 18:45:24 +0300

    fixed code after mentors review

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,32 +8,41 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const dateArr = date.split(fromFormat[fromFormat.length - 1]);
-  const result = [];
+  const oldSeparator = fromFormat.pop();
+  const newSeparator = toFormat.pop();
 
-  result[toFormat.indexOf('DD')] = dateArr[fromFormat.indexOf('DD')];
-  result[toFormat.indexOf('MM')] = dateArr[fromFormat.indexOf('MM')];
+  const oldDate = date.split(oldSeparator);
+  const formattedDate = [];
+
+  if (toFormat.includes('DD')) {
+    formattedDate[toFormat.indexOf('DD')] = oldDate[fromFormat.indexOf('DD')];
+  }
+
+  if (toFormat.includes('MM')) {
+    formattedDate[toFormat.indexOf('MM')] = oldDate[fromFormat.indexOf('MM')];
+  }
 
   if (toFormat.indexOf('YY') !== -1) {
     if (fromFormat.indexOf('YYYY') !== -1) {
-      result[toFormat.indexOf('YY')] =
-        dateArr[fromFormat.indexOf('YYYY')].slice(2);
+      formattedDate[toFormat.indexOf('YY')] =
+        oldDate[fromFormat.indexOf('YYYY')].slice(2);
     } else {
-      result[toFormat.indexOf('YY')] = dateArr[fromFormat.indexOf('YY')];
+      formattedDate[toFormat.indexOf('YY')] = oldDate[fromFormat.indexOf('YY')];
     }
   } else {
-    if (dateArr[fromFormat.indexOf('YY')] >= 30) {
-      result[toFormat.indexOf('YYYY')] =
-        '19' + dateArr[fromFormat.indexOf('YY')];
-    } else if (dateArr[fromFormat.indexOf('YY')] < 30) {
-      result[toFormat.indexOf('YYYY')] =
-        '20' + dateArr[fromFormat.indexOf('YY')];
+    if (oldDate[fromFormat.indexOf('YY')] >= 30) {
+      formattedDate[toFormat.indexOf('YYYY')] =
+        '19' + oldDate[fromFormat.indexOf('YY')];
+    } else if (oldDate[fromFormat.indexOf('YY')] < 30) {
+      formattedDate[toFormat.indexOf('YYYY')] =
+        '20' + oldDate[fromFormat.indexOf('YY')];
     } else {
-      result[toFormat.indexOf('YYYY')] = dateArr[fromFormat.indexOf('YYYY')];
+      formattedDate[toFormat.indexOf('YYYY')] =
+        oldDate[fromFormat.indexOf('YYYY')];
     }
   }
 
-  return result.join(toFormat[toFormat.length - 1]);
+  return formattedDate.join(newSeparator);
 }
 
 module.exports = formatDate;
