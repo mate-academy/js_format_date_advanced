603c581666930a5a2172d352ed257c80cc21248c
Artem Androsenko <tyoma.and@gmail.com>
2024-07-01 13:28:10 +0300

    Add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,64 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const oldDateSeparator = fromFormat[3];
+  const splitFromFormat = date.split(oldDateSeparator);
+  const newFormatDate = [];
+  const year = [];
+  let month = '';
+  let day = '';
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    switch (fromFormat[i][0]) {
+      case 'Y':
+        year.push(splitFromFormat[i]);
+        break;
+
+      case 'M':
+        month = splitFromFormat[i];
+        break;
+
+      case 'D':
+        day = splitFromFormat[i];
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    switch (toFormat[i][0]) {
+      case 'Y':
+        const yearNewFormatLength = toFormat[i].length;
+
+        if (year[0].length === yearNewFormatLength) {
+          newFormatDate.push(...year);
+        } else if (year[0].length > yearNewFormatLength) {
+          newFormatDate.push(year[0].slice(2));
+        } else if (year[0].length < yearNewFormatLength && year[0] < 30) {
+          year.unshift(20);
+          newFormatDate.push(year.join(''));
+        } else if (+year[0] === 30 || year[0] > 30) {
+          year.unshift(19);
+          newFormatDate.push(year.join(''));
+        }
+        break;
+
+      case 'M':
+        newFormatDate.push(month);
+        break;
+
+      case 'D':
+        newFormatDate.push(day);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return newFormatDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
