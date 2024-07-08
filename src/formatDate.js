2f5a4e47e7802bd3e2e2cb1b20185ba971a9ce57
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-07-08 14:47:46 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,64 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const dateSeparated = date.split(fromFormat[3]);
+  let day = '';
+  let month = '';
+  let year = '';
+
+  for (let i = 0; i < 3; i++) {
+    switch (fromFormat[i]) {
+      case 'YYYY':
+        year = dateSeparated[i];
+        break;
+      case 'YY':
+        year = dateSeparated[i];
+        break;
+      case 'MM':
+        month = dateSeparated[i];
+        break;
+      case 'DD':
+        day = dateSeparated[i];
+        break;
+    }
+  }
+
+  let finalDate = '';
+
+  for (let i = 0; i < 3; i++) {
+    switch (toFormat[i]) {
+      case 'MM':
+        finalDate += month;
+        break;
+      case 'DD':
+        finalDate += day;
+        break;
+      case 'YY':
+        if (year.length === 2) {
+          finalDate += year;
+        } else {
+          finalDate += year.slice(-2);
+        }
+        break;
+      case 'YYYY':
+        if (year.length === 4) {
+          finalDate += year;
+        } else {
+          if (parseInt(year) < 30) {
+            finalDate += '20' + year;
+          } else {
+            finalDate += '19' + year;
+          }
+        }
+        break;
+    }
+
+    if (i < 2) {
+      finalDate += toFormat[3];
+    }
+  }
+
+  return finalDate;
 }
 
 module.exports = formatDate;
