8a31471c57275f5d0d9189d44fad8754ff080ca7
SerhiiSemennikov <semennik@hotmail.com>
2024-07-10 00:22:46 +0200

    new

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,30 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromDevider = fromFormat[3];
+  const toDevider = toFormat[3];
+
+  const dateValues = date.split(fromDevider);
+
+  const objectDate = {};
+
+  fromFormat.forEach((key, value) => {
+    objectDate[key] = dateValues[value];
+  });
+
+  if (toFormat.includes('YY')) {
+    objectDate['YY'] = objectDate['YYYY'].slice(2);
+  } else if (objectDate['YY'] && toFormat.includes('YYYY')) {
+    objectDate['YYYY'] =
+      objectDate['YY'] < 30 ? '20' + objectDate['YY'] : '19' + objectDate['YY'];
+  }
+
+  const toDate = toFormat
+    .slice(0, 3)
+    .map((key) => objectDate[key])
+    .join(toDevider);
+
+  return toDate;
 }
 
 module.exports = formatDate;
