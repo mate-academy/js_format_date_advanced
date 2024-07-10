5925822c28d430a871424fba647b5e99f582c62d
YaroslavKolesnyk <kolesnik.y2004@gmail.com>
2024-07-10 17:16:43 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,41 +8,71 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const separator = fromFormat[fromFormat.length - 1];
-  const separator2 = toFormat[toFormat.length - 1];
+  const fromSeparator = fromFormat[3];
+  const dateParts = date.split(fromSeparator);
 
-  const curDate = date.split(separator);
-  const dateObj = {};
+  let yearPart = '';
+  let monthPart = '';
+  let dayPart = '';
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    dateObj[fromFormat[i]] = curDate[i];
+    switch (fromFormat[i]) {
+      case 'YYYY':
+      case 'YY':
+        yearPart = dateParts[i];
+        break;
+
+      case 'DD':
+        dayPart = dateParts[i];
+        break;
+
+      case 'MM':
+        monthPart = dateParts[i];
+        break;
+    }
   }
 
-  const result = [];
+  const newFormat = [];
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    if (toFormat[i] === 'YYYY' && fromFormat.indexOf('YY') !== -1) {
-      let curYear = dateObj['YY'];
+  for (let i = 0; i < toFormat.length; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY':
+        newFormat[i] =
+          yearPart.length === 2 ? convertFormatYYtoYYYY(yearPart) : yearPart;
+        break;
 
-      if (curYear < 30) {
-        curYear = '20' + curYear;
-      } else {
-        curYear = '19' + curYear;
-      }
+      case 'YY':
+        newFormat[i] =
+          yearPart.length === 4 ? convertFormatYYYYtoYY(yearPart) : yearPart;
+        break;
 
-      result.push(curYear);
-      continue;
+      case 'DD':
+        newFormat[i] = dayPart;
+        break;
+
+      case 'MM':
+        newFormat[i] = monthPart;
+        break;
     }
-
-    if (toFormat[i] === 'YY' && fromFormat.indexOf('YYYY') !== -1) {
-      result.push(dateObj['YYYY'].slice(-2));
-      continue;
-    }
-
-    result.push(dateObj[toFormat[i]]);
   }
 
-  return result.join(separator2);
+  function convertFormatYYYYtoYY(year) {
+    return year.slice(-2);
+  }
+
+  function convertFormatYYtoYYYY(year) {
+    const LIMIT_21ST_CENTURY = 30;
+    const INITIAL_DIGITS_YEARS_21ST_CENTURY = '20';
+    const INITIAL_DIGITS_YEARS_20TH_CENTURY = '19';
+
+    if (+year < LIMIT_21ST_CENTURY) {
+      return INITIAL_DIGITS_YEARS_21ST_CENTURY + year;
+    }
+
+    return INITIAL_DIGITS_YEARS_20TH_CENTURY + year;
+  }
+
+  return newFormat.join(toFormat[3]);
 }
 
 module.exports = formatDate;
