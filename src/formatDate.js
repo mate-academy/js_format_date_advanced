408d6e95c3adf1f8de9897cbce87e48c1a60a698
katerynaLias <58365756+KateLis84@users.noreply.github.com>
2024-07-16 13:01:56 +0300

    complete the task

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,30 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat.pop();
+  const toSeparator = toFormat.pop();
+
+  const dateParts = date.split(fromSeparator);
+
+  const dateMap = {};
+
+  fromFormat.forEach((part, index) => {
+    dateMap[part] = dateParts[index];
+  });
+
+  if (dateMap['YYYY']) {
+    dateMap['YY'] = dateMap['YYYY'].slice(-2);
+  }
+
+  if (dateMap['YY']) {
+    const yy = parseInt(dateMap['YY'], 10);
+
+    dateMap['YYYY'] = (yy < 30 ? '20' : '19') + dateMap['YY'];
+  }
+
+  const newDateParts = toFormat.map((part) => dateMap[part]);
+
+  return newDateParts.join(toSeparator);
 }
 
 module.exports = formatDate;
