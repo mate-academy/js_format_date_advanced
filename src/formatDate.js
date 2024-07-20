6dae9ded91d752b12ecbb7c8ac39b7735f88a801
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-20 08:33:10 +0300

    js_format_date_advanced

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,38 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const fromParts = fromFormat.slice(0, -1);
+  const toParts = toFormat.slice(0, -1);
+
+  const dateParts = date.split(fromSeparator);
+
+  const dateObj = {};
+
+  fromParts.forEach((part, index) => {
+    dateObj[part] = dateParts[index];
+  });
+
+  const formattedDate = toParts
+    .map((part) => {
+      if (part === 'YYYY' && dateObj['YY']) {
+        const year = parseInt(dateObj['YY'], 10);
+
+        if (year < 30) {
+          return '20' + dateObj['YY'];
+        } else {
+          return '19' + dateObj['YY'];
+        }
+      } else if (part === 'YY' && dateObj['YYYY']) {
+        return dateObj['YYYY'].slice(-2);
+      } else {
+        return dateObj[part];
+      }
+    })
+    .join(toSeparator);
+
+  return formattedDate;
 }
 
 module.exports = formatDate;
