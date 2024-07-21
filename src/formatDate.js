a487210758bd6b1aa13542a47c9f576f6e8cc859
Tanya Baletska <t.baletska@gmail.com>
2024-07-21 22:35:42 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,26 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat.pop();
+  const toSeparator = toFormat.pop();
+  const dateParts = date.split(fromSeparator);
+  const dateMap = fromFormat.reduce((map, part, index) => {
+    map[part] = dateParts[index];
+
+    return map;
+  }, {});
+
+  if (dateMap['YYYY'] && toFormat.includes('YY')) {
+    dateMap['YY'] = dateMap['YYYY'].slice(-2);
+  } else if (dateMap['YY'] && toFormat.includes('YYYY')) {
+    const year = parseInt(dateMap['YY'], 10);
+
+    dateMap['YYYY'] = year < 30 ? `20${dateMap['YY']}` : `19${dateMap['YY']}`;
+  }
+
+  const newDateParts = toFormat.map((part) => dateMap[part]);
+
+  return newDateParts.join(toSeparator);
 }
 
 module.exports = formatDate;
