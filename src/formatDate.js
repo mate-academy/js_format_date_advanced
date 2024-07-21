c627bf5025389c2b32094e3a03dc6f0eef735657
Anton Donchenko <donch.a080@gmail.com>
2024-07-21 09:55:08 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,42 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separator = fromFormat.pop();
+  const newSeparator = toFormat.pop();
+  const dateParts = date.split(separator);
+  const newDate = ['', '', ''];
+
+  const formatMap = {
+    DD: 'день',
+    MM: 'місяць',
+    YYYY: 'рік',
+    YY: 'рік',
+  };
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    const format = fromFormat[i];
+    const partType = formatMap[format];
+    const partValue = dateParts[i];
+
+    let newIndex = -1;
+
+    for (let j = 0; j < toFormat.length; j++) {
+      if (formatMap[toFormat[j]] === partType) {
+        newIndex = j;
+        break;
+      }
+    }
+
+    if (format === 'YYYY' && toFormat[newIndex] === 'YY') {
+      newDate[newIndex] = partValue.slice(-2);
+    } else if (format === 'YY' && toFormat[newIndex] === 'YYYY') {
+      newDate[newIndex] = (+partValue < 30 ? '20' : '19') + partValue;
+    } else {
+      newDate[newIndex] = partValue;
+    }
+  }
+
+  return newDate.join(newSeparator);
 }
 
 module.exports = formatDate;
