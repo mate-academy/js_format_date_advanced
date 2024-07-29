c6e38aa0919aaf934d788d27343975e8b88df998
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-07-29 20:07:21 +0200

    format_date_task_2

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,49 +8,34 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const formattedToDateParts = {}; // day, month, year
+  const dateParts = {};
   const values = date.split(fromFormat[3]);
-  let formattedDate = '';
 
   for (let i = 0; i < 3; i++) {
-    if (fromFormat[i] === 'YYYY' && toFormat[i] === 'YY') {
-      formattedToDateParts[2] = values[i].slice(-2);
-    } else if (fromFormat[i] === 'YY' && toFormat[i] === 'YYYY') {
-      const year = Number(values[i]);
+    dateParts[fromFormat[i]] = values[i];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < 3; i++) {
+    let value = dateParts[toFormat[i]];
+
+    if (toFormat[i] === 'YY' && (dateParts['YYYY'] || dateParts['YY'])) {
+      value = (dateParts['YYYY'] || dateParts['YY']).slice(-2);
+    } else if (toFormat[i] === 'YYYY' && dateParts['YY']) {
+      const year = Number(dateParts['YY']);
 
       if (year < 30) {
-        formattedToDateParts[2] = '20' + values[i];
+        value = '20' + dateParts['YY'];
       } else {
-        formattedToDateParts[2] = '19' + values[i];
+        value = '19' + dateParts['YY'];
       }
-    } else if (fromFormat[i] === 'YY' || fromFormat[i] === 'YYYY') {
-      formattedToDateParts[2] = values[i];
     }
 
-    if (fromFormat[i] === 'DD') {
-      formattedToDateParts[0] = values[i];
-    }
-
-    if (fromFormat[i] === 'MM') {
-      formattedToDateParts[1] = values[i];
-    }
+    result.push(value);
   }
 
-  for (let i = 0; i < 3; i++) {
-    if (toFormat[i] === 'DD') {
-      formattedDate += formattedToDateParts[0];
-    } else if (toFormat[i] === 'MM') {
-      formattedDate += formattedToDateParts[1];
-    } else if (toFormat[i] === 'YY' || toFormat[i] === 'YYYY') {
-      formattedDate += formattedToDateParts[2];
-    }
-
-    if (i < 2) {
-      formattedDate += toFormat[3];
-    }
-  }
-
-  return formattedDate;
+  return result.join(toFormat[3]);
 }
 
 module.exports = formatDate;
