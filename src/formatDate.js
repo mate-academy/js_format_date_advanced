acacd3ac472ba9bd58912b652968a04f90975020
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-07-29 15:36:26 +0200

    format_date_task

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,49 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const formattedToDateParts = {}; // day, month, year
+  const values = date.split(fromFormat[3]);
+  let formattedDate = '';
+
+  for (let i = 0; i < 3; i++) {
+    if (fromFormat[i] === 'YYYY' && toFormat[i] === 'YY') {
+      formattedToDateParts[2] = values[i].slice(-2);
+    } else if (fromFormat[i] === 'YY' && toFormat[i] === 'YYYY') {
+      const year = Number(values[i]);
+
+      if (year < 30) {
+        formattedToDateParts[2] = '20' + values[i];
+      } else {
+        formattedToDateParts[2] = '19' + values[i];
+      }
+    } else if (fromFormat[i] === 'YY' || fromFormat[i] === 'YYYY') {
+      formattedToDateParts[2] = values[i];
+    }
+
+    if (fromFormat[i] === 'DD') {
+      formattedToDateParts[0] = values[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      formattedToDateParts[1] = values[i];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i] === 'DD') {
+      formattedDate += formattedToDateParts[0];
+    } else if (toFormat[i] === 'MM') {
+      formattedDate += formattedToDateParts[1];
+    } else if (toFormat[i] === 'YY' || toFormat[i] === 'YYYY') {
+      formattedDate += formattedToDateParts[2];
+    }
+
+    if (i < 2) {
+      formattedDate += toFormat[3];
+    }
+  }
+
+  return formattedDate;
 }
 
 module.exports = formatDate;
