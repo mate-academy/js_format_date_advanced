b46d6122bc908c579a42444738d30e475b2a9180
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-08-04 19:06:14 +0300

    different solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -13,33 +13,35 @@ function formatDate(date, fromFormat, toFormat) {
   const initialArray = fromFormat.slice(0, 3);
   const resultingArray = toFormat.slice(0, 3);
   const dateArray = date.split(initialSeparator);
-  const result = [];
+  const objectDate = {};
 
-  for (const initialElement of initialArray) {
-    for (const resultingElement of resultingArray) {
-      const initialIndex = initialArray.indexOf(initialElement);
-      const resultingIndex = resultingArray.indexOf(resultingElement);
+  for (let i = 0; i < dateArray.length; i++) {
+    let initialVariable = initialArray[i];
+    let dateValue = dateArray[i];
 
-      if (initialElement === resultingElement) {
-        result[resultingIndex] = dateArray[initialIndex];
-        continue;
-      }
+    if (!resultingArray.includes(initialVariable)) {
+      if (initialVariable === 'YY') {
+        initialVariable = 'YYYY';
 
-      if (initialElement.includes('YY') && resultingElement.includes('YY')) {
-        if (initialElement.length < resultingElement.length) {
-          if (+dateArray[initialIndex] < 30) {
-            result[resultingIndex] = `20${dateArray[initialIndex]}`;
-          } else {
-            result[resultingIndex] = `19${dateArray[initialIndex]}`;
-          }
+        if (+dateValue < 30) {
+          dateValue = `20${dateValue}`;
         } else {
-          result[resultingIndex] = dateArray[initialIndex].slice(2);
+          dateValue = `19${dateValue}`;
         }
+      } else {
+        initialVariable = 'YY';
+        dateValue = dateValue.slice(2);
       }
     }
+
+    objectDate[initialVariable] = dateValue;
   }
 
-  return result.join(resultingSeparator);
+  for (let i = 0; i < resultingArray.length; i++) {
+    resultingArray[i] = objectDate[resultingArray[i]];
+  }
+
+  return resultingArray.join(resultingSeparator);
 }
 
 module.exports = formatDate;
