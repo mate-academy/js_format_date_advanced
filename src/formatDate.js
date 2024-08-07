ebe6a6c8f46ebc8eda25a23f17d8bbacb5ebf5a1
Ivan Paiareli <ipayareli@gmail.com>
2024-08-07 08:42:01 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,55 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const sepOld = fromFormat[3];
+  const sepNew = toFormat[3];
+  const dateArr = date.split(sepOld);
+  let year = 0;
+  let month = 0;
+  let day = 0;
+  const newDate = [];
+
+  for (let i = 0; i <= 2; i++) {
+    if (fromFormat[i] === 'YYYY' || fromFormat[i] === 'YY') {
+      year = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'DD') {
+      day = dateArr[i];
+    }
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    if (toFormat[i] === 'YYYY' && year.length === 4) {
+      newDate[i] = year;
+    }
+
+    if (toFormat[i] === 'YY' && year.length === 4) {
+      newDate[i] = year.toString().slice(2);
+    }
+
+    if (toFormat[i] === 'YYYY' && year.length === 2) {
+      if (year < 30) {
+        newDate[i] = ['20', year].join('');
+      } else {
+        newDate[i] = ['19', year].join('');
+      }
+    }
+
+    if (toFormat[i] === 'MM') {
+      newDate[i] = month;
+    }
+
+    if (toFormat[i] === 'DD') {
+      newDate[i] = day;
+    }
+  }
+
+  return newDate.join(sepNew);
 }
 
 module.exports = formatDate;
