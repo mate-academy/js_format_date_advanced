72d38dbb02420febdf1db4365d6d16da87a29d85
Your Name <you@example.com>
2024-08-10 18:49:37 +0700

    first commit

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,43 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldYearIndex = fromFormat.findIndex((elem) => elem.includes('YY'));
+  const oldMonthIndex = fromFormat.indexOf('MM');
+  const oldDayIndex = fromFormat.indexOf('DD');
+
+  const newYearIndex = toFormat.findIndex((elem) => elem.includes('YY'));
+  const newMonthIndex = toFormat.indexOf('MM');
+  const newDayIndex = toFormat.indexOf('DD');
+
+  const splitChar = fromFormat[3];
+  const joinChar = toFormat[3];
+
+  const dateArray = date.split(splitChar);
+
+  const newArray = [...dateArray];
+
+  if (
+    fromFormat[oldYearIndex].length === 2 &&
+    toFormat[newYearIndex].length === 4
+  ) {
+    if (dateArray[oldYearIndex].slice(-2) < 30) {
+      newArray[newYearIndex] = '20' + dateArray[oldYearIndex].slice(-2);
+    } else {
+      newArray[newYearIndex] = '19' + dateArray[oldYearIndex];
+    }
+  } else if (
+    fromFormat[oldYearIndex].length === 4 &&
+    toFormat[newYearIndex].length === 2
+  ) {
+    newArray[newYearIndex] = dateArray[oldYearIndex].slice(-2);
+  } else {
+    newArray[newYearIndex] = dateArray[oldYearIndex];
+  }
+
+  newArray[newMonthIndex] = dateArray[oldMonthIndex];
+  newArray[newDayIndex] = dateArray[oldDayIndex];
+
+  return newArray.join(joinChar);
 }
 
 module.exports = formatDate;
