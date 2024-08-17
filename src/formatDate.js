3f611f5571a969e6e24af977268f9fb105179fbb
Tatiana Makohon (Palamarchuk) <tatiana@MacBook-Pro-Tatiana.local>
2024-08-17 14:55:00 +0300

    check

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,38 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
-}
+  const data = date.split(fromFormat[3]);
 
+  const object = {};
+
+  for (let i = 0; i < 3; i++) {
+    object[fromFormat[i]] = data[i];
+  }
+
+  if (object.YYYY) {
+    object.YY = object.YYYY.slice(2);
+  }
+
+  if (object.YY < 30) {
+    object.YYYY = '20' + `${object.YY}`;
+  } else {
+    object.YYYY = '19' + `${object.YY}`;
+  }
+
+  const objectKeys = Object.keys(object);
+  const objectValues = Object.values(object);
+
+  let result = [];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    for (let j = 0; j < objectKeys.length; j++) {
+      if (toFormat[i] === objectKeys[j]) {
+        result.push(objectValues[j]);
+      }
+    }
+  }
+  result = result.join(toFormat[3]);
+
+  return result;
+}
 module.exports = formatDate;
