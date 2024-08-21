985933371e30d67258036351a370957e3d87e5fd
Karina Koliada <koladakarina1112@gmail.com>
2024-08-21 14:18:14 +0200

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,32 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  // делим дату на части
+  const dateParts = date.split(fromFormat[3]);
+  const newDateObject = {};
+
+  for (let i = 0; i < 3; i++) {
+    newDateObject[fromFormat[i]] = dateParts[i];
+  }
+  // проверка если в fromFormat есть YYYY, а в toFormat — YY,
+  // то newDateObject['YY'] будет присвоено последние две цифры
+
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    newDateObject['YY'] = newDateObject['YYYY'].slice(-2);
+  } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    const year = parseInt(newDateObject['YY']);
+
+    newDateObject['YYYY'] =
+      year < 30 ? '20' + newDateObject['YY'] : '19' + newDateObject['YY'];
+  }
+
+  // формирование новой даты
+  const newDate = toFormat
+    .slice(0, 3)
+    .map((part) => newDateObject[part])
+    .join(toFormat[3]);
+
+  return newDate;
 }
 
 module.exports = formatDate;
