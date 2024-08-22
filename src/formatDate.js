49cb69dc3378f90a16b89542bacab97e52b782f9
Karina Koliada <koladakarina1112@gmail.com>
2024-08-22 09:27:07 +0200

    fixed

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,20 +8,16 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // делим дату на части
   const delimiter = date.split(fromFormat[3]);
   const newDateObject = {};
 
   for (let i = 0; i < 3; i++) {
     newDateObject[fromFormat[i]] = delimiter[i];
   }
-  // Новые переменные для преобразования годов
 
   const CURRENT_CENTURY_THRESHOLD = 30;
   const CENTURY_1900 = '19';
   const CENTURY_2000 = '20';
-  // проверка если в fromFormat есть YYYY, а в toFormat — YY,
-  // то newDateObject['YY'] будет присвоено последние две цифры
 
   if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
     newDateObject['YY'] = newDateObject['YYYY'].slice(-2);
@@ -33,10 +29,8 @@ function formatDate(date, fromFormat, toFormat) {
         ? CENTURY_2000 + newDateObject['YY']
         : CENTURY_1900 + newDateObject['YY'];
   }
-  // Проверка, что toFormat имеет достаточное количество элементов
 
   if (toFormat.length >= 4) {
-    // формирование новой даты
     const newDate = toFormat
       .slice(0, 3)
       .map((part) => newDateObject[part])
