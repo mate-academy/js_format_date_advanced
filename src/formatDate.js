9a67ee7bb50cd17b1f538027f91766cfd4ab7c22
Oleg Usenko <Oleg911Usenko@gmail.com>
2024-08-26 15:30:00 +0300

    refactoring code

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,84 +8,61 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const fromFormatObject = arrayInObject(fromFormat);
-  const toFormatObject = arrayInObject(toFormat);
   const arrayOldDate = date.split(fromFormat[3]);
-  const dateObject = arrayDateFromObject(arrayOldDate, fromFormat);
 
-  if (fromFormatObject.yy !== toFormatObject.yy) {
-    if (fromFormatObject.yy.length === 4) {
-      dateObject.yy = dateObject.yy.slice(2);
-    } else {
-      if (+dateObject.yy < 30) {
-        dateObject.yy = '20' + dateObject.yy;
-      } else {
-        dateObject.yy = '19' + dateObject.yy;
-      }
+  let year = '';
+  let day = '';
+  let month = '';
+
+  for (let i = 0; i < 3; i++) {
+    if (fromFormat[i] === 'YYYY' || fromFormat[i] === 'YY') {
+      year = arrayOldDate[i];
+      continue;
+    }
+
+    if (fromFormat[i] === 'DD') {
+      day = arrayOldDate[i];
+      continue;
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month = arrayOldDate[i];
+      continue;
     }
   }
 
-  return resultDate(dateObject, toFormat);
-}
-
-function resultDate(dateObject, toFormat) {
   const result = [];
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 3; i++) {
     if (toFormat[i] === 'YYYY' || toFormat[i] === 'YY') {
-      result.push(dateObject.yy);
+      if (toFormat[i].length === year.length) {
+        result[i] = year;
+      } else {
+        if (year.length === 2) {
+          if (+year < 30) {
+            result[i] = '20' + year;
+          } else {
+            result[i] = '19' + year;
+          }
+        } else {
+          result[i] = year.slice(2);
+        }
+      }
+      continue;
     }
 
     if (toFormat[i] === 'MM') {
-      result.push(dateObject.mm);
+      result[i] = month;
+      continue;
     }
 
     if (toFormat[i] === 'DD') {
-      result.push(dateObject.dd);
+      result[i] = day;
+      continue;
     }
   }
 
   return result.join(toFormat[3]);
-}
-
-function arrayDateFromObject(arrayOldDate, fromFormat) {
-  const dateObject = {};
-
-  for (let i = 0; i < 4; i++) {
-    if (fromFormat[i] === 'YYYY' || fromFormat[i] === 'YY') {
-      dateObject.yy = arrayOldDate[i];
-    }
-
-    if (fromFormat[i] === 'MM') {
-      dateObject.mm = arrayOldDate[i];
-    }
-
-    if (fromFormat[i] === 'DD') {
-      dateObject.dd = arrayOldDate[i];
-    }
-  }
-
-  return dateObject;
-}
-
-function arrayInObject(array) {
-  const result = {};
-
-  for (let i = 0; i < 4; i++) {
-    if (array[i] === 'YYYY' || array[i] === 'YY') {
-      result.yy = array[i];
-    }
-
-    if (array[i] === 'DD') {
-      result.dd = array[i];
-    }
-
-    if (array[i] === 'MM') {
-      result.mm = array[i];
-    }
-  }
-
-  return result;
 }
 
 module.exports = formatDate;
