66a2819f6af0cb48a22f065622c3124010b2eef4
Svidruk <mr.svid999@gmail.com>
2024-08-27 18:04:50 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,43 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const splittedDate = date.split(fromFormat[3]);
+
+  const yearFormat = fromFormat.includes('YY') ? 'YY' : 'YYYY';
+
+  const year = splittedDate[fromFormat.indexOf(yearFormat)];
+  const month = splittedDate[fromFormat.indexOf('MM')];
+  const day = splittedDate[fromFormat.indexOf('DD')];
+
+  const newYearFormat = toFormat.includes('YY') ? 'YY' : 'YYYY';
+
+  const newIndexOfYear = toFormat.indexOf(newYearFormat);
+  const newIndexOfMonth = toFormat.indexOf('MM');
+  const newIndexOfDay = toFormat.indexOf('DD');
+
+  const newDate = [];
+
+  let formatedYear = year;
+
+  if (yearFormat.length - newYearFormat.length === 2) {
+    formatedYear = year.slice(2, 4);
+  }
+
+  if (yearFormat.length - newYearFormat.length === -2) {
+    formatedYear = +year >= 30 ? '19' + year : '20' + year;
+  }
+
+  newDate[newIndexOfYear] = formatedYear;
+  newDate[newIndexOfMonth] = month;
+  newDate[newIndexOfDay] = day;
+
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
+
+/* formatDate(
+  '97/02/18',
+  ['YY', 'MM', 'DD', '/'],
+  ['DD', 'MM', 'YYYY', '.'],
+); // '18.02.1997' */
