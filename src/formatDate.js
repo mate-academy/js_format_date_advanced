ebb37872ccde189943debcbcd65a48a653fb4358
Svidruk <mr.svid999@gmail.com>
2024-08-29 19:47:15 +0300

    solution with suggested AI chages

--- src/formatDate.js
+++ src/formatDate.js
@@ -10,13 +10,13 @@
 function formatDate(date, fromFormat, toFormat) {
   const splittedDate = date.split(fromFormat[3]);
 
-  const yearFormat = fromFormat.includes('YY') ? 'YY' : 'YYYY';
+  const yearFormat = fromFormat.includes('YYYY') ? 'YYYY' : 'YY';
 
   const year = splittedDate[fromFormat.indexOf(yearFormat)];
   const month = splittedDate[fromFormat.indexOf('MM')];
   const day = splittedDate[fromFormat.indexOf('DD')];
 
-  const newYearFormat = toFormat.includes('YY') ? 'YY' : 'YYYY';
+  const newYearFormat = toFormat.includes('YYYY') ? 'YYYY' : 'YY';
 
   const newIndexOfYear = toFormat.indexOf(newYearFormat);
   const newIndexOfMonth = toFormat.indexOf('MM');
@@ -26,11 +26,11 @@ function formatDate(date, fromFormat, toFormat) {
 
   let formatedYear = year;
 
-  if (yearFormat.length - newYearFormat.length === 2) {
+  if (yearFormat === 'YYYY' && newYearFormat === 'YY') {
     formatedYear = year.slice(2, 4);
   }
 
-  if (yearFormat.length - newYearFormat.length === -2) {
+  if (yearFormat === 'YY' && newYearFormat === 'YYYY') {
     formatedYear = +year >= 30 ? '19' + year : '20' + year;
   }
 
