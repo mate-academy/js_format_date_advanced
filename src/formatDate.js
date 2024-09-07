0eb4b26724fc7f2c548bc263f08d2272676dd01b
Vlad <vladyslav.vasylevych@gmail.com>
2024-09-07 14:49:24 +0300

    add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,54 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+
+  const oldSeparator = fromFormat[fromFormat.length - 1];
+  const newSeparator = toFormat[toFormat.length - 1];
+
+  const oldDateParts = date.split(oldSeparator);
+
+  let datePartsMatched = {};
+
+  for (let i = 0; i < oldDateParts.length; i++) {
+    datePartsMatched[fromFormat[i]] = oldDateParts[i];
+  }
+
+  datePartsMatched = convertYear(datePartsMatched);
+
+  const newDateParts = [];
+
+  for (let i = 0; i < oldDateParts.length; i++) {
+    const newDateFormat = toFormat[i];
+
+    const newDatePart = datePartsMatched[newDateFormat];
+
+    newDateParts.push(newDatePart);
+  }
+
+  return newDateParts.join(newSeparator);
+}
+/**
+ * @param {Object} datePartsMatched
+ *
+ * @returns {Object}
+ */
+
+function convertYear(datePartsMatched) {
+  const FOUR_DIGIT_YEAR_FORMAT = 'YYYY';
+  const TWO_DIGIT_YEAR_FORMAT = 'YY';
+
+  if (FOUR_DIGIT_YEAR_FORMAT in datePartsMatched) {
+    const year = datePartsMatched[FOUR_DIGIT_YEAR_FORMAT];
+
+    datePartsMatched[TWO_DIGIT_YEAR_FORMAT] = year.slice(2);
+  } else if (TWO_DIGIT_YEAR_FORMAT in datePartsMatched) {
+    const year = datePartsMatched[TWO_DIGIT_YEAR_FORMAT];
+    const newYear = +year < 30 ? `20${year}` : `19${year}`;
+
+    datePartsMatched[FOUR_DIGIT_YEAR_FORMAT] = newYear;
+  }
+
+  return datePartsMatched;
 }
 
 module.exports = formatDate;
