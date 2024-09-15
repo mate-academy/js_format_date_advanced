392a609efc4420af2792ba3baaf68d979ca05e65
Vladyslav Kriuk <kriukvlad@gmail.com>
2024-09-15 22:50:10 +0300

    New Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,29 +8,31 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const FROM_SEPARATOR_FORMAT = fromFormat[3];
-  const TO_SEPARATOR_FORMAT = toFormat[3];
-  let year, month, day;
-  const RESULT = [];
+  const fromSeparatorFormat = fromFormat[3];
+  const toSeparatorFormat = toFormat[3];
+  const dateObject = {};
 
-  const ARRAY_FROM_DATE = date.split(FROM_SEPARATOR_FORMAT);
+  const arrayFromDate = date.split(fromSeparatorFormat);
 
   fromFormat.forEach((part, i) => {
     switch (part) {
       case 'YYYY':
-        year = ARRAY_FROM_DATE[i];
+        dateObject.year = arrayFromDate[i];
         break;
 
       case 'YY':
-        year = ARRAY_FROM_DATE[i];
+        const year = parseInt(arrayFromDate[i]);
+
+        dateObject.year =
+          year < 30 ? '20' + arrayFromDate[i] : '19' + arrayFromDate[i];
         break;
 
       case 'MM':
-        month = ARRAY_FROM_DATE[i];
+        dateObject.month = arrayFromDate[i];
         break;
 
       case 'DD':
-        day = ARRAY_FROM_DATE[i];
+        dateObject.day = arrayFromDate[i];
         break;
 
       default:
@@ -38,31 +40,28 @@ function formatDate(date, fromFormat, toFormat) {
     }
   });
 
-  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
-    year = year.slice(-2);
-  }
+  const result = toFormat
+    .map((part) => {
+      if (part === 'YYYY') {
+        return dateObject.year;
+      }
 
-  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
-    year = parseInt(year) < 30 ? '20' + year : '19' + year;
-  }
+      if (part === 'YY') {
+        return dateObject.year.slice(-2);
+      }
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
-    if (toFormat[i] === 'YYYY' || toFormat[i] === 'YY') {
-      RESULT.push(year);
-    }
+      if (part === 'MM') {
+        return dateObject.month;
+      }
 
-    if (toFormat[i] === 'MM') {
-      RESULT.push(month);
-    }
+      if (part === 'DD') {
+        return dateObject.day;
+      }
+    })
+    .filter(Boolean)
+    .join(toSeparatorFormat);
 
-    if (toFormat[i] === 'DD') {
-      RESULT.push(day);
-    }
-  }
-
-  const FINAL_RESULT = RESULT.join(TO_SEPARATOR_FORMAT);
-
-  return FINAL_RESULT;
+  return result;
 }
 
 module.exports = formatDate;
