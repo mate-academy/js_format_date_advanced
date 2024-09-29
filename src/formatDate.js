36233caeb72257388450274f9e7150598d1ce7e4
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-09-29 14:26:36 +0300

    update

--- src/formatDate.js
+++ src/formatDate.js
@@ -13,16 +13,16 @@ function formatDate(date, fromFormat, toFormat) {
   const separatorFrom = fromFormat[fromFormat.length - 1]; // '-',
   const separatorToFormat = toFormat[toFormat.length - 1]; // '.'
 
-  const formatedYear = [];
+  const formattedDatePart = [];
 
-  const newDate = date.split(separatorFrom); // ['2020', '02', '18'],
+  const dateParts = date.split(separatorFrom); // ['2020', '02', '18'],
   const dayIndex = takeIndexOf(fromFormat, 'DD'); // [2]
   const monthIndex = takeIndexOf(fromFormat, 'MM'); // [1]
   const yearIndex = takeIndexOf(fromFormat, 'YYYY'); // [0]
 
-  let year = newDate[yearIndex]; // '2020' [0]
-  const month = newDate[monthIndex]; // '02' [1]
-  const day = newDate[dayIndex]; // '18' [2]
+  let year = dateParts[yearIndex]; // '2020' [0]
+  const month = dateParts[monthIndex]; // '02' [1]
+  const day = dateParts[dayIndex]; // '18' [2]
 
   if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
     if (+year < 30) {
@@ -36,11 +36,11 @@ function formatDate(date, fromFormat, toFormat) {
     year = year.slice(2);
   }
 
-  formatedYear[takeIndexOf(toFormat, 'YYYY')] = year;
-  formatedYear[takeIndexOf(toFormat, 'MM')] = month;
-  formatedYear[takeIndexOf(toFormat, 'DD')] = day;
+  formattedDatePart[takeIndexOf(toFormat, 'YYYY')] = year;
+  formattedDatePart[takeIndexOf(toFormat, 'MM')] = month;
+  formattedDatePart[takeIndexOf(toFormat, 'DD')] = day;
 
-  return formatedYear.join(separatorToFormat);
+  return formattedDatePart.join(separatorToFormat);
 }
 
 function takeIndexOf(format, param) {
