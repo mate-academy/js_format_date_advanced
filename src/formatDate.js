559042d8aea8fab734ac57df1f7c720a0ac47ea6
ab3MN <ab3mn3@gmail.com>
2024-09-30 21:55:57 +0300

    feat: implemented formatData

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,34 @@
  *
  * @returns {string}
  */
+
+const getObjectDate = (dateParts, fromFormat) =>
+  fromFormat.reduce((objectFormat, format, i) => {
+    if (format === 'YY' && dateParts[i].length === 2) {
+      objectFormat['YYYY'] =
+        dateParts[i] < '30' ? `20${dateParts[i]}` : `19${dateParts[i]}`;
+    } else {
+      objectFormat[format] = dateParts[i];
+    }
+
+    return objectFormat;
+  }, {});
+
+const convertYear = (objectDate, toFormat) => {
+  if (!objectDate['YY'] && objectDate['YYYY'] && toFormat.includes('YY')) {
+    objectDate['YY'] = objectDate['YYYY'].slice(-2);
+  }
+};
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separatorFrom = fromFormat.pop();
+  const separatorTo = toFormat.pop();
+  const dateParts = date.split(separatorFrom);
+  const objectDate = getObjectDate(dateParts, fromFormat);
+
+  convertYear(objectDate, toFormat);
+
+  return toFormat.map((format) => objectDate[format]).join(separatorTo);
 }
 
 module.exports = formatDate;
