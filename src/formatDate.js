0447d48634bebfb9aabe992088764581d5a42659
Halyna <minimal005@gmail.com>
2024-10-01 13:59:22 +0300

    I hope this solution is better

--- src/formatDate.js
+++ src/formatDate.js
@@ -13,19 +13,15 @@ function formatDate(date, fromFormat, toFormat) {
   const changedDate = [];
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
-    switch (fromFormat[i]) {
-      case 'YYYY':
-        receivedDate['YY'] = dateArr[i].slice(2);
-        receivedDate['YYYY'] = dateArr[i];
-        break;
-      case 'YY':
-        receivedDate['YYYY'] = (dateArr[i] < 30 ? 20 : 19) + dateArr[i];
-        receivedDate['YY'] = dateArr[i];
-        break;
-      default:
-        receivedDate[fromFormat[i]] = dateArr[i];
-        break;
+    if (fromFormat[i] === 'YYYY') {
+      receivedDate['YY'] = dateArr[i].slice(2);
     }
+
+    if (fromFormat[i] === 'YY') {
+      receivedDate['YYYY'] = (dateArr[i] < 30 ? 20 : 19) + dateArr[i];
+    }
+
+    receivedDate[fromFormat[i]] = dateArr[i];
   }
 
   for (const elem of toFormat.slice(0, -1)) {
