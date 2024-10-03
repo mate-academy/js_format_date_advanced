c1aedb3f891b2dcaf5c790ae87a21228b9188bc1
SofiiaZhmurko <sofiiak209@gmail.com>
2024-10-03 15:19:13 +1300

    new Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,33 +9,60 @@
  */
 
 function formatDate(date, fromFormat, toFormat) {
-  const formatedDate = [];
-  const splitedDate = date.split(fromFormat[3]);
+  const fromSeparatorFormat = fromFormat[3];
+  const toSeparatorFormat = toFormat[3];
+  const dateObject = {};
 
-  for (let i = 0; i < fromFormat.length - 1; i++) {
-    for (let j = 0; j < toFormat.length - 1; j++) {
-      if (fromFormat[i] === 'YYYY' && toFormat[j] === 'YY') {
-        formatedDate[j] = splitedDate[i].slice(2);
+  const arrayFromDate = date.split(fromSeparatorFormat);
+
+  fromFormat.forEach((part, i) => {
+    switch (part) {
+      case 'YYYY':
+        dateObject.year = arrayFromDate[i];
         break;
-      }
 
-      if (fromFormat[i] === 'YY' && toFormat[j] === 'YYYY') {
-        if (splitedDate[i] < 30) {
-          formatedDate[j] = '20' + splitedDate[i];
-        } else {
-          formatedDate[j] = '19' + splitedDate[i];
-        }
+      case 'YY':
+        const year = parseInt(arrayFromDate[i]);
 
+        dateObject.year =
+          year < 30 ? '20' + arrayFromDate[i] : '19' + arrayFromDate[i];
         break;
-      }
 
-      if (fromFormat[i] === toFormat[j]) {
-        formatedDate[j] = splitedDate[i];
-      }
+      case 'MM':
+        dateObject.month = arrayFromDate[i];
+        break;
+
+      case 'DD':
+        dateObject.day = arrayFromDate[i];
+        break;
+
+      default:
+        break;
     }
-  }
+  });
 
-  return formatedDate.join(toFormat[3]);
+  const result = toFormat
+    .map((part) => {
+      if (part === 'YYYY') {
+        return dateObject.year;
+      }
+
+      if (part === 'YY') {
+        return dateObject.year.slice(-2);
+      }
+
+      if (part === 'MM') {
+        return dateObject.month;
+      }
+
+      if (part === 'DD') {
+        return dateObject.day;
+      }
+    })
+    .filter(Boolean)
+    .join(toSeparatorFormat);
+
+  return result;
 }
 
 module.exports = formatDate;
