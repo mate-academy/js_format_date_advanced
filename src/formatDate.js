070f6c24627a3c9c3f07fb2efa4f771a931ef507
Misha <mizunskijm3@gmail.com>
2024-10-09 21:35:59 +0300

    fix

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,6 +7,14 @@
  *
  * @returns {string}
  */
+function convertToYYYY(year) {
+  return parseInt(year) < 30 ? '20' + year : '19' + year;
+}
+
+function convertToYY(year) {
+  return year.slice(-2);
+}
+
 function formatDate(date, fromFormat, toFormat) {
   const separatorFrom = fromFormat[fromFormat.length - 1];
   const dateParts = date.split(separatorFrom);
@@ -17,23 +25,11 @@ function formatDate(date, fromFormat, toFormat) {
     dateObj[part] = dateParts[index];
   });
 
-  const convertYear = (year, format) => {
-    if (format === 'YYYY') {
-      if (year.length === 2) {
-        return parseInt(year) < 30 ? '20' + year : '19' + year;
-      } else {
-        return year;
-      }
-    } else if (format === 'YY') {
-      return year.slice(-2);
-    }
-
-    return year;
-  };
-
   const newDateParts = toFormat.slice(0, -1).map((part) => {
-    if (part === 'YYYY' || part === 'YY') {
-      return convertYear(dateObj['YYYY'] || dateObj['YY'], part);
+    if (part === 'YYYY') {
+      return dateObj['YY'] ? convertToYYYY(dateObj['YY']) : dateObj['YYYY'];
+    } else if (part === 'YY') {
+      return dateObj['YYYY'] ? convertToYY(dateObj['YYYY']) : dateObj['YY'];
     }
 
     return dateObj[part];
