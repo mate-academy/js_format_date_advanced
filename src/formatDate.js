71b619dd0a902f39eddba15a784b4778a6d219b6
Misha <mizunskijm3@gmail.com>
2024-10-09 21:59:41 +0300

    corrected

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,13 @@
  * @returns {string}
  */
 function convertToYYYY(year) {
-  return parseInt(year) < 30 ? '20' + year : '19' + year;
+  const numYear = parseInt(year, 10);
+
+  return numYear < 10
+    ? '200' + numYear
+    : numYear < 30
+      ? '20' + numYear
+      : '19' + numYear;
 }
 
 function convertToYY(year) {
@@ -18,25 +24,26 @@ function convertToYY(year) {
 function formatDate(date, fromFormat, toFormat) {
   const separatorFrom = fromFormat[fromFormat.length - 1];
   const dateParts = date.split(separatorFrom);
-
-  const dateObj = {};
-
-  fromFormat.slice(0, -1).forEach((part, index) => {
-    dateObj[part] = dateParts[index];
-  });
-
-  const newDateParts = toFormat.slice(0, -1).map((part) => {
-    if (part === 'YYYY') {
-      return dateObj['YY'] ? convertToYYYY(dateObj['YY']) : dateObj['YYYY'];
-    } else if (part === 'YY') {
-      return dateObj['YYYY'] ? convertToYY(dateObj['YYYY']) : dateObj['YY'];
-    }
-
-    return dateObj[part];
-  });
-
   const separatorTo = toFormat[toFormat.length - 1];
 
-  return newDateParts.join(separatorTo);
+  const dateMap = {};
+
+  fromFormat.slice(0, -1).forEach((part, index) => {
+    dateMap[part] = dateParts[index];
+  });
+
+  const result = toFormat.slice(0, -1).map((part) => {
+    if (part === 'YY') {
+      return convertToYY(dateMap['YYYY'] || dateMap['YY']);
+    }
+
+    if (part === 'YYYY') {
+      return dateMap['YYYY'] || convertToYYYY(dateMap['YY']);
+    }
+
+    return dateMap[part];
+  });
+
+  return result.join(separatorTo);
 }
 module.exports = formatDate;
