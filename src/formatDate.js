d761fbca298ac6f89027b8944478abe7de6f4353
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-01 19:13:30 +0200

    add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,41 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateArrow = date.split(fromFormat[fromFormat.length - 1]);
+  const result = [];
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (dateArrow[fromFormat.indexOf(toFormat[i])] === undefined) {
+      fromFormat[i] = toFormat[i];
+    }
+
+    if (
+      dateArrow[fromFormat.indexOf(toFormat[i])].length < toFormat[i].length &&
+      dateArrow[fromFormat.indexOf(toFormat[i])] < 30
+    ) {
+      dateArrow[fromFormat.indexOf(toFormat[i])] =
+        '20' + dateArrow[fromFormat.indexOf(toFormat[i])];
+    }
+
+    if (
+      dateArrow[fromFormat.indexOf(toFormat[i])].length < toFormat[i].length &&
+      dateArrow[fromFormat.indexOf(toFormat[i])] >= 30
+    ) {
+      dateArrow[fromFormat.indexOf(toFormat[i])] =
+        '19' + dateArrow[fromFormat.indexOf(toFormat[i])];
+    }
+
+    if (
+      dateArrow[fromFormat.indexOf(toFormat[i])].length > toFormat[i].length
+    ) {
+      dateArrow[fromFormat.indexOf(toFormat[i])] =
+        dateArrow[fromFormat.indexOf(toFormat[i])].slice(2);
+    }
+
+    result[i] = dateArrow[fromFormat.indexOf(toFormat[i])];
+  }
+
+  return result.join(toFormat[toFormat.length - 1]);
 }
 
 module.exports = formatDate;
