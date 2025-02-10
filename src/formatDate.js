15042f803a529d9e67ab2d23f51171512fc6868a
Illia <hetsko.ilya@gmail.com>
2025-02-10 21:40:18 +0200

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,36 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const separatorFrom = fromFormat[3];
+  const separatorTo = toFormat[3];
+
+  const parts = date.split(separatorFrom);
+  const dateObj = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateObj[fromFormat[i]] = parts[i];
+  }
+
+  if (fromFormat[0] === 'YYYY' && toFormat[0] === 'YY') {
+    dateObj['YY'] = dateObj['YYYY'].slice(2);
+  } else if (fromFormat[0] === 'YY' && toFormat[0] === 'YYYY') {
+    let yearPrefix;
+
+    if (dateObj['YY'] < '30') {
+      yearPrefix = '20';
+    } else {
+      yearPrefix = '19';
+    }
+    dateObj['YYYY'] = yearPrefix + dateObj['YY'];
+  }
+
+  const newDate = [];
+
+  for (let i = 0; i < 3; i++) {
+    newDate.push(dateObj[toFormat[i]]);
+  }
+
+  return newDate.join(separatorTo);
 }
 
 module.exports = formatDate;
