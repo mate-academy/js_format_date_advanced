583bd3452add0856e240cd318aba03a7da0d0458
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-02-24 16:36:22 +0200

    add small fixes

--- src/formatDate.js
+++ src/formatDate.js
@@ -14,15 +14,15 @@ function formatDate(date, fromFormat, toFormat) {
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
     for (let y = 0; y < toFormat.length - 1; y++) {
-      if ((fromFormat[i] === 'YYYY') & (toFormat[y] === 'YY')) {
+      if (fromFormat[i] === 'YYYY' && toFormat[y] === 'YY') {
         newDateFormat[y] = dateArray[i].split('').slice(2).join('');
       }
 
-      if ((fromFormat[i] === 'YY') & (Number(dateArray[i]) < 30)) {
+      if (fromFormat[i] === 'YY' && Number(dateArray[i]) < 30) {
         newDateFormat[y] = `20${dateArray[i]}`;
       }
 
-      if ((fromFormat[i] === 'YY') & (Number(dateArray[i]) >= 30)) {
+      if (fromFormat[i] === 'YY' && Number(dateArray[i]) >= 30) {
         newDateFormat[y] = `19${dateArray[i]}`;
       }
 
