47e616f948d1db40d6cc17fbac94c0e93670f3c2
Yvhenii <mtr1289@gmail.com>
2025-03-26 22:50:12 +0200

    errr

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,24 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSep = fromFormat[3];
+  const toSep = toFormat[3];
+  const parts = date.split(fromSep);
+  const dateParts = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateParts[fromFormat[i]] = parts[i];
+  }
+
+  if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    const year = parseInt(dateParts['YY'], 10);
+    dateParts['YYYY'] = year < 30 ? `20${dateParts['YY']}` : `19${dateParts['YY']}`;
+    } else if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    dateParts['YY'] = dateParts['YYYY'].slice(-2);
+  }
+
+  return [dateParts[toFormat[0]], dateParts[toFormat[1]], dateParts[toFormat[2]]].join(toSep);
+  
 }
 
 module.exports = formatDate;
