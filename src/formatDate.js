448457e106e9c461a4e030e9fb1d3ad9a6da31fa
Alex Kryzhanovskyi <kryzhanoskyi.oleksandr@gmail.com>
2025-04-01 16:34:20 +0300

    formatDateSolution

--- src/formatDate.js
+++ src/formatDate.js
@@ -6,9 +6,38 @@
  * @param {string[]} toFormat
  *
  * @returns {string}
+ *
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const dateParts = date.split(fromFormat[3]);
+  const structuredDate = {};
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    structuredDate[fromFormat[i]] = dateParts[i];
+  }
+
+  if (structuredDate['YY']) {
+    const year = parseInt(structuredDate['YY']);
+
+    if (year < 30) {
+      structuredDate['YYYY'] = `20${structuredDate['YY']}`;
+    } else {
+      structuredDate['YYYY'] = `19${structuredDate['YY']}`;
+    }
+  }
+
+  const result = toFormat.slice(0, -1).map((part) => {
+    if (part === 'YYYY') {
+      return structuredDate['YYYY'];
+    } else if (part === 'YY') {
+      return structuredDate['YYYY'].slice(2);
+    } else {
+      return structuredDate[part];
+    }
+  });
+
+  // nconsole.log(result.join(toFormat[3]));
+  return result.join(toFormat[3]);
 }
 
 module.exports = formatDate;
