d971ee6f8796e537994941bb29ef85460e302e40
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-04-19 15:23:12 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,42 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldSeparator = fromFormat[fromFormat.length - 1];
+  const oldDate = date.split(oldSeparator);
+
+  function findIndex(format, value) {
+    for (let i = 0; i < format.length; i++) {
+      if (format[i] === value) {
+        return i;
+      }
+    }
+  }
+
+  const day = oldDate[findIndex(fromFormat, 'DD')];
+  const month = oldDate[findIndex(fromFormat, 'MM')];
+  const year = oldDate[findIndex(fromFormat, 'YYYY')];
+  const shortYear = oldDate[findIndex(fromFormat, 'YY')];
+
+  const newDayIndex = findIndex(toFormat, 'DD');
+  const newMonthIndex = findIndex(toFormat, 'MM');
+  const newYearIndex = findIndex(toFormat, 'YYYY') ?? findIndex(toFormat, 'YY');
+
+  const newSeparator = toFormat[toFormat.length - 1];
+  const newDate = [];
+
+  newDate[newDayIndex] = day;
+  newDate[newMonthIndex] = month;
+
+  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
+    newDate[newYearIndex] = year.slice(2);
+  } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
+    newDate[newYearIndex] =
+      shortYear < 30 ? `20${shortYear}` : `19${shortYear}`;
+  } else {
+    newDate[newYearIndex] = year || shortYear;
+  }
+
+  return newDate.join(newSeparator);
 }
 
 module.exports = formatDate;
