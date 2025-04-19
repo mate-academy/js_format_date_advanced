8963a57916e8b9b83b7273fe025d24e974453b05
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-04-19 15:38:34 +0300

    solution 2

--- src/formatDate.js
+++ src/formatDate.js
@@ -21,8 +21,9 @@ function formatDate(date, fromFormat, toFormat) {
 
   const day = oldDate[findIndex(fromFormat, 'DD')];
   const month = oldDate[findIndex(fromFormat, 'MM')];
-  const year = oldDate[findIndex(fromFormat, 'YYYY')];
-  const shortYear = oldDate[findIndex(fromFormat, 'YY')];
+  const year =
+    oldDate[findIndex(fromFormat, 'YYYY')] ??
+    oldDate[findIndex(fromFormat, 'YY')];
 
   const newDayIndex = findIndex(toFormat, 'DD');
   const newMonthIndex = findIndex(toFormat, 'MM');
@@ -37,10 +38,9 @@ function formatDate(date, fromFormat, toFormat) {
   if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
     newDate[newYearIndex] = year.slice(2);
   } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
-    newDate[newYearIndex] =
-      shortYear < 30 ? `20${shortYear}` : `19${shortYear}`;
+    newDate[newYearIndex] = year < 30 ? `20${year}` : `19${year}`;
   } else {
-    newDate[newYearIndex] = year || shortYear;
+    newDate[newYearIndex] = year;
   }
 
   return newDate.join(newSeparator);
