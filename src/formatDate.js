a5037e04c86270f6d9784405379404f80fcf4999
Yana Lunina <yanalunina96@gmail.com>
2025-04-28 20:34:06 +0300

    Solution 2.0

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,26 +8,22 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const separators = ['-', '.', '/'];
-  let separator = '';
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
 
-  for (let i = 0; i < separators.length; i++) {
-    if (date.includes(separators[i])) {
-      separator = separators[i];
-      break;
-    }
-  }
-
-  const dateParts = date.split(separator);
+  const dateParts = date.split(fromSeparator);
   const dateMap = {};
   const resultParts = [];
 
-  for (let i = 0; i < fromFormat.length; i++) {
-    dateMap[fromFormat[i]] = dateParts[i];
+  const fromParts = fromFormat.slice(0, -1);
+  const toParts = toFormat.slice(0, -1);
+
+  for (let i = 0; i < fromParts.length; i++) {
+    dateMap[fromParts[i]] = dateParts[i];
   }
 
-  for (let i = 0; i < toFormat.length; i++) {
-    const part = toFormat[i];
+  for (let i = 0; i < toParts.length; i++) {
+    const part = toParts[i];
     let value = dateMap[part];
 
     if (part === 'YYYY' && !value) {
@@ -43,7 +39,7 @@ function formatDate(date, fromFormat, toFormat) {
     resultParts.push(value);
   }
 
-  return resultParts.join(separator);
+  return resultParts.join(toSeparator);
 }
 
 module.exports = formatDate;
