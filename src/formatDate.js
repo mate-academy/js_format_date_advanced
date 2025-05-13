1648489d84d2ba319b77563eadc1d81bd01d8241
Nazariy <nazariypanochko@gmail.com>
2025-05-13 14:21:26 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,30 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separatorFrom = fromFormat[3];
+  const separatorTo = toFormat[3];
+
+  const dateParts = date.split(separatorFrom);
+  const map = {};
+
+  for (let i = 0; i < 3; i++) {
+    map[fromFormat[i]] = dateParts[i];
+  }
+
+  if (toFormat.includes('YYYY') && fromFormat.includes('YY')) {
+    const yy = map['YY'];
+    const num = Number(yy);
+
+    map['YYYY'] = num < 30 ? `20${yy}` : `19${yy}`;
+  }
+
+  if (toFormat.includes('YY') && fromFormat.includes('YYYY')) {
+    map['YY'] = map['YYYY'].slice(2);
+  }
+
+  const resultParts = toFormat.slice(0, 3).map((format) => map[format]);
+
+  return resultParts.join(separatorTo);
 }
 
 module.exports = formatDate;
