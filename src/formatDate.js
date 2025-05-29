15fc0c82904e0aa6778c7a482e413783c4091ce5
vasylzelenchuk <vasyazyasinya@gmail.com>
2025-05-29 14:08:43 +0200

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,63 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let fromSeparator;
+
+  for (const char of date) {
+    if (!(char >= '0' && char <= '9')) {
+      fromSeparator = char;
+      break;
+    }
+  }
+
+  let cleanedDate = date.trim();
+
+  if (cleanedDate.endsWith(fromSeparator)) {
+    cleanedDate = cleanedDate.slice(0, -1);
+  }
+
+  const dateParts = cleanedDate
+    .split(fromSeparator)
+    .filter((part) => part !== '');
+
+  const dateMap = {};
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    dateMap[fromFormat[i]] = dateParts[i];
+  }
+
+  let toSeparator = '';
+
+  for (const item of toFormat) {
+    if (!(item === 'YYYY' || item === 'YY' || item === 'MM' || item === 'DD')) {
+      toSeparator = item;
+      break;
+    }
+  }
+
+  if (!toSeparator) {
+    toSeparator = fromSeparator;
+  }
+
+  const formattedParts = toFormat
+    .filter(
+      (part) =>
+        part === 'YYYY' || part === 'YY' || part === 'MM' || part === 'DD',
+    )
+    .map((part) => {
+      if (part === 'YY' && dateMap['YYYY']) {
+        return dateMap['YYYY'].slice(-2);
+      } else if (part === 'YYYY' && dateMap['YY']) {
+        const yy = parseInt(dateMap['YY'], 10);
+        const prefix = yy < 30 ? '20' : '19';
+
+        return prefix + dateMap['YY'];
+      } else {
+        return dateMap[part];
+      }
+    });
+
+  return formattedParts.join(toSeparator);
 }
 
 module.exports = formatDate;
