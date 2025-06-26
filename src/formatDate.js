e9c2a85fbff160563d7e13b82c69bc92657d0cb9
Denys Herych <fojjof2@gmail.com>
2025-06-26 21:48:32 +0200

    dynamic solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,135 +8,68 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
+  const separatorFrom = fromFormat[3];
+  const separatorTo = toFormat[3];
   let year = null;
   let day = null;
   let month = null;
-  let newArr;
+  const newArr = date.split(separatorFrom);
+  const resultArr = [];
 
-  // #region toFormat
-  switch (fromFormat.join('')) {
-    case 'YYYYMMDD-':
-      newArr = date.split('-');
-      year = newArr[0];
-      month = newArr[1];
-      day = newArr[2];
+  for (let i = 0; i < 3; i++) {
+    switch (fromFormat[i]) {
+      case 'YYYY':
+        year = newArr[i];
 
-      break;
+        break;
 
-    case 'YYYYMMDD.':
-      newArr = date.split('.');
-      year = newArr[0];
-      month = newArr[1];
-      day = newArr[2];
+      case 'MM':
+        month = newArr[i];
+        break;
 
-      break;
+      case 'YY':
+        if (newArr[i] >= 30) {
+          year = '19' + newArr[i];
+        } else {
+          year = '20' + newArr[i];
+        }
+        break;
 
-    case 'DDMMYYYY.':
-      newArr = date.split('.');
-      year = newArr[2];
-      month = newArr[1];
-      day = newArr[0];
+      case 'DD':
+        day = newArr[i];
+        break;
 
-      break;
-    case 'DDMMYYYY-':
-      newArr = date.split('-');
-      year = newArr[2];
-      month = newArr[1];
-      day = newArr[0];
-
-      break;
-    case 'DDMMYY/':
-      newArr = date.split('/');
-
-      if (newArr[2] <= 30) {
-        year = '20' + newArr[2];
-      } else {
-        year = '19' + newArr[2];
-      }
-      month = newArr[1];
-      day = newArr[0];
-
-      break;
-
-    case 'YYMMDD/':
-      newArr = date.split('/');
-
-      if (newArr[0] >= 30) {
-        year = '19' + newArr[0];
-      } else {
-        year = '20' + newArr[0];
-      }
-      month = newArr[1];
-      day = newArr[2];
-
-      break;
-
-    case 'MMYYYYDD-':
-      newArr = date.split('-');
-      year = newArr[1];
-      month = newArr[0];
-      day = newArr[2];
-
-      break;
-
-    case 'MMDDYYYY/':
-      newArr = date.split('/');
-      year = newArr[2];
-      month = newArr[0];
-      day = newArr[1];
-
-      break;
-
-    case 'YYMMDD.':
-      newArr = date.split('.');
-
-      if (newArr[0] >= 30) {
-        year = '19' + newArr[0];
-      } else {
-        year = '20' + newArr[0];
-      }
-      month = newArr[1];
-      day = newArr[2];
-
-      break;
-
-    default:
+      default:
+        break;
+    }
   }
 
-  if (year === '00') {
-    year = '2000';
+  for (let i = 0; i < 3; i++) {
+    switch (toFormat[i]) {
+      case 'DD':
+        resultArr[i] = day;
+        break;
+
+      case 'MM':
+        resultArr[i] = month;
+        break;
+
+      case 'YY':
+        resultArr[i] = year.slice(2);
+        break;
+
+      case 'YYYY':
+        resultArr[i] = year;
+        break;
+
+      default:
+        break;
+    }
   }
 
-  // #endregion
+  const resultStr = resultArr.join(separatorTo);
 
-  // #region fromFormat
-  switch (toFormat.join('')) {
-    case 'YYYYMMDD.':
-      return year + '.' + month + '.' + day;
-    case 'DDMMYYYY-':
-      return day + '-' + month + '-' + year;
-    case 'DDMMYY/':
-      return day + '/' + month + '/' + year.slice(2);
-
-    case 'YYYYMMDD-':
-      return year + '-' + month + '-' + day;
-
-    case 'MMDDYY/':
-      return month + '/' + day + '/' + year.slice(2);
-
-    case 'YYYYMMDD/':
-      return year + '/' + month + '/' + day;
-    default:
-      break;
-  }
-
-  // #endregion
+  return resultStr;
 }
 
 module.exports = formatDate;
-
-// formatDate(
-//         '2012-12-21',
-//         ['YYYY', 'MM', 'DD', '-'],
-//         ['YYYY', 'MM', 'DD', '.'],
-//       );
