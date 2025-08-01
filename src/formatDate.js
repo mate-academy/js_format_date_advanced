4180fa4bf72d111757f0b04d881c58e619b6f0ae
Vasyliev Vadym <protocol222333@gmail.com>
2025-08-01 13:31:57 +0300

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,39 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSep = fromFormat.at(-1);
+  const toSep = toFormat.at(-1);
+  const dateParts = date.split(fromSep);
+  const parseDate = {};
+  const formattedDate = [];
+
+  for (let i = 0; i < dateParts.length; i++) {
+    parseDate[fromFormat[i]] = dateParts[i];
+  }
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i] === toSep) {
+      continue;
+    }
+
+    const key = toFormat[i];
+
+    if (key === 'YY' && parseDate['YYYY']) {
+      formattedDate.push(parseDate['YYYY'].slice(-2));
+    } else if (key === 'YYYY' && parseDate['YY']) {
+      const year = +parseDate['YY'];
+
+      if (year < 30) {
+        formattedDate.push(year + 2000);
+      } else {
+        formattedDate.push(year + 1900);
+      }
+    } else {
+      formattedDate.push(parseDate[key]);
+    }
+  }
+
+  return formattedDate.join(toSep);
 }
 
 module.exports = formatDate;
