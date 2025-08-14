ba3e329d84c453ef976879d244d04a8e006356e8
Jorge Menezes Jr <jgrei.junior@gmail.com>
2025-08-13 23:14:48 -0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,31 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+
+  const parts = date.split(fromSeparator);
+
+  const dateObject = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateObject[fromFormat[i]] = parts[i];
+  }
+
+  if (fromFormat.includes('YY') && !toFormat.includes('YY')) {
+    const yy = parseInt(dateObject['YY'], 10);
+
+    dateObject['YYYY'] =
+      yy < 30 ? `20${dateObject['YY']}` : `19${dateObject['YY']}`;
+  } else if (fromFormat.includes('YYYY') && !toFormat.includes('YYYY')) {
+    dateObject['YY'] = dateObject['YYYY'].slice(-2);
+  }
+
+  const result = [toFormat[0], toFormat[1], toFormat[2]]
+    .map((part) => dateObject[part])
+    .join(toSeparator);
+
+  return result;
 }
 
 module.exports = formatDate;
