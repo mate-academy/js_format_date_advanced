68ee3f49eaa5d83822dcffd23b9781bb4fada560
yahohulia <yaroslavhohulia@gmail.com>
2025-09-29 19:10:17 +0300

    add js solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,32 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const days = date.split(fromFormat[3]);
+  const map = {
+    [fromFormat[0]]: days[0],
+    [fromFormat[1]]: days[1],
+    [fromFormat[2]]: days[2],
+  };
+
+  const calendar = [];
+
+  for (let i = 0; i < 3; i++) {
+    if (toFormat[i] === 'YYYY' && map['YY']) {
+      if (parseInt(map['YY'], 10) < 30) {
+        map['YYYY'] = '20' + map['YY'];
+      } else {
+        map['YYYY'] = '19' + map['YY'];
+      }
+    }
+
+    if (toFormat[i] === 'YY' && map['YYYY']) {
+      map['YY'] = map['YYYY'].slice(-2);
+    }
+  }
+
+  calendar.push(map[toFormat[0]], map[toFormat[1]], map[toFormat[2]]);
+
+  return calendar.join(toFormat[3]);
 }
 
 module.exports = formatDate;
