a8a7d07663fc8d501690b232b403f8cb3d33be53
Josias <josiasonesokataborda22@gmail.com>
2025-10-07 11:51:26 -0300

    resolucao da atividade

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,40 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separatorFrom = fromFormat[fromFormat.length - 1];
+  const separatorTo = toFormat[toFormat.length - 1];
+
+  const dateParts = date.split(separatorFrom);
+  const dateMap = {};
+
+  for (let i = 0; i < 3; i++) {
+    dateMap[fromFormat[i]] = dateParts[i];
+  }
+
+  const newParts = [];
+
+  for (let i = 0; i < 3; i++) {
+    const part = toFormat[i];
+    let value = dateMap[part];
+
+    if (value === undefined) {
+      if (part === 'YYYY') {
+        value = dateMap['YY'];
+      } else if (part === 'YY') {
+        value = dateMap['YYYY'];
+      }
+    }
+
+    if (part === 'YYYY' && value.length === 2) {
+      value = Number(value) < 30 ? `20${value}` : `19${value}`;
+    } else if (part === 'YY' && value.length === 4) {
+      value = value.slice(2);
+    }
+
+    newParts.push(value);
+  }
+
+  return newParts.join(separatorTo);
 }
 
 module.exports = formatDate;
