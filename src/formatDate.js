2b44a8268281a9b243d312c7cc0e9642fddbcedb
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-07 07:22:18 -0300

    development of format date advanced

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,60 @@
  *
  * @returns {string}
  */
+
+
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  let fourDigitsFrom = false;
+  let fourDigitsTo = false;
+  const separatorFrom = fromFormat[3];
+  const separatorTo = toFormat[3];
+  const newDate = [];
+
+ const text = date.split(separatorFrom);
+
+  let indexFromYear = fromFormat.indexOf('YY');
+  const indexFromMonth = fromFormat.indexOf('MM');
+  const indexFromDay = fromFormat.indexOf('DD');
+
+
+  const day = text[indexFromDay];
+  const month = text[indexFromMonth];
+  let year = text[indexFromYear];
+  if (indexFromYear === -1) {
+    indexFromYear = fromFormat.indexOf('YYYY');
+    year = text[indexFromYear];
+    fourDigitsFrom = true;
+  }
+
+  let indexToYear = toFormat.indexOf('YY');
+  const indexToMonth = toFormat.indexOf('MM');
+  const indexToDay = toFormat.indexOf('DD');
+  if (indexToYear === -1) {
+    indexToYear = toFormat.indexOf('YYYY');
+    fourDigitsTo = true;
+  }
+
+  if (fourDigitsFrom) {
+    if ( !fourDigitsTo) {
+      year = year.slice(2);
+    }
+  } else if (fourDigitsTo){
+      if (parseInt(year) < 30) {
+        year = '20' + year;
+      } else {
+        year = '19' + year;
+      }
+  }
+
+  newDate[indexToDay] = day;
+  newDate[indexToMonth] = month;
+  newDate[indexToYear] = year;
+
+  return newDate.join(separatorTo);
+
+
+};
+
 
 module.exports = formatDate;
