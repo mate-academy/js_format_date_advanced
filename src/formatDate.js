7e7f88df6f9d4652681ce8aacf9b3603bdd3f7bf
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-10-30 01:16:30 +0200

    Add format date

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,38 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+  const fromPartOfTheDate = fromFormat.slice(0, 3);
+  const toPartOfTheDate = toFormat.slice(0, 3);
+  const currentDate = date.split(fromSeparator);
+  const dataDate = {};
+
+  for (let i = 0; i < fromPartOfTheDate.length; i++) {
+    dataDate[fromPartOfTheDate[i]] = currentDate[i];
+  }
+
+  if (toPartOfTheDate.includes('YY') && dataDate.YYYY) {
+    dataDate.YY = dataDate.YYYY.slice(-2);
+  } else if (toPartOfTheDate.includes('YYYY') && dataDate.YY) {
+    const lastNumbersOfDate = +dataDate.YY;
+    const fullYear =
+      lastNumbersOfDate < 30
+        ? 2000 + lastNumbersOfDate
+        : 1900 + lastNumbersOfDate;
+
+    dataDate.YYYY = String(fullYear);
+  }
+
+  const newDateParts = [];
+
+  for (let i = 0; i < toPartOfTheDate.length; i++) {
+    newDateParts.push(dataDate[toPartOfTheDate[i]]);
+  }
+
+  const resFormatDate = newDateParts.join(toSeparator);
+
+  return resFormatDate;
 }
 
 module.exports = formatDate;
