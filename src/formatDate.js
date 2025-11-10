8020b2f7613453adfc5ca937b70967c7ea83176a
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2025-11-10 13:38:26 +0100

    My format date

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,26 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const parts = date.split(fromFormat[3]);
+  const dateObj = {
+    [fromFormat[0]]: parts[0],
+    [fromFormat[1]]: parts[1],
+    [fromFormat[2]]: parts[2],
+  };
+
+  const result = toFormat.slice(0, 3).map((part) => {
+    if (dateObj[part]) {
+      return dateObj[part];
+    } else if (part === 'YY') {
+      return dateObj['YYYY'].slice(-2);
+    } else if (part === 'YYYY') {
+      const year = dateObj['YY'];
+
+      return parseInt(year) < 30 ? '20' + year : '19' + year;
+    }
+  });
+
+  return result.join(toFormat[3]);
 }
 
 module.exports = formatDate;
