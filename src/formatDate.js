f5f7493542683280ee88b50e07006bf4aea16cac
Rostyslav <rhudovskyi@gmail.com>
2025-12-22 20:17:48 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,34 +8,43 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const separator = toFormat[toFormat.length - 1];
-  const parts = date.split(separator);
+  if (!date) {
+    return {};
+  }
+
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+
+  const parts = date.split(fromSeparator);
   const dateObj = {};
-  let year = dateObj['YYYY'] || dateObj['YY'];
 
   for (let i = 0; i < parts.length; i++) {
     dateObj[fromFormat[i]] = parts[i];
   }
 
-  const targetYearFormat = toFormat.find((f) => f.includes('Y'));
+  const currentYear = dateObj['YYYY'] || dateObj['YY'];
+  let fullYear, shortYear;
 
-  if (targetYearFormat === 'YY' && year.length === 4) {
-    year = year.slice(-2);
-  } else if (targetYearFormat === 'YYYY' && year.length === 2) {
-    year = +year < 30 ? '20' + year : '19' + year;
+  if (currentYear.length === 4) {
+    fullYear = currentYear;
+    shortYear = currentYear.slice(-2);
+  } else {
+    fullYear = +currentYear < 30 ? '20' + currentYear : '19' + currentYear;
+    shortYear = currentYear;
   }
-  dateObj['YYYY'] = year;
-  dateObj['YY'] = year;
+
+  dateObj['YYYY'] = fullYear;
+  dateObj['YY'] = shortYear;
 
   const resultParts = [];
 
-  for (let i = 0; i < toFormat.length - 1; i++) {
+  for (let i = 0; i < 3; i++) {
     const key = toFormat[i];
 
     resultParts.push(dateObj[key]);
   }
 
-  return resultParts.join(separator);
+  return resultParts.join(toSeparator);
 }
 
 module.exports = formatDate;
