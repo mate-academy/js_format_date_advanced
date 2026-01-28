52d7023d85e578287914de4cb25488a9fe75f4a5
uselessmorning <shtoykoigor@ukr.net>
2026-01-28 22:11:27 +0100

    sollution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,37 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSep = fromFormat[fromFormat.length - 1];
+  const toSep = toFormat[toFormat.length - 1];
+
+  const dateValue = date.split(fromSep);
+  const partsArchive = {};
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    const key = fromFormat[i];
+
+    partsArchive[key] = dateValue[i];
+  }
+
+  if (partsArchive['YYYY'] && toFormat.includes('YY')) {
+    partsArchive['YY'] = partsArchive['YYYY'].slice(-2);
+  }
+
+  if (partsArchive['YY'] && toFormat.includes('YYYY')) {
+    const yearShort = Number(partsArchive['YY']);
+
+    partsArchive['YYYY'] = (yearShort < 30 ? '20' : '19') + partsArchive['YY'];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    const key = toFormat[i];
+
+    result.push(partsArchive[key]);
+  }
+
+  return result.join(toSep); // <--- Фінальний результат
 }
 
 module.exports = formatDate;
