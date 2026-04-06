4c7c3fa02b8fb58eb60791738f6e240bb74622ff
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-06 18:47:20 +0000

    feat: implement date formatting function

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,37 @@
  *
  * @returns {string}
  */
-function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+const formatDate = (date, fromFormat, toFormat) => {
+  const DATE_LENGTH = 3;
+
+  const initialSeparator = fromFormat[fromFormat.length - 1];
+  const targetSeparator = toFormat[toFormat.length - 1];
+
+  const initialDateParts = date.split(initialSeparator);
+
+  const dateParts = {};
+
+  for (let i = 0; i < DATE_LENGTH; i++) {
+    dateParts[fromFormat[i]] = initialDateParts[i];
+  }
+
+  if (!dateParts['YYYY']) {
+    const shortYear = dateParts['YY'];
+
+    dateParts['YYYY'] = (shortYear < 30 ? '20' : '19') + shortYear;
+  }
+
+  if (!dateParts['YY']) {
+    dateParts['YY'] = dateParts['YYYY'].slice(-2);
+  }
+
+  const targetDateParts = [];
+
+  for (let i = 0; i < DATE_LENGTH; i++) {
+    targetDateParts.push(dateParts[toFormat[i]]);
+  }
+
+  return targetDateParts.join(targetSeparator);
+};
 
 module.exports = formatDate;
