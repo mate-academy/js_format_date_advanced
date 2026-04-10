68e72c5858b651d070368e2a77af53a32ac6b0ba
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-04-10 23:20:03 +0200

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,29 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const parts = date.split(fromFormat[3]);
+  const keys = fromFormat.slice(0, 3);
+  let result = [];
+
+  const mapping = Object.fromEntries(
+    keys.map((key, index) => [key, parts[index]]),
+  );
+
+  if (mapping.YYYY) {
+    mapping.YY = mapping.YYYY.slice(-2);
+  } else if (mapping.YY) {
+    const yy = mapping.YY.padStart(2, '0');
+
+    mapping.YYYY = (Number(yy) < 30 ? '20' : '19') + yy;
+  }
+
+  result = [
+    mapping[toFormat[0]],
+    mapping[toFormat[1]],
+    mapping[toFormat[2]],
+  ].join(toFormat[3]);
+
+  return result;
 }
 
 module.exports = formatDate;
