94266e772e7cb009ba1870c6439e8c04905ba135
Oleksandr Shyshenok <shyshenok81@gmail.com>
2026-04-17 20:40:34 +0300

    first solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,37 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+
+  const dateParts = date.split(fromSeparator);
+
+  // Розкладаємо в об'єкт для зручності
+  const map = {};
+
+  for (let i = 0; i < 3; i++) {
+    map[fromFormat[i]] = dateParts[i];
+  }
+
+  // Обробка року
+  if (map['YYYY']) {
+    map['YY'] = map['YYYY'].slice(-2);
+  }
+
+  if (map['YY'] && !map['YYYY']) {
+    const year = Number(map['YY']);
+
+    map['YYYY'] = year < 30 ? '20' + map['YY'] : '19' + map['YY'];
+  }
+
+  // Формуємо нову дату
+  const result = [];
+
+  for (let i = 0; i < 3; i++) {
+    result.push(map[toFormat[i]]);
+  }
+
+  return result.join(toSeparator);
 }
 
 module.exports = formatDate;
