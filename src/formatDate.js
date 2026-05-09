3edf758123fb0def2dbd6cef75ae136279c904c9
nex1994 <bilicki1994@gmail.com>
2026-05-09 10:03:03 +0200

    init

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,33 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSep = fromFormat[fromFormat.length - 1];
+  const toSep = toFormat[toFormat.length - 1];
+
+  const parts = date.split(fromSep);
+  const fromTokens = fromFormat.slice(0, -1);
+
+  const valueMap = {};
+
+  fromTokens.forEach((token, i) => {
+    valueMap[token] = parts[i];
+  });
+
+  if (valueMap['YY'] !== undefined && valueMap['YYYY'] === undefined) {
+    const yy = parseInt(valueMap['YY'], 10);
+
+    valueMap['YYYY'] =
+      (yy < 30 ? '20' : '19') + valueMap['YY'].padStart(2, '0');
+  }
+
+  if (valueMap['YYYY'] !== undefined && valueMap['YY'] === undefined) {
+    valueMap['YY'] = valueMap['YYYY'].slice(-2);
+  }
+
+  return toFormat
+    .slice(0, -1)
+    .map((token) => valueMap[token])
+    .join(toSep);
 }
 
 module.exports = formatDate;
