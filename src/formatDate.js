b8dc9608a6bb8a219c4ef3b1d3e9b771992df03b
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-05-11 12:28:16 +0300

    try1

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,44 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let result = '';
+
+  const dateParts = date.split(fromFormat[3]);
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    let value = '';
+
+    // YYYY -> YY
+    if (toFormat[i] === 'YY' && fromFormat.includes('YYYY')) {
+      const yearIndex = fromFormat.indexOf('YYYY');
+
+      value = dateParts[yearIndex].slice(-2);
+
+      // YY -> YYYY
+    } else if (toFormat[i] === 'YYYY' && fromFormat.includes('YY')) {
+      const yearIndex = fromFormat.indexOf('YY');
+
+      const shortYear = Number(dateParts[yearIndex]);
+
+      if (shortYear < 30) {
+        value = `20${dateParts[yearIndex]}`;
+      } else {
+        value = `19${dateParts[yearIndex]}`;
+      }
+    } else {
+      const partIndex = fromFormat.indexOf(toFormat[i]);
+
+      value = dateParts[partIndex];
+    }
+
+    result += value;
+
+    if (i !== toFormat.length - 2) {
+      result += toFormat[toFormat.length - 1];
+    }
+  }
+
+  return result;
 }
 
 module.exports = formatDate;
