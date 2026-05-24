0980c9ce5112eff42c0b9a9d0c73b50780fd5e2b
Tendeeer <leifisisidj747@gmail.com>
2026-05-24 23:45:09 +0300

    Add date format conversion with YY/YYYY support

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,31 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const fromSeparator = fromFormat.at(-1);
+  const toSeparator = toFormat.at(-1);
 
+  const fromParts = fromFormat.slice(0, -1);
+  const toParts = toFormat.slice(0, -1);
+
+  const dateParts = date.split(fromSeparator);
+
+  const dateObject = {};
+
+  fromParts.forEach((part, index) => {
+    dateObject[part] = dateParts[index];
+  });
+
+  if (fromParts.includes('YYYY') && toParts.includes('YY')) {
+    dateObject['YY'] = dateObject['YYYY'].slice(-2);
+  }
+
+  if (fromParts.includes('YY') && toParts.includes('YYYY')) {
+    const yy = dateObject['YY'];
+    const prefix = parseInt(yy) < 30 ? '20' : '19';
+
+    dateObject['YYYY'] = prefix + yy;
+  }
+
+  return toParts.map((part) => dateObject[part]).join(toSeparator);
+}
 module.exports = formatDate;
