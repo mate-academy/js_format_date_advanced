d97b5280ee6386f81709e44fd4e7d95f04d9d93d
svladimir1010 <vladimireur@gmail.com>
2026-05-25 23:28:41 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,33 @@
  *
  * @returns {string}
  */
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const sepFrom = fromFormat.at(3);
+  const sepTo = toFormat.at(3);
+
+  const fromDateArray = date.split(sepFrom);
+  const fromDateObj = {};
+
+  fromFormat.slice(0, 3).forEach((format, index) => {
+    fromDateObj[format] = fromDateArray[index];
+  });
+
+  if (fromDateObj.YYYY) {
+    fromDateObj.YY = fromDateObj.YYYY.slice(2);
+  } else {
+    const shortYear = +fromDateObj.YY;
+
+    fromDateObj.YYYY = String(
+      shortYear < 30 ? 2000 + shortYear : 1900 + shortYear,
+    );
+  }
+
+  const newDateArray = toFormat
+    .slice(0, 3)
+    .map((format) => fromDateObj[format]);
+
+  return newDateArray.join(sepTo);
 }
 
 module.exports = formatDate;
