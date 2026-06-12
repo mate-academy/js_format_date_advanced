f989dbde75cb636e1a9050bb2f14b5ff138b26fc
olhalutsenko <oleshna.27.l@icloud.com>
2026-06-12 17:05:14 +0200

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,51 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let separator = '';
+
+  for (let i = 0; i < date.length; i++) {
+    if (date[i] < '0' || date[i] > '9') {
+      separator = date[i];
+      break;
+    }
+  }
+
+  const parts = date.split(separator);
+
+  const result = {};
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    let value = parts[i];
+    const key = fromFormat[i];
+
+    if (key === 'YYYY') {
+      value = value.slice(-2);
+    }
+
+    if (key === 'YY') {
+      if (Number(value) < 30) {
+        value = '20' + value;
+      } else {
+        value = '19' + value;
+      }
+    }
+
+    let normalizedKey = key;
+
+    if (key === 'YY') {
+      normalizedKey = 'YYYY';
+    }
+    result[normalizedKey] = value;
+    result[key] = value;
+  }
+
+  const output = [];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    output.push(result[toFormat[i]]);
+  }
+
+  return output.join(toFormat[3]);
 }
 
 module.exports = formatDate;
