05070b625948d932e92df7849e9f1ee512180d97
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-06-24 11:57:22 +0300

    Implement `formatDate` function for flexible date conversions

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,41 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const parts = date.split(fromFormat[fromFormat.length - 1]);
+  const labels = fromFormat.slice(0, -1);
+
+  const values = {};
+
+  labels.forEach((label, index) => {
+    values[label] = parts[index];
+  });
+
+  const results = [];
+
+  for (const type of toFormat.slice(0, -1)) {
+    if (type === 'YYYY' || type === 'YY') {
+      results.push(getYear(type, values));
+    } else {
+      results.push(values[type]);
+    }
+  }
+
+  return results.join(toFormat[toFormat.length - 1]);
+}
+
+function getYear(type, values) {
+  if (
+    (type === 'YYYY' && 'YYYY' in values) ||
+    (type === 'YY' && 'YY' in values)
+  ) {
+    return values[type];
+  } else if (type === 'YY') {
+    return values['YYYY'].slice(2);
+  } else {
+    const year = values['YY'];
+
+    return Number(year) < 30 ? '20' + year : '19' + year;
+  }
 }
 
 module.exports = formatDate;
