df441f850b02db01471e5f1db898a0f1341a2c32
DYM <dmetedji@gmail.com>
2026-07-14 08:02:51 +0000

    Add formatDate solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -13,37 +13,26 @@ function formatDate(date, fromFormat, toFormat) {
   const century19 = 1900;
   const century20 = 2000;
   const yearBorder = 30;
-  let toYearFormat = '';
-  let fromYearFormat = '';
   let result = '';
 
-  if (toFormat.includes('YYYY')) {
-    toYearFormat = 'YYYY';
-  } else {
-    toYearFormat = 'YY';
-  }
-
-  if (fromFormat.includes('YYYY')) {
-    fromYearFormat = 'YYYY';
-  } else {
-    fromYearFormat = 'YY';
-  }
+  const toYearFormat = toFormat.includes('YYYY') ? 'YYYY' : 'YY';
+  const fromYearFormat = fromFormat.includes('YYYY') ? 'YYYY' : 'YY';
 
   const fromYearIndex = fromFormat.indexOf(fromYearFormat);
   const toYearIndex = toFormat.indexOf(toYearFormat);
 
-  if (fromYearFormat === toYearFormat) {
-    newDate[toYearIndex] = dateSplit[fromYearIndex];
-  } else {
-    if (toYearFormat === 'YYYY') {
-      if (+dateSplit[fromYearIndex] < yearBorder) {
-        newDate[toYearIndex] = century20 + +dateSplit[fromYearIndex];
-      } else {
-        newDate[toYearIndex] = century19 + +dateSplit[fromYearIndex];
-      }
+  if (fromYearFormat === 'YY') {
+    if (+dateSplit[fromYearIndex] < yearBorder) {
+      dateSplit[fromYearIndex] = century20 + +dateSplit[fromYearIndex];
     } else {
-      newDate[toYearIndex] = dateSplit[fromYearIndex] - century19;
+      dateSplit[fromYearIndex] = century19 + +dateSplit[fromYearIndex];
     }
+  }
+
+  if (toYearFormat === 'YY') {
+    newDate[toYearIndex] = dateSplit[fromYearIndex].slice(-2);
+  } else {
+    newDate[toYearIndex] = dateSplit[fromYearIndex];
   }
 
   newDate[toFormat.indexOf('MM')] = dateSplit[fromFormat.indexOf('MM')];
