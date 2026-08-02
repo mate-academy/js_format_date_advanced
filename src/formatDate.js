5f1a1b486cf1c1ec85ae6f93490b0b3efc6cf6d6
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-08-03 00:55:47 +0300

    add

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,46 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  if (!date) {
+    return undefined;
+  }
+
+  const oldSeparator = fromFormat[fromFormat.length - 1];
+  const dateParts = date.split(oldSeparator);
+
+  const dateMap = {};
+
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    const key = fromFormat[i];
+    const value = dateParts[i];
+
+    dateMap[key] = value;
+  }
+
+  if (toFormat.includes('YY') && dateMap['YYYY']) {
+    dateMap['YY'] = dateMap['YYYY'].slice(-2);
+  }
+
+  if (toFormat.includes('YYYY') && dateMap['YY']) {
+    const year = dateMap['YY'];
+
+    if (Number(year) < 30) {
+      dateMap['YYYY'] = `20${year}`;
+    } else {
+      dateMap['YYYY'] = `19${year}`;
+    }
+  }
+
+  const newSeparator = toFormat[toFormat.length - 1];
+  const resultParts = [];
+
+  for (let j = 0; j < toFormat.length - 1; j++) {
+    const key = toFormat[j];
+
+    resultParts.push(dateMap[key]);
+  }
+
+  return resultParts.join(newSeparator);
 }
 
 module.exports = formatDate;
