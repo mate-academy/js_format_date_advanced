bfcabf001a5cbee1e7396b33cb2d9897735e95ec
Artem Mayboroda <artmay77777@gmail.com>
2026-08-13 17:51:02 +0300

    Implement formatDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,32 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+  const dateParts = date.split(fromSeparator);
+  const dateByFormat = {};
+
+  for (let i = 0; i < 3; i += 1) {
+    dateByFormat[fromFormat[i]] = dateParts[i];
+  }
+
+  if (dateByFormat.YYYY !== undefined) {
+    dateByFormat.YY = dateByFormat.YYYY.slice(-2);
+  }
+
+  if (dateByFormat.YY !== undefined) {
+    const century = Number(dateByFormat.YY) < 30 ? '20' : '19';
+
+    dateByFormat.YYYY = century + dateByFormat.YY;
+  }
+
+  const result = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    result[i] = dateByFormat[toFormat[i]];
+  }
+
+  return result.join(toSeparator);
 }
 
 module.exports = formatDate;
