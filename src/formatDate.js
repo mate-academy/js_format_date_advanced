7ad4d936667aa44843965e16bc23fabacdd656a2
Oleh Puzin <olegpuzin77@icloud.com>
2026-08-16 00:38:23 +0300

    add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,47 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const result = [];
+  const dateArr = date.split(`${fromFormat.at(-1)}`);
+
+  const indexDay = fromFormat.indexOf('DD');
+  const indexMonth = fromFormat.indexOf('MM');
+  const indexYear = dateArr.length - indexDay - indexMonth;
+
+  const day = dateArr[indexDay];
+  const month = dateArr[indexMonth];
+  let year = dateArr[indexYear];
+
+  if (toFormat.includes('YYYY')) {
+    if (+year >= 30 && year.length < 4) {
+      year = `19${year}`;
+    } else if (year.length < 4) {
+      year = `20${year}`;
+    }
+  }
+
+  if (toFormat.includes('YY')) {
+    year = year.slice(-2);
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    switch (toFormat[i]) {
+      case 'YYYY':
+      case 'YY':
+        result.push(year);
+        break;
+      case 'DD':
+        result.push(day);
+        break;
+      case 'MM':
+        result.push(month);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return result.join(`${toFormat.at(-1)}`);
 }
 
 module.exports = formatDate;
